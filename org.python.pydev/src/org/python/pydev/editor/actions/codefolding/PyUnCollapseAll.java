3dbee23b01e992112263c09bf7f320afc8905ece
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2004-07-22 14:08:29 +0000

    Collapse / un collapse all/
    git-svn-id: svn://localhost/trunk@122 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ org.python.pydev/src/org/python/pydev/editor/actions/codefolding/PyUnCollapseAll.java
@@ -0,0 +1,49 @@
+/*
+ * Created on Jul 22, 2004
+ *
+ * @author Fabio Zadrozny
+ */
+package org.python.pydev.editor.actions.codefolding;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.projection.ProjectionAnnotationModel;
+import org.python.pydev.editor.actions.PyAction;
+import org.python.pydev.editor.actions.PySelection;
+import org.python.pydev.editor.codefolding.PyProjectionAnnotation;
+
+/**
+ * @author Fabio Zadrozny
+ */
+public class PyUnCollapseAll extends PyAction {
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(IAction action) {
+		PySelection ps = new PySelection ( getTextEditor ( ), false );
+
+        IAnnotationModel model = (IAnnotationModel) getTextEditor ( )
+                .getAdapter(ProjectionAnnotationModel.class);
+
+        if (model != null) {
+            ArrayList collapsed = new ArrayList();
+            //put annotations in array list.
+            Iterator iter = model.getAnnotationIterator();
+            while (iter != null && iter.hasNext()) {
+                PyProjectionAnnotation element = (PyProjectionAnnotation) iter.next();
+                Position position = model.getPosition(element);
+                
+                model.removeAnnotation(element);
+                element.markExpanded();
+                model.addAnnotation(element, position);
+            }
+        }
+        
+    }
+
+}
