91e729aa600750fa20dc02c39d37b339b6b2b126
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2004-09-16 15:28:41 +0000

    changed package name (this package is just for reference).
    git-svn-id: svn://localhost/trunk@168 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ org.python.pydev/PySrc/aRefactoring/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2003 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Package implementing the refactoring interface module of eric3.
+
+This package implements just the interface for the eric3 IDE. The
+refactoring is done using the Bicyle Repair Man (brm) software
+(<a href="http://bicyclerepair.sourceforge.net">http://bicyclerepair.sourceforge.net</a>).
+"""
