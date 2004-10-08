59d19d9b9b1200ad15249e0490832c3a041ace61
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2004-10-08 16:47:10 +0000

    Execute build.py to get the formatted page.
    git-svn-id: svn://localhost/trunk@211 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ org.python.pydev.help/pydev.sf.net/build.py
@@ -0,0 +1,30 @@
+
+
+
+def Template( p_template, p_name ):
+
+    contents_file = '_%s.contents.html' % p_name
+    target_file   = '%s.html' % p_name
+    
+    d = {
+        'title' :    p_name,
+        'contents' : file( contents_file, 'r' ).read(),
+    }
+
+    contents = file( p_template, 'r' ).read()
+    contents = contents % d
+    file( target_file, 'w' ).write( contents ) 
+
+
+def Main():
+    Template( '_template.html', 'index' )
+    Template( '_template.html', 'Features' )
+    Template( '_template.html', 'Download' )
+    Template( '_template.html', 'Roadmap' )
+    Template( '_template.html', 'codecompletion' )
+    Template( '_template.html', 'contentassist' )
+    Template( '_template.html', 'refactoring' )
+    Template( '_template.html', 'Faq' )
+
+
+Main()
