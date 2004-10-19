39ef0f411fd98a0a502b5ddc0ec0ec7ae0e47e17
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2004-10-19 18:58:25 +0000

    New proposals on quick fix.
    git-svn-id: svn://localhost/trunk@225 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/src/org/python/pydev/editor/correctionassist/PythonCorrectionProcessor.java
+++ org.python.pydev/src/org/python/pydev/editor/correctionassist/PythonCorrectionProcessor.java
@@ -82,18 +82,107 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
 
         PySelection ps = new PySelection(edit, false);
 
-        List results = new ArrayList(); 
-        try {
-            results.addAll(getAssignToResults(ps));
-        } catch (BadLocationException e) {
-        }
+        List results = new ArrayList();
+        String sel = getLine(ps);
         
-        try {
-            results.addAll(getCreations(ps));
-        } catch (BadLocationException e1) {
+        
+        //at least some text must be selected 
+        if(ps.textSelection.getLength() > 0){
+            
+	        try {
+	            results.addAll(getTryProps(ps));
+	        } catch (BadLocationException e) {
+	        }
+        } else if(sel.indexOf("import") == -1){
+
+            try {
+                results.addAll(getAssignToResults(ps));
+	        } catch (BadLocationException e) {
+	        }
+	        
+	        try {
+	            results.addAll(getCreations(ps));
+	        } catch (BadLocationException e1) {
+	        }
+        }else{
+
+	        try {
+	            results.addAll(getMoveImports(ps));
+	        } catch (BadLocationException e1) {
+	        }
         }
 
+    
         return (ICompletionProposal[]) results.toArray(new ICompletionProposal[0]);
+    }
+
+    
+    
+    /**
+     * @param ps
+     * @return
+     */
+    private List getTryProps(PySelection ps)  throws BadLocationException {
+        ArrayList l = new ArrayList();
+        String indentation = PyBackspace.getStaticIndentationString();
+        
+        int start = ps.startLine.getOffset();
+        int end = ps.endLine.getOffset()+ps.endLine.getLength();
+        
+        String string = ps.doc.get(start, end-start);
+        String delimiter = PyAction.getDelimiter(ps.doc, 0);
+        
+        int firstCharPosition = PyAction.getFirstCharRelativePosition(ps.doc, start);
+        String startIndent = "";
+        int i = 0;
+        while(i < firstCharPosition){
+            startIndent += " ";
+            i++;
+        }
+        
+        int finRelNewPos;
+        int excRelNewPos;
+        string = indentation+ string.replaceAll(delimiter, delimiter+indentation);
+        String except = startIndent+"try:"+delimiter+string+delimiter;
+        except += startIndent+"except:"+delimiter;
+        excRelNewPos = except.length() - delimiter.length() -1;
+        except += startIndent+indentation+"raise";
+
+        String finall = startIndent+"try:"+delimiter+string+delimiter;
+        finall += startIndent+"finally:"+delimiter;
+        finall += startIndent+indentation;
+        finRelNewPos = finall.length();
+        finall += "pass";
+
+        l.add(new CompletionProposal(except, start, end-start, excRelNewPos, null,
+                "Surround with try..except", null, null));
+        
+        l.add(new CompletionProposal(finall, start, end-start, finRelNewPos, null,
+                "Surround with try..finally", null, null));
+
+        return l;
+    }
+
+    /**
+     * @param ps
+     * @return
+     */
+    private List getMoveImports(PySelection ps) throws BadLocationException {
+        ArrayList l = new ArrayList();
+        String sel = getLine(ps).trim();
+
+        int i = sel.indexOf("import");
+        if(ps.startLineIndex != ps.endLineIndex)
+            return l;
+        
+        
+        String delimiter = PyAction.getDelimiter(ps.doc, 0);
+        
+        if(i != -1){
+            l.add(new FixCompletionProposal(sel+delimiter, 0, 0, ps.startLine.getOffset(), null,
+                    "Move import to global scope", null, null, ps.startLineIndex+1));
+        }
+        return l;
     }
 
     /**
@@ -103,8 +192,10 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
      */
     private List getCreations(PySelection ps) throws BadLocationException {
         List l = new ArrayList();
+        String sel = getLine(ps);
 
-        if (ps.selection.trim().length() == 0) {
+
+        if (sel.trim().length() == 0) {
             return l;
         }
 
@@ -124,7 +215,7 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
 
         
         String delim = PyAction.getDelimiter(ps.doc, 0);
-        String cls = "class "+callName+":"+delim+delim;
+        String cls = "class "+callName+"(object):"+delim+delim;
         
         String self = "self";
         if(params.trim().length() != 0){
@@ -142,15 +233,15 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
             int lineOfOffset = ps.doc.getLineOfOffset(ps.absoluteCursorOffset);
             
             if(lineOfOffset > 0){
-                newPos = ps.doc.getLineInformation(lineOfOffset - 1).getOffset();
+                newPos = ps.doc.getLineInformation(lineOfOffset).getOffset();
             }
             
-            l.add(new CompletionProposal(cls, newPos, 0, cls.length()+1, null,
-                    "Create new class (global context)", null, null));
+            l.add(new FixCompletionProposal(cls, newPos, 0, cls.length()+1, null,
+                    "Make this a new class", null, null, ps.startLineIndex+4));
 
             method = method.replaceFirst("%s", "");
-            l.add(new CompletionProposal(method, newPos, 0, method.length()+1, null,
-                    "Create new method (global context)", null, null));
+            l.add(new FixCompletionProposal(method, newPos, 0, method.length()+1, null,
+                    "Make this a new method", null, null, ps.startLineIndex+2));
 
         }else{ //we are in a method or class context
 
@@ -170,7 +261,7 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
 	        }
 	        
 	        
-	        if(ps.selection.indexOf("self.") != -1){ //we are going for a class method.
+	        if(sel.indexOf("self.") != -1){ //we are going for a class method.
 	            
 	            if (current instanceof FunctionNode) { //if we are in a class, here we are within a method.
 	                FunctionNode node = (FunctionNode) current;
@@ -199,12 +290,34 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
 	            }
 	            
 	        }else{ //we are going for a class or a global method.
-	            //TODO: End this.
-//	            l.add(new CompletionProposal(callName, 0, 0, 0, null,
-//	                    "Create new class", null, null));
-//	            
-//	            l.add(new CompletionProposal(callName, 0, 0, 0, null,
-//	                    "Create new method", null, null));
+		        
+	            while (current != null) {
+	                if(current instanceof ClassNode) {
+		                break;
+		            }
+		            current = ModelUtils.getPreviousNode(current);
+		        }
+	            if(current instanceof ClassNode){
+	                ClassNode node = (ClassNode) current;
+	                
+	                int newPos = 0;
+	                int lineOfOffset = node.getStart().line;
+	                
+	                if(lineOfOffset > 0){
+	                    newPos = ps.doc.getLineInformation(lineOfOffset).getOffset();
+	                }
+	                method = method+delim;
+	                
+	                method = method.replaceFirst("%s", "");
+	                
+		            l.add(new CompletionProposal(method, newPos, 0, method.length(), null,
+		                    "Create new method (in global context)", null, null));
+
+	                cls = cls+delim;
+
+	                l.add(new CompletionProposal(cls, newPos, 0, cls.length(), null,
+		                    "Create new class (in global context)", null, null));
+	            }
 	        }
         }        
         return l;
@@ -212,19 +325,26 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
 
 
     /**
+     * 
+     */
+    private String getLine(PySelection ps) {
+        return ps.selection.replaceAll("#.*", "");
+    }
+    
+    /**
      * @param ps
      * @throws BadLocationException
      */
     private List getAssignToResults(PySelection ps) throws BadLocationException {
         List l = new ArrayList();
-
-        if (ps.selection.trim().length() == 0) {
+        String sel = getLine(ps);
+        if (sel.trim().length() == 0) {
             return l;
         }
 
         //first thing: check were we are and check that no single '=' exists.
         // '==' may happen.
-        if (ps.selection.replaceAll("==", "").indexOf("=") == -1) {
+        if (sel.replaceAll("==", "").indexOf("=") == -1) {
 
             //second: go on and make the suggestion.
             //
@@ -245,7 +365,7 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
             //					 |result| = 1+1
             //					 self.|result| = 1+1
 
-            String callName = getBeforeParentesisTok(ps);
+            String callName = getTokToAssign(ps);
 
             if(callName.length() > 0){
                 //all that just to change first char to lower case.
@@ -271,10 +391,37 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
      * @param ps
      * @return
      */
+    private String getTokToAssign(PySelection ps) {
+        String beforeParentesisTok = getBeforeParentesisTok(ps);
+        if(beforeParentesisTok.length() > 0){
+            return beforeParentesisTok;
+        }
+        //otherwise, try to find . (ignore code after #)
+        String string = getLine(ps);
+        String callName = "";
+        //get parentesis position and go backwards
+
+        int i;
+        if ((i = string.lastIndexOf(".")) != -1) {
+            callName = "";
+
+            for (int j = i+1; j < string.length() && stillInTok(string, j); j++) {
+                callName += string.charAt(j);
+            }
+        }
+        return callName;
+    }
+
+    /**
+     * @param ps
+     * @return
+     */
     private String getInsideParentesisTok(PySelection ps) {
-        int beg = ps.selection.indexOf('(')+1;
-        int end = ps.selection.indexOf(')');
-        return ps.selection.substring(beg, end);
+        String sel = getLine(ps);
+
+        int beg = sel.indexOf('(')+1;
+        int end = sel.indexOf(')');
+        return sel.substring(beg, end);
     }
 
     /**
@@ -282,12 +429,13 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
      * @return string with the token or empty token if not found.
      */
     private String getBeforeParentesisTok(PySelection ps) {
-        String string = ps.selection.replaceAll("\\(.*\\)", "()");
+        String string = getLine(ps);
 
         int i;
 
         String callName = "";
-        if ((i = string.indexOf("()")) != -1) {
+        //get parentesis position and go backwards
+        if ((i = string.indexOf("(")) != -1) {
             callName = "";
 
             for (int j = i-1; j >= 0 && stillInTok(string, j); j--) {
@@ -306,7 +454,7 @@ public class PythonCorrectionProcessor implements IContentAssistProcessor {
     private boolean stillInTok(String string, int j) {
         char c = string.charAt(j);
 
-        return c != '\n' && c != '\r' && c != ' ' && c != '.' && c != '(' && c != ')' && c != ',' && c != ']' && c != '[';
+        return c != '\n' && c != '\r' && c != ' ' && c != '.' && c != '(' && c != ')' && c != ',' && c != ']' && c != '[' && c != '#';
     }
 
     /*
