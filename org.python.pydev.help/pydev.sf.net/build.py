02c853e17393260d6f1fa26da528f6ecc84b208f
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2004-12-02 16:29:54 +0000

    New page
    git-svn-id: svn://localhost/trunk@287 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev.help/pydev.sf.net/build.py
+++ org.python.pydev.help/pydev.sf.net/build.py
@@ -1,3 +1,4 @@
+import datetime
 def _topicDefault():
     s = \
 '''
@@ -30,6 +31,7 @@ def _getTopicTd(link, name):
     return s % (link, name)
 
 def _topicFeatures():
+    return ''
     s = \
 '''
 <td colspan="2" align="middle" height="100" valign="middle">
@@ -73,22 +75,31 @@ def _topicFeatures():
       )
     
 
-def Template( p_template, p_name, p_title=None , p_topic = None):
+def Template( p_template, p_name, p_title, p_topic , p_channel, p_otherFeatures):
 
+    if p_otherFeatures is None:
+        p_otherFeatures = ''
+        
     if p_topic is None:
         p_topic = _topicDefault()
 
     if p_title is None:
         p_title = p_name
         
+    if p_channel is None:
+        raise RuntimeError('Channel not set')
+        
     contents_file = '_%s.contents.html' % p_name
     target_file   = '%s.html' % p_name
+    
     
     d = {
         'title' :    p_title,
         'contents' : file( contents_file, 'r' ).read(),
-        'perc':'%',
-        'topic': p_topic
+        'topic': p_topic,
+        'channel': p_channel,
+        'date': datetime.datetime.now().strftime('%d %B %Y'),
+        'other_features': p_otherFeatures
     }
 
     contents = file( p_template, 'r' ).read()
@@ -96,25 +107,70 @@ def Template( p_template, p_name, p_title=None , p_topic = None):
     file( target_file, 'w' ).write( contents ) 
 
 
+def _getFeaturesTd(link, name):
+    s = \
+'''
+<tr>
+<td colspan="1"><img src="images/button.png"/> </td>
+<td colspan="1"><a class="menu_links" href="%s">%s</a> </td>
+</tr>
+''' % (link, name)
+
+    return s
+    
+def _otherFeatures():
+    s = \
+'''
+%s
+%s
+%s
+%s
+%s
+%s
+%s
+%s
+%s
+''' 
+    return s % (
+       _getFeaturesTd('editor.html','Editor'),
+       _getFeaturesTd('debug.html','Debugger'),
+       _getFeaturesTd('codecompletion.html','Code Completion'),
+       _getFeaturesTd('templates.html','Templates'),
+       _getFeaturesTd('codecoverage.html','Code Coverage'),
+       _getFeaturesTd('contentassist.html','Content Assistants'),
+       _getFeaturesTd('refactoring.html','Refactoring'),
+       _getFeaturesTd('tasks.html','Tasks'),
+       _getFeaturesTd('pylint.html','PyLint'),
+      )
+
+
 def Main():
-    Template( '_template.html', 'index'                   , 'Pydev'                    )
-    Template( '_template.html', 'features'                , 'Features'                 ,_topicFeatures())
-    Template( '_template.html', 'download'                , 'Download'                 )
-    Template( '_template.html', 'roadmap'                 , 'Roadmap'                  )
-    Template( '_template.html', 'codecompletion'          , 'Code Completion'          ,_topicFeatures())
-    Template( '_template.html', 'codecompletionsnapshots' , 'Code Completion Snapshots')
-    Template( '_template.html', 'contentassist'           , 'Content Assist (Ctrl+1)'  ,_topicFeatures())
-    Template( '_template.html', 'refactoring'             , 'Refactoring'              ,_topicFeatures())
-    Template( '_template.html', 'faq'                     , 'FAQ'                      )
-    Template( '_template.html', 'credits'                 , 'Credits'                  )
-    Template( '_template.html', 'codecoverage'            , 'Code Coverage'            ,_topicFeatures())
-    Template( '_template.html', 'run'                     , 'Run'                      )
-    Template( '_template.html', 'debug'                   , 'Debugger'                 ,_topicFeatures())
-    Template( '_template.html', 'debug_prefs'             , 'Debug Preferences'        )
-    Template( '_template.html', 'editor_prefs'            , 'Editor Preferences'       ,_topicFeatures())
-    Template( '_template.html', 'pylint'                  , 'PyLint'                   ,_topicFeatures())
-    Template( '_template.html', 'pychecker'               , 'PyChecker'                )
-    Template( '_template.html', 'tasks'                   , 'Tasks'                    ,_topicFeatures())
+    channelPydev             = '6798751939'
+    channelPydevFeatures     = '1935586933'
+    channelPydevFeaturesMain = '9124732182'
+    channelPydevIndex        = '0232202381'
     
     
-Main()
+    Template( '_template.html', 'index'                   , 'Pydev'                    ,None             , channelPydevIndex        , None)
+    Template( '_template.html', 'download'                , 'Download'                 ,None             , channelPydev             , None)
+    Template( '_template.html', 'roadmap'                 , 'Roadmap'                  ,None             , channelPydev             , None)
+    Template( '_template.html', 'codecompletionsnapshots' , 'Code Completion Snapshots',None             , channelPydevFeatures     , None)
+    Template( '_template.html', 'faq'                     , 'FAQ'                      ,None             , channelPydev             , None)
+    Template( '_template.html', 'credits'                 , 'Credits'                  ,None             , channelPydev             , None)
+    Template( '_template.html', 'run'                     , 'Run'                      ,None             , channelPydev             , None)
+    Template( '_template.html', 'debug_prefs'             , 'Debug Preferences'        ,None             , channelPydev             , None)
+    Template( '_template.html', 'pychecker'               , 'PyChecker'                ,None             , channelPydevFeatures     , None)
+    Template( '_template.html', 'features'                , 'Features'                 ,_topicFeatures() , channelPydevFeaturesMain , _otherFeatures())
+    Template( '_template.html', 'codecompletion'          , 'Code Completion'          ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    Template( '_template.html', 'templates'               , 'Templates'                ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    Template( '_template.html', 'contentassist'           , 'Content Assist (Ctrl+1)'  ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    Template( '_template.html', 'refactoring'             , 'Refactoring'              ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    Template( '_template.html', 'codecoverage'            , 'Code Coverage'            ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    Template( '_template.html', 'debug'                   , 'Debugger'                 ,_topicFeatures() , channelPydev             , _otherFeatures())
+    Template( '_template.html', 'editor_prefs'            , 'Editor Preferences'       ,_topicFeatures() , channelPydev             , _otherFeatures())
+    Template( '_template.html', 'pylint'                  , 'PyLint'                   ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    Template( '_template.html', 'tasks'                   , 'Tasks'                    ,_topicFeatures() , channelPydevFeatures     , _otherFeatures())
+    print "done"
+    
+if __name__ == '__main__':
+    Main()
