a49a82b990c0060af57aede8431224572807fe98
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2005-07-20 11:10:42 +0000

    fix for getting the libs location.
    git-svn-id: svn://localhost/trunk@512 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/PySrc/interpreterInfo.py
+++ org.python.pydev/PySrc/interpreterInfo.py
@@ -1,6 +1,12 @@
 '''
 This module was created to get information available in the interpreter, such as libraries,
 paths, etc.
+
+TODO: get the builtin modules embeeded in python from sys.builtin_module_names... 
+sys.builtin_module_names: contains the builtin modules embeeded in python (rigth now, we specify all manually).
+
+sys.prefix: A string giving the site-specific directory prefix where the platform independent Python files are installed
+
 '''
 import sys
 import os
@@ -8,10 +14,13 @@ import os
 if __name__ == '__main__':
     executable = sys.executable
     print 'EXECUTABLE:%s|' % executable
-    executableFolder = os.path.dirname(executable)
-    executableFolderLower = executableFolder.lower()
+    
+    #this is the new implementation to get the system folders 
+    #(still need to check if it works in linux)
+    #(previously, we were getting the executable dir, but that is not always correct...)
+    prefix = sys.prefix.lower()
     
     for p in sys.path:
-        if p.lower().startswith(executableFolderLower):
+        if p.lower().startswith(prefix):
             print '|', p
     
