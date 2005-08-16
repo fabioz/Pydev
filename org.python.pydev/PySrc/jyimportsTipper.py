e074ba20a84d12a5b30fdc37d45360b8ee3f15ca
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2005-08-16 16:22:06 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@601 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/PySrc/jyimportsTipper.py
+++ org.python.pydev/PySrc/jyimportsTipper.py
@@ -1,4 +1,20 @@
 import sys
+from java.lang.reflect import Method
+from java.lang.reflect import Field
+from java.lang import Class
+from java.lang import System
+from java.lang import String
+from java.lang import Object
+from java.lang.System import arraycopy
+from java.lang.System import out
+from java.io import OutputStream
+
+from org.python.core import PyReflectedFunction
+from org.python.core import PyReflectedField
+from org.python.core import PyReflectedConstructor
+
+from org.python import core
+from org.python.core import PyReflectedFunction
 
 
 #completion types.
@@ -42,16 +58,121 @@ def GenerateTip( data ):
     return tips
     
 
-import org.python.core as _core
-
+class Info:
+    
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.doc = kwargs.get('doc', None)
+        self.args = kwargs.get('args', None) #tuple of strings
+        self.varargs = kwargs.get('varargs', None) #string
+        self.kwargs = kwargs.get('kwargs', None) #string
+        self.ret = kwargs.get('ret', None) #string
+        
+    def basicAsStr(self):
+        '''@returns this class information as a string (just basic format)
+        '''
+        
+        s = 'function:%s args=%s, varargs=%s, kwargs=%s, docs:%s' % \
+            (str(self.name), str( self.args), str( self.varargs), str( self.kwargs), str( self.doc))
+        return s
+        
 def isclass(cls):
-    return isinstance(cls, _core.PyClass)
+    return isinstance(cls, core.PyClass)
 
-def isfunction(func):
-    return isinstance(func, _core.PyFunction)
+def ismethod(func):
+    '''this function should return the information gathered on a function
+    
+    @param func: this is the function we want to get info on
+    @return a tuple where:
+        0 = indicates whether the parameter passed is a method or not
+        1 = a list of classes 'Info', with the info gathered from the function
+            this is a list because when we have methods from java with the same name and different signatures,
+            we actually have many methods, each with its own set of arguments
+    '''
+    
+    if isinstance(func, core.PyFunction):
+        #ok, this is from python, created by jython
+        #print '    PyFunction'
+        
+        def getargs(func_code):
+            """Get information about the arguments accepted by a code object.
+        
+            Three things are returned: (args, varargs, varkw), where 'args' is
+            a list of argument names (possibly containing nested lists), and
+            'varargs' and 'varkw' are the names of the * and ** arguments or None."""
+        
+            nargs = func_code.co_argcount
+            names = func_code.co_varnames
+            args = list(names[:nargs])
+            step = 0
+        
+            varargs = None
+            if func_code.co_flags & func_code.CO_VARARGS:
+                varargs = func_code.co_varnames[nargs]
+                nargs = nargs + 1
+            varkw = None
+            if func_code.co_flags & func_code.CO_VARKEYWORDS:
+                varkw = func_code.co_varnames[nargs]
+            return args, varargs, varkw
+        
+        args = getargs(func.func_code)
+        return 1, [Info(func.func_name, args = args[0], varargs = args[1],  kwargs = args[2], doc = func.func_doc)]
+        
+    if isinstance(func, core.PyMethod):
+        #this is something from java itself, and jython just wrapped it...
+        
+        #things to play in func:
+        #['__call__', '__class__', '__cmp__', '__delattr__', '__dir__', '__doc__', '__findattr__', '__name__', '_doget', 'im_class',
+        #'im_func', 'im_self', 'toString']
+        #print '    PyMethod'
+        #that's the PyReflectedFunction... keep going to get it
+        func = func.im_func
+
+    if isinstance(func, PyReflectedFunction):
+        #this is something from java itself, and jython just wrapped it...
+        
+        #print '    PyReflectedFunction'
+        
+        infos = []
+        for i in range(len(func.argslist)):
+            #things to play in func.argslist[i]:
+                
+            #'PyArgsCall', 'PyArgsKeywordsCall', 'REPLACE', 'StandardCall', 'args', 'compare', 'compareTo', 'data', 'declaringClass'
+            #'flags', 'isStatic', 'matches', 'precedence']
+            
+            #print '        ', func.argslist[i].data.__class__
+            #func.argslist[i].data.__class__ == java.lang.reflect.Method
+            
+            if func.argslist[i]:
+                met = func.argslist[i].data
+                name = met.getName()
+                ret = met.getReturnType()
+                parameterTypes = met.getParameterTypes()
+                
+                args = []
+                for j in range(len(parameterTypes)):
+                    paramTypesClass = parameterTypes[j]
+                    paramClassName = paramTypesClass.getName()
+                    #if the parameter equals [C, it means it it a char array, so, let's change it
+                    if paramClassName == '[C':
+                        paramClassName = 'char[]'
+                    args.append(paramClassName)
+
+                
+                info = Info(name, args = args, ret = ret)
+                #print info.basicAsStr()
+                infos.append(info)
+
+        return 1, infos
+        
+    return 0, None
 
 def ismodule(mod):
-    return isinstance(mod, _core.PyModule)
+    return isinstance(mod, core.PyModule)
+
+
+def dirObj(obj):
+    return dir(obj)
 
 
 
@@ -61,7 +182,7 @@ def GenerateImportsTipForModule( mod ):
     '''
     ret = []
     
-    dirComps = dir( mod )
+    dirComps = dirObj( mod )
     
     dontGetDocsOn = (float, int, str, tuple, list, type)
     for d in dirComps:
@@ -71,9 +192,11 @@ def GenerateImportsTipForModule( mod ):
         obj = getattr(mod, d)
         retType = TYPE_BUILTIN
 
-        if isfunction(obj):
+        isMet = ismethod(obj)
+        if isMet[0]:
+            info = isMet[1][0]
             try:
-                args, vargs, kwargs, defaults = inspect.getargspec( obj )
+                args, vargs, kwargs = info.args, info.varargs, info.kwargs
                     
                 r = ''
                 for a in ( args ):
--- org.python.pydev/PySrc/tests/test_jysimpleTipper.py
+++ org.python.pydev/PySrc/tests/test_jysimpleTipper.py
@@ -1,138 +1,58 @@
 import unittest
+import os
+import sys
+#make it as if we were executing from the directory above this one (so that we can use pycompletionserver
+#without the need for it being in the pythonpath)
+sys.argv[0] = os.path.dirname(sys.argv[0]) 
+#twice the dirname to get the previous level from this file.
+sys.path.insert(1, os.path.join(  os.path.dirname( sys.argv[0] )) )
+
+from jyimportsTipper import ismethod
+from jyimportsTipper import isclass
+from jyimportsTipper import dirObj
+import jyimportsTipper
 from java.lang.reflect import Method
-from java.lang.reflect import Field
-from java.lang import Class
 from java.lang import System
 from java.lang import String
-from java.lang import Object
 from java.lang.System import arraycopy
 from java.lang.System import out
-from java.io import OutputStream
 
-from org.python.core import PyReflectedFunction
-from org.python.core import PyReflectedField
-from org.python.core import PyReflectedConstructor
-
-from org.python import core
-from org.python.core import PyReflectedFunction
+__DBG = 0
+def dbg(s):
+    if __DBG:
+        print s
+        
 
 
-class Info:
+class TestMod(unittest.TestCase):
     
-    def __init__(self, name, **kwargs):
-        self.name = name
-        self.doc = kwargs.get('doc', None)
-        self.args = kwargs.get('args', None) #tuple of strings
-        self.varargs = kwargs.get('varargs', None) #string
-        self.kwargs = kwargs.get('kwargs', None) #string
-        self.ret = kwargs.get('ret', None) #string
-        
-    def basicAsStr(self):
-        '''@returns this class information as a string (just basic format)
-        '''
-        
-        s = 'function:%s args=%s, varargs=%s, kwargs=%s, docs:%s' % \
-            (str(self.name), str( self.args), str( self.varargs), str( self.kwargs), str( self.doc))
-        return s
-        
-def isclass(cls):
-    return isinstance(cls, core.PyClass)
+    def assertArgs(self, tok, args, tips):
+        for a in tips:
+            if tok == a[0]:
+                self.assertEquals(args, a[2])
+                return
+        raise AssertionError('%s not in %s', tok, tips)
 
-def ismethod(func):
-    '''this function should return the information gathered on a function
-    
-    @param func: this is the function we want to get info on
-    @return a tuple where:
-        0 = indicates whether the parameter passed is a method or not
-        1 = a list of classes 'Info', with the info gathered from the function
-            this is a list because when we have methods from java with the same name and different signatures,
-            we actually have many methods, each with its own set of arguments
-    '''
-    
-    if isinstance(func, core.PyFunction):
-        #ok, this is from python, created by jython
-        print '    PyFunction'
-        
-        def getargs(func_code):
-            """Get information about the arguments accepted by a code object.
-        
-            Three things are returned: (args, varargs, varkw), where 'args' is
-            a list of argument names (possibly containing nested lists), and
-            'varargs' and 'varkw' are the names of the * and ** arguments or None."""
-        
-            nargs = func_code.co_argcount
-            names = func_code.co_varnames
-            args = list(names[:nargs])
-            step = 0
-        
-            varargs = None
-            if func_code.co_flags & func_code.CO_VARARGS:
-                varargs = func_code.co_varnames[nargs]
-                nargs = nargs + 1
-            varkw = None
-            if func_code.co_flags & func_code.CO_VARKEYWORDS:
-                varkw = func_code.co_varnames[nargs]
-            return args, varargs, varkw
-        
-        args = getargs(func.func_code)
-        return 1, [Info(func.func_name, args = args[0], varargs = args[1],  kwargs = args[2], doc = func.func_doc)]
-        
-    if isinstance(func, core.PyMethod):
-        #this is something from java itself, and jython just wrapped it...
-        
-        #things to play in func:
-        #['__call__', '__class__', '__cmp__', '__delattr__', '__dir__', '__doc__', '__findattr__', '__name__', '_doget', 'im_class',
-        #'im_func', 'im_self', 'toString']
-        print '    PyMethod'
-        #that's the PyReflectedFunction... keep going to get it
-        func = func.im_func
+    def assertIn(self, tok, tips):
+        self.assertEquals(4, len(tips[0]))
+        for a in tips:
+            if tok == a[0]:
+                return a
+        raise AssertionError('%s not in %s', tok, tips)
 
-    if isinstance(func, PyReflectedFunction):
-        #this is something from java itself, and jython just wrapped it...
+    def testImports3(self):
+        tip = jyimportsTipper.GenerateTip('os')
+        ret = self.assertIn('path', tip)
+        self.assertEquals('', ret[2])
         
-        print '    PyReflectedFunction'
-        
-        infos = []
-        for i in range(len(func.argslist)):
-            #things to play in func.argslist[i]:
-                
-            #'PyArgsCall', 'PyArgsKeywordsCall', 'REPLACE', 'StandardCall', 'args', 'compare', 'compareTo', 'data', 'declaringClass'
-            #'flags', 'isStatic', 'matches', 'precedence']
-            
-            #print '        ', func.argslist[i].data.__class__
-            #func.argslist[i].data.__class__ == java.lang.reflect.Method
-            
-            met = func.argslist[i].data
-            name = met.getName()
-            ret = met.getReturnType()
-            parameterTypes = met.getParameterTypes()
-            
-            args = []
-            for j in range(len(parameterTypes)):
-                paramTypesClass = parameterTypes[j]
-                paramClassName = paramTypesClass.getName()
-                #if the parameter equals [C, it means it it a char array, so, let's change it
-                if paramClassName == '[C':
-                    paramClassName = 'char[]'
-                args.append(paramClassName)
-
-                
-            info = Info(name, args = args, ret = ret)
-            print info.basicAsStr()
-            infos.append(info)
-
-        return 1, infos
-        
-    return 0, None
-
-def ismodule(mod):
-    return isinstance(mod, core.PyModule)
+    def testImports(self):
+        tip = jyimportsTipper.GenerateTip('__builtin__')
+        self.assertIn('tuple'          , tip)
+        self.assertIn('RuntimeError'   , tip)
+        self.assertIn('RuntimeWarning' , tip)
 
 
-def dirObj(obj):
-    return dir(obj)
-
-class Test(unittest.TestCase):
+class TestCompl(unittest.TestCase):
 
     def setUp(self):
         unittest.TestCase.setUp(self)
@@ -142,28 +62,31 @@ class Test(unittest.TestCase):
 
     def testGettingInfoOnJython(self):
         
-        print '\n\n--------------------------- Method'
+        dbg( '\n\n--------------------------- Method')
         assert not ismethod(Method)[0]
         assert isclass(Method)
             
-        print '\n\n--------------------------- System'
+        dbg( '\n\n--------------------------- System')
         assert not ismethod(System)[0]
         assert isclass(System)
             
-        print '\n\n--------------------------- String'
+        dbg( '\n\n--------------------------- String')
         assert not ismethod(System)[0]
         assert isclass(String)
         assert len(dirObj(String)) > 10
             
-        print '\n\n--------------------------- arraycopy'
-        assert ismethod(arraycopy)[0]
+        dbg( '\n\n--------------------------- arraycopy')
+        isMet = ismethod(arraycopy)
+        assert isMet[0]
+        assert isMet[1][0].basicAsStr() == "function:arraycopy args=['java.lang.Object', 'int', 'java.lang.Object', 'int', 'int'], varargs=None, kwargs=None, docs:None"
         assert not isclass(arraycopy)
             
-        print '\n\n--------------------------- out'
-        assert ismethod(out)
+        dbg( '\n\n--------------------------- out')
+        isMet = ismethod(out)
+        assert not isMet[0]
         assert not isclass(out)
             
-        print '\n\n--------------------------- out.println'
+        dbg( '\n\n--------------------------- out.println')
         isMet = ismethod(out.println)
         assert isMet[0]
         assert len(isMet[1]) == 10
@@ -171,7 +94,7 @@ class Test(unittest.TestCase):
         assert isMet[1][1].basicAsStr() == "function:println args=['long'], varargs=None, kwargs=None, docs:None"
         assert not isclass(out.println)
         
-        print '\n\n--------------------------- str'
+        dbg( '\n\n--------------------------- str')
         isMet = ismethod(str)
         assert isMet[0]
         assert isMet[1][0].basicAsStr() == "function:str args=['org.python.core.PyObject'], varargs=None, kwargs=None, docs:None"
@@ -182,7 +105,7 @@ class Test(unittest.TestCase):
             a=3
             return a
         
-        print '\n\n--------------------------- met1'
+        dbg( '\n\n--------------------------- met1')
         isMet = ismethod(met1)
         assert isMet[0]
         assert isMet[1][0].basicAsStr() == "function:met1 args=[], varargs=None, kwargs=None, docs:None"
@@ -194,7 +117,7 @@ class Test(unittest.TestCase):
             a=1
             return a
         
-        print '\n\n--------------------------- met2'
+        dbg( '\n\n--------------------------- met2')
         isMet = ismethod(met2)
         assert isMet[0]
         assert isMet[1][0].basicAsStr() == "function:met2 args=['arg1', 'arg2'], varargs=vararg, kwargs=kwarg, docs:docmet2"
