1d916e6141563f14e0a3cd6e67802921a5280b96
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2005-08-25 11:01:59 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@79 42a48d6c-1186-114d-bcb1-a7d39102e91d

---
+++ com.python.pydev.codecompletion/tests/com/python/pydev/codecompletion/participant/CompletionParticipantTest.java
@@ -0,0 +1,25 @@
+/*
+ * License: Common Public License v1.0
+ * Created on 25/08/2005
+ * 
+ * @author Fabio Zadrozny
+ */
+package com.python.pydev.codecompletion.participant;
+
+import junit.framework.TestCase;
+
+public class CompletionParticipantTest extends TestCase {
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(CompletionParticipantTest.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
