b0426de79f6cd15db62892fe5181bb654150bf0c
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2005-08-30 16:08:15 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@86 42a48d6c-1186-114d-bcb1-a7d39102e91d

---
+++ com.python.pydev.debug/src/com/python/pydev/debug/DebugPlugin.java
@@ -0,0 +1,54 @@
+package com.python.pydev.debug;
+
+import org.eclipse.ui.plugin.*;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class DebugPlugin extends AbstractUIPlugin {
+
+	//The shared instance.
+	private static DebugPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public DebugPlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static DebugPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("com.python.pydev.debug", path);
+	}
+}
