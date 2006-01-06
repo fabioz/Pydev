0314e14f9fa24bbdedfd58a2f49aba37948362b0
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-01-06 00:42:10 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@219 42a48d6c-1186-114d-bcb1-a7d39102e91d

---
+++ com.python.pydev/tests/mail/SendApp.java
@@ -0,0 +1,36 @@
+package mail;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import com.sun.mail.smtp.SMTPMessage;
+import com.sun.mail.smtp.SMTPTransport;
+
+public class SendApp {
+    public static void send(String smtpHost, int smtpPort, String from, String to, String subject, String content) throws AddressException, MessagingException {
+        // Create a mail session
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "" + smtpPort);
+        Session session = Session.getDefaultInstance(props, null);
+        
+        // Construct the message
+        SMTPMessage msg = new SMTPMessage(session);
+        msg.setFrom(new InternetAddress(from));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        msg.setSubject(subject);
+        msg.setText(content);
+
+        // Send the message
+        System.out.println("sending");
+        SMTPTransport.send(msg);
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Send a test message
+        send("esss.com.br", 25, "fabioz@esss.com.br", "fabioz@esss.com.br", "re: test", "Did it go?");
+    }
+}
