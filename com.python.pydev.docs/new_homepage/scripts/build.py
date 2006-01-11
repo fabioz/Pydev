592cd8182c543d07b84f8ad8f9575ed80a46df43
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-01-11 16:55:19 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@241 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.docs/new_homepage/scripts/build.py
+++ com.python.pydev.docs/new_homepage/scripts/build.py
@@ -1,14 +1,24 @@
+import datetime
 LAST_VERSION_TAG='0.9.8.6'
 
 
 def template( template, contents, title ):
 
-    contents_file = '_%s.contents.html' % p_name
-    target_file   = 'final/%s.html' % p_name
+    contents_file = '../%s.contents.html' % contents
+    target_file   = '../final/%s.html' % contents
 
+    newFile = file( contents_file, 'r' ).read()
+
+    contents = file( template, 'r' ).read()
+    contents = contents.replace('%(contents)s', newFile)
+    contents = contents.replace('%(title)s', title)
+    contents = contents.replace('%(date)s', datetime.datetime.now().strftime('%d %B %Y'))
+    contents = contents.replace('LAST_VERSION_TAG', LAST_VERSION_TAG)
+    file( target_file, 'w' ).write( contents ) 
 
 def main():
-    template('template.html', 'index', 'Pydev Extensions')
+    template('../template1.html', 'index', 'Pydev Extensions')
 
 if __name__ == '__main__':
     main()
+    print 'built'
