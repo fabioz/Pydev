f2c296e343b193f39fdd356147da0104823f889b
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-02-02 16:33:29 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@298 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.docs/new_homepage/scripts/build.py
+++ com.python.pydev.docs/new_homepage/scripts/build.py
@@ -3,7 +3,32 @@ LAST_VERSION_TAG='0.9.8.8'
 
 #http://www.fabioz.com/pydev/successful_payment.html
 #http://www.fabioz.com/pydev/cancel_payment.html
+manual_101 = \
+(('manual_101_root'           , 'Getting Started'                ),
+('manual_101_install'        , 'Installing'                     ),
+('manual_101_interpreter'    , 'Configuring the interpreter'    ),
+('manual_101_project_conf'   , 'Creating a project'             ),
+('manual_101_project_conf2'  , 'Configuring a project'          ),
+('manual_101_first_module'   , 'Creating a module'              ),
+('manual_101_run'            , 'Running your first program'     ),
+('manual_101_tips'           , 'Some useful tips'               ))
 
+manual_adv = \
+(('manual_adv_features'       , 'Features'                        ),
+('manual_adv_editor_prefs'   , 'Editor preferences'              ),
+('manual_adv_refactoring'    , 'Refactoring'                     ),
+('manual_adv_assistants'     , 'Content Assistants'              ),
+('manual_adv_coverage'       , 'Code Coverage'                   ),
+('manual_adv_tasks'          , 'Tasks'                           ),
+('manual_adv_code_analysis'  , 'Code Analysis'                   ),
+('manual_adv_quick_outline'  , 'Quick Outline'                   ),
+('manual_adv_gotodef'        , 'Go to Definition'                ),
+('manual_adv_compltemp'      , 'Templates completion'            ),
+('manual_adv_complctx'       , 'Context-sensitive completions'   ),
+('manual_adv_complnoctx'     , 'Context-insensitive completions' ),
+('manual_adv_complauto'      , 'Auto-suggest keywords'           ),
+('manual_adv_debugger'       , 'Debugger'                        ),
+('manual_adv_remote_debugger', 'Remote Debugger'                 ))
 
 def template( template, contents, title, **kwargs ):
 
@@ -35,7 +60,7 @@ def getContents(contents_file, tag):
         iend = contents_file.index('</%s>'%tag)
         contents_area = contents_file[istart: iend]
     except ValueError:
-        return ""
+        return ''
     return contents_area
     
 
@@ -56,23 +81,23 @@ def main():
     template('../templateManual.html', 'manual_101_run'            , 'Running your first program'      )
     template('../templateManual.html', 'manual_101_tips'           , 'Some useful tips'                )
     
-    template('../templateManual.html', 'manual_adv_features'       , "Features"                        )
-    template('../templateManual.html', 'manual_adv_editor_prefs'   , "Editor preferences"              )
-    template('../templateManual.html', 'manual_adv_refactoring'    , "Refactoring"                     )
-    template('../templateManual.html', 'manual_adv_assistants'     , "Content Assistants"              )
-    template('../templateManual.html', 'manual_adv_coverage'       , "Code Coverage"                   )
-    template('../templateManual.html', 'manual_adv_tasks'          , "Tasks"                           )
-    template('../templateManual.html', 'manual_adv_code_analysis'  , "Code Analysis"                   )
-    template('../templateManual.html', 'manual_adv_quick_outline'  , "Quick Outline"                   )
-    template('../templateManual.html', 'manual_adv_gotodef'        , "Go to Definition"                )
-    template('../templateManual.html', 'manual_adv_compltemp'      , "Templates completion"            )
-    template('../templateManual.html', 'manual_adv_complctx'       , "Context-sensitive completions"   )
-    template('../templateManual.html', 'manual_adv_complnoctx'     , "Context-insensitive completions" )
-    template('../templateManual.html', 'manual_adv_complauto'      , "Auto-suggest keywords"           )
-    template('../templateManual.html', 'manual_adv_debugger'       , "Debugger"                        )
-    template('../templateManual.html', 'manual_adv_remote_debugger', "Remote Debugger"                 )
+    template('../templateManual.html', 'manual_adv_features'       , 'Features'                        )
+    template('../templateManual.html', 'manual_adv_editor_prefs'   , 'Editor preferences'              )
+    template('../templateManual.html', 'manual_adv_refactoring'    , 'Refactoring'                     )
+    template('../templateManual.html', 'manual_adv_assistants'     , 'Content Assistants'              )
+    template('../templateManual.html', 'manual_adv_coverage'       , 'Code Coverage'                   )
+    template('../templateManual.html', 'manual_adv_tasks'          , 'Tasks'                           )
+    template('../templateManual.html', 'manual_adv_code_analysis'  , 'Code Analysis'                   )
+    template('../templateManual.html', 'manual_adv_quick_outline'  , 'Quick Outline'                   )
+    template('../templateManual.html', 'manual_adv_gotodef'        , 'Go to Definition'                )
+    template('../templateManual.html', 'manual_adv_compltemp'      , 'Templates completion'            )
+    template('../templateManual.html', 'manual_adv_complctx'       , 'Context-sensitive completions'   )
+    template('../templateManual.html', 'manual_adv_complnoctx'     , 'Context-insensitive completions' )
+    template('../templateManual.html', 'manual_adv_complauto'      , 'Auto-suggest keywords'           )
+    template('../templateManual.html', 'manual_adv_debugger'       , 'Debugger'                        )
+    template('../templateManual.html', 'manual_adv_remote_debugger', 'Remote Debugger'                 )
     
-    template('../templateManual.html', 'manual_adv_keybindings'    , "Keybindings"                     )
+    template('../templateManual.html', 'manual_adv_keybindings'    , 'Keybindings'                     )
 
 
 if __name__ == '__main__':
