147373379a9261679334ba3a368945a7bf5aa09f
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-02-10 18:51:39 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@319 42a48d6c-1186-114d-bcb1-a7d39102e91d

---
+++ com.python.pydev.docs/pydev_freemind.mm
@@ -0,0 +1,29 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1139594066796" ID="Freemind_Link_1375434945" MODIFIED="1139596875734" STYLE="bubble" TEXT="Pydev">
+<cloud COLOR="#99ffff"/>
+<node CREATED="1139594080375" ID="_" MODIFIED="1139596863015" POSITION="right" TEXT="New Project Wizard">
+<edge WIDTH="thin"/>
+<node CREATED="1139594135046" ID="Freemind_Link_1097477604" MODIFIED="1139594152796" TEXT="Select external projects to add to pythonpath"/>
+<node CREATED="1139594154062" ID="Freemind_Link_242417594" MODIFIED="1139594162953" TEXT="Select / create source folders"/>
+</node>
+<node CREATED="1139594201281" ID="Freemind_Link_91517654" MODIFIED="1139594204484" POSITION="left" TEXT="Bugs">
+<node CREATED="1139594167765" ID="Freemind_Link_363994591" MODIFIED="1139594196687" TEXT="Ctrl+Alt+W: looses action"/>
+<node CREATED="1139594377406" ID="Freemind_Link_442163909" MODIFIED="1139596778843" TEXT="from xxx &apos;import&apos; completion should only work in the &apos;code&apos; partition"/>
+<node CREATED="1139595096843" ID="Freemind_Link_148953784" MODIFIED="1139596781765" TEXT="&#xa;def Foo(self):&#xa;    print _Bar()&#xa;&#xa;def _Bar():&#xa;    pass&#xa;&#xa;try:&#xa;    # Should show duplicated error (shows unused import)&#xa;    from empty2 import Bar2 as _Bar &#xa;except:&#xa;    #if available, use it&#xa;    pass&#xa;">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1139596523890" ID="Freemind_Link_1004815783" MODIFIED="1139596670812" TEXT="def Load(self):&#xa;    #Is giving Unused variable: i&#xa;    for i in xrange(10):    &#xa;        coerce(dict[i].text.strip())"/>
+</node>
+<node CREATED="1139594227703" ID="Freemind_Link_1643430887" MODIFIED="1139594232546" POSITION="right" TEXT="Code Completion">
+<node CREATED="1139594237328" ID="Freemind_Link_215044614" MODIFIED="1139594286953" TEXT="Code Completion for parameters: bring the data as &apos;ctx insensitive after &apos;x&apos; characters"/>
+<node CREATED="1139594311703" ID="Freemind_Link_1252891293" MODIFIED="1139594341078" TEXT="filter compatible interfaces if there is some method already declared (in the context)"/>
+<node CREATED="1139594344859" ID="Freemind_Link_378572787" MODIFIED="1139594366187" TEXT="filter method if there is some assert isinstance(xxx,Class)"/>
+<node CREATED="1139594431937" ID="Freemind_Link_1599281754" MODIFIED="1139596817750" TEXT="calltips for methods should be implemented"/>
+</node>
+<node CREATED="1139594461453" ID="Freemind_Link_521048315" MODIFIED="1139594464906" POSITION="left" TEXT="Extensions">
+<node CREATED="1139594467156" ID="Freemind_Link_1278244917" MODIFIED="1139594642328" TEXT="Pretty print"/>
+<node CREATED="1139594480906" ID="Freemind_Link_739678165" MODIFIED="1139594644687" TEXT="Refactoring"/>
+</node>
+</node>
+</map>
