de7d1854a5cce9b859f8dffae1f2c09626643a82
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2006-05-26 13:41:32 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@1194 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/src/org/python/pydev/editor/PyDoubleClickStrategy.java
+++ org.python.pydev/src/org/python/pydev/editor/PyDoubleClickStrategy.java
@@ -1,453 +1,24 @@
 /*
- * Author: atotic
+ * Author: atotic, fabioz
  * Created on Apr 14, 2004
  * License: Common Public License v1.0
  */
 package org.python.pydev.editor;
-
-import java.io.IOException;
-import java.io.Reader;
 
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
 import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.Region;
-import org.eclipse.jface.text.source.ICharacterPairMatcher;
+import org.python.copiedfromeclipsesrc.PythonPairMatcher;
 import org.python.pydev.core.docutils.DocUtils;
 
 /**
- * Our double-click implementation. 
- * 
- * Copied org.eclipse.jdt.internal.ui.text.java.JavaDoubleClickStrategy.
+ * Our double-click implementation. Based on org.eclipse.jdt.internal.ui.text.java.JavaDoubleClickStrategy.
  */
 public class PyDoubleClickStrategy implements ITextDoubleClickStrategy {
 
-	public class JavaCodeReader extends Reader {
-	
-		/** The EOF character */
-		public static final int EOF= -1;
-	
-		private boolean fSkipComments= false;
-		private boolean fSkipStrings= false;
-		private boolean fForward= false;
-	
-		private IDocument fDocument;
-		private int fOffset;
-	
-		private int fEnd= -1;
-		private int fCachedLineNumber= -1;
-		private int fCachedLineOffset= -1;
-	
-	
-		public int read(char cbuf[], int off, int len) throws IOException {
-			int end= off + len;
-			for (int i= off; i < end; i++) {
-				int ch= read();
-				if (ch == -1) {
-					if (i == off) {
-						return -1;
-					} else {
-						return i - off;
-					}
-				}
-				cbuf[i]= (char)ch;
-			}
-			return len;
-		}		
-	
-		/**
-		 * @see Reader#ready()
-		 */		
-		public boolean ready() throws IOException {
-			return true;
-		}
-	
-		/**
-		 * Gets the content as a String
-		 */
-		public String getString() throws IOException {
-			StringBuffer buf= new StringBuffer();
-			int ch;
-			while ((ch= read()) != -1) {
-				buf.append((char)ch);
-			}
-			return buf.toString();
-		}
-		public JavaCodeReader() {
-		}
-	
-		/**
-		 * Returns the offset of the last read character. Should only be called after read has been called.
-		 */
-		public int getOffset() {
-			return fForward ? fOffset -1 : fOffset;
-		}
-	
-		public void configureForwardReader(IDocument document, int offset, int length, boolean skipComments, boolean skipStrings) throws IOException {
-			fDocument= document;
-			fOffset= offset;
-			fSkipComments= skipComments;
-			fSkipStrings= skipStrings;
-		
-			fForward= true;
-			fEnd= Math.min(fDocument.getLength(), fOffset + length);		
-		}
-	
-		public void configureBackwardReader(IDocument document, int offset, boolean skipComments, boolean skipStrings) throws IOException {
-			fDocument= document;
-			fOffset= offset;
-			fSkipComments= skipComments;
-			fSkipStrings= skipStrings;
-		
-			fForward= false;
-			try {
-				fCachedLineNumber= fDocument.getLineOfOffset(fOffset);
-			} catch (BadLocationException x) {
-				throw new IOException(x.getMessage());
-			}
-		}
-	
-		/*
-		 * @see Reader#close()
-		 */
-		public void close() throws IOException {
-			fDocument= null;
-		}
-	
-		/*
-		 * @see SingleCharReader#read()
-		 */
-		public int read() throws IOException {
-			try {
-				return fForward ? readForwards() : readBackwards();
-			} catch (BadLocationException x) {
-				throw new IOException(x.getMessage());
-			}
-		}
-	
-		private void gotoCommentEnd() throws BadLocationException {
-			while (fOffset < fEnd) {
-				char current= fDocument.getChar(fOffset++);
-				if (current == '*') {
-					if (fOffset < fEnd && fDocument.getChar(fOffset) == '/') {
-						++ fOffset;
-						return;
-					}
-				}
-			}
-		}
-	
-		private void gotoStringEnd(char delimiter) throws BadLocationException {
-			while (fOffset < fEnd) {
-				char current= fDocument.getChar(fOffset++);
-				if (current == '\\') {
-					// ignore escaped characters
-					++ fOffset;
-				} else if (current == delimiter) {
-					return;
-				}
-			}
-		}
-	
-		private void gotoLineEnd() throws BadLocationException {
-			int line= fDocument.getLineOfOffset(fOffset);
-			fOffset= fDocument.getLineOffset(line + 1);
-		}
-	
-		private int readForwards() throws BadLocationException {
-			while (fOffset < fEnd) {
-				char current= fDocument.getChar(fOffset++);
-			
-				switch (current) {
-					case '/':
-					
-						if (fSkipComments && fOffset < fEnd) {
-							char next= fDocument.getChar(fOffset);
-							if (next == '*') {
-								// a comment starts, advance to the comment end
-								++ fOffset;
-								gotoCommentEnd();
-								continue;
-							} else if (next == '/') {
-								// '//'-comment starts, advance to the line end
-								gotoLineEnd();
-								continue;
-							}
-						}
-					
-						return current;
-					
-					case '"':
-					case '\'':
-				
-						if (fSkipStrings) {
-							gotoStringEnd(current);
-							continue;
-						}
-					
-						return current;
-				}
-			
-				return current;
-			}
-		
-			return EOF;
-		}
-	
-		private void handleSingleLineComment() throws BadLocationException {
-			int line= fDocument.getLineOfOffset(fOffset);
-			if (line < fCachedLineNumber) {
-				fCachedLineNumber= line;
-				fCachedLineOffset= fDocument.getLineOffset(line);
-				int offset= fOffset;
-				while (fCachedLineOffset < offset) {
-					char current= fDocument.getChar(offset--);
-					if (current == '/' && fCachedLineOffset <= offset && fDocument.getChar(offset) == '/') {
-						fOffset= offset;
-						return;
-					}
-				}
-			}
-		}
-	
-		private void gotoCommentStart() throws BadLocationException {
-			while (0 < fOffset) {
-				char current= fDocument.getChar(fOffset--);
-				if (current == '*' && 0 <= fOffset && fDocument.getChar(fOffset) == '/')
-					return;
-			}
-		}
-	
-		private void gotoStringStart(char delimiter) throws BadLocationException {
-			while (0 < fOffset) {
-				char current= fDocument.getChar(fOffset);
-				if (current == delimiter) {
-					if ( !(0 <= fOffset && fDocument.getChar(fOffset -1) == '\\'))
-						return;
-				}
-				-- fOffset;
-			}
-		}
-		
-		private int readBackwards() throws BadLocationException {
-		
-			while (0 < fOffset) {
-				-- fOffset;
-			
-				handleSingleLineComment();
-			
-				char current= fDocument.getChar(fOffset);
-				switch (current) {
-					case '/':
-					
-						if (fSkipComments && fOffset > 1) {
-							char next= fDocument.getChar(fOffset - 1);
-							if (next == '*') {
-								// a comment ends, advance to the comment start
-								fOffset -= 2;
-								gotoCommentStart();
-								continue;
-							}
-						}
-					
-						return current;
-					
-					case '"':
-					case '\'':
-				
-						if (fSkipStrings) {
-							-- fOffset;
-							gotoStringStart(current);
-							continue;
-						}
-					
-						return current;
-				}
-			
-				return current;
-			}
-		
-			return EOF;
-		}
-	}
-
-	
-	/**
-	 * swiped an internal class straight out of Java code
-	 * it was originaly org.eclipse.jdt.internal.ui.text.JavaPairMatcher
-	 */
-	public class JavaPairMatcher implements ICharacterPairMatcher {
-	
-		protected char[] fPairs;
-		protected IDocument fDocument;
-		protected int fOffset;
-	
-		protected int fStartPos;
-		protected int fEndPos;
-		protected int fAnchor;
-	
-		protected JavaCodeReader fReader= new JavaCodeReader();
-	
-	
-		public JavaPairMatcher(char[] pairs) {
-			fPairs= pairs;
-		}
-	
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.text.source.ICharacterPairMatcher#match(org.eclipse.jface.text.IDocument, int)
-		 */
-		public IRegion match(IDocument document, int offset) {
-
-			fOffset= offset;
-
-			if (fOffset < 0)
-				return null;
-
-			fDocument= document;
-
-			if (fDocument != null && matchPairsAt() && fStartPos != fEndPos)
-				return new Region(fStartPos, fEndPos - fStartPos + 1);
-			
-			return null;
-		}
-	
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.text.source.ICharacterPairMatcher#getAnchor()
-		 */
-		public int getAnchor() {
-			return fAnchor;
-		}
-	
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.text.source.ICharacterPairMatcher#dispose()
-		 */
-		public void dispose() {
-			clear();
-			fDocument= null;
-			fReader= null;
-		}
-	
-		/*
-		 * @see org.eclipse.jface.text.source.ICharacterPairMatcher#clear()
-		 */
-		public void clear() {
-			if (fReader != null) {
-				try {
-					fReader.close();
-				} catch (IOException x) {
-					// ignore
-				}
-			}
-		}
-	
-		protected boolean matchPairsAt() {
-
-			int i;
-			int pairIndex1= fPairs.length;
-			int pairIndex2= fPairs.length;
-
-			fStartPos= -1;
-			fEndPos= -1;
-
-			// get the chars preceding and following the start position
-			try {
-
-				char prevChar= fDocument.getChar(Math.max(fOffset - 1, 0));
-//	   modified behavior for http://dev.eclipse.org/bugs/show_bug.cgi?id=16879			
-//				char nextChar= fDocument.getChar(fOffset);
-
-				// search for opening peer character next to the activation point
-				for (i= 0; i < fPairs.length; i= i + 2) {
-//					if (nextChar == fPairs[i]) {
-//						fStartPos= fOffset;
-//						pairIndex1= i;
-//					} else 
-					if (prevChar == fPairs[i]) {
-						fStartPos= fOffset - 1;
-						pairIndex1= i;
-					}
-				}
-			
-				// search for closing peer character next to the activation point
-				for (i= 1; i < fPairs.length; i= i + 2) {
-					if (prevChar == fPairs[i]) {
-						fEndPos= fOffset - 1;
-						pairIndex2= i;
-					} 
-//					else if (nextChar == fPairs[i]) {
-//						fEndPos= fOffset;
-//						pairIndex2= i;
-//					}
-				}
-
-				if (fEndPos > -1) {
-					fAnchor= RIGHT;
-					fStartPos= searchForOpeningPeer(fEndPos, fPairs[pairIndex2 - 1], fPairs[pairIndex2], fDocument);
-					if (fStartPos > -1)
-						return true;
-					else
-						fEndPos= -1;
-				}	else if (fStartPos > -1) {
-					fAnchor= LEFT;
-					fEndPos= searchForClosingPeer(fStartPos, fPairs[pairIndex1], fPairs[pairIndex1 + 1], fDocument);
-					if (fEndPos > -1)
-						return true;
-					else
-						fStartPos= -1;
-				}
-
-			} catch (BadLocationException x) {
-			} catch (IOException x) {
-			}
-
-			return false;
-		}
-	
-		protected int searchForClosingPeer(int offset, int openingPeer, int closingPeer, IDocument document) throws IOException {
-		
-			fReader.configureForwardReader(document, offset + 1, document.getLength(), true, true);
-		
-			int stack= 1;
-			int c= fReader.read();
-			while (c != JavaCodeReader.EOF) {
-				if (c == openingPeer && c != closingPeer)
-					stack++;
-				else if (c == closingPeer)
-					stack--;
-				
-				if (stack == 0)
-					return fReader.getOffset();
-				
-				c= fReader.read();
-			}
-		
-			return  -1;
-		}
-	
-		protected int searchForOpeningPeer(int offset, int openingPeer, int closingPeer, IDocument document) throws IOException {
-		
-			fReader.configureBackwardReader(document, offset, true, true);
-		
-			int stack= 1;
-			int c= fReader.read();
-			while (c != JavaCodeReader.EOF) {
-				if (c == closingPeer && c != openingPeer)
-					stack++;
-				else if (c == openingPeer)
-					stack--;
-				
-				if (stack == 0)
-					return fReader.getOffset();
-				
-				c= fReader.read();
-			}
-		
-			return -1;
-		}
-	}
-	protected JavaPairMatcher fPairMatcher = new JavaPairMatcher(DocUtils.BRACKETS);
+	protected PythonPairMatcher fPairMatcher = new PythonPairMatcher(DocUtils.BRACKETS);
 
 	/**
 	 * @see ITextDoubleClickStrategy#doubleClicked
@@ -462,18 +33,14 @@ public class PyDoubleClickStrategy implements ITextDoubleClickStrategy {
 		IDocument document = textViewer.getDocument();
 
 		IRegion region = fPairMatcher.match(document, offset);
-		if (region != null && region.getLength() >= 2)
-			textViewer.setSelectedRange(
-				region.getOffset() + 1,
-				region.getLength() - 2);
-		else
+		if (region != null && region.getLength() >= 2){
+			textViewer.setSelectedRange(region.getOffset() + 1, region.getLength() - 2);
+		}else{
 			selectWord(textViewer, document, offset);
+		}
 	}
 
-	protected void selectWord(
-		ITextViewer textViewer,
-		IDocument document,
-		int anchor) {
+	protected void selectWord(ITextViewer textViewer, IDocument document, int anchor) {
 
 		try {
 
@@ -482,8 +49,10 @@ public class PyDoubleClickStrategy implements ITextDoubleClickStrategy {
 
 			while (offset >= 0) {
 				c = document.getChar(offset);
-				if (!Character.isJavaIdentifierPart(c))
+				if (!Character.isJavaIdentifierPart(c)){
 					break;
+				}
+					
 				--offset;
 			}
 
@@ -494,17 +63,19 @@ public class PyDoubleClickStrategy implements ITextDoubleClickStrategy {
 
 			while (offset < length) {
 				c = document.getChar(offset);
-				if (!Character.isJavaIdentifierPart(c))
+				if (!Character.isJavaIdentifierPart(c)){
 					break;
+				}
 				++offset;
 			}
 
 			int end = offset;
 
-			if (start == end)
+			if (start == end){
 				textViewer.setSelectedRange(start, 0);
-			else
+			}else{
 				textViewer.setSelectedRange(start + 1, end - start - 1);
+			}
 
 		} catch (BadLocationException x) {
 		}
--- org.python.pydev/src/org/python/pydev/editor/partitioner/FastPythonPartitionScanner.java
+++ org.python.pydev/src/org/python/pydev/editor/partitioner/FastPythonPartitionScanner.java
@@ -1,12 +1,16 @@
 package org.python.pydev.editor.partitioner;
 
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.rules.ICharacterScanner;
 import org.eclipse.jface.text.rules.IPartitionTokenScanner;
 import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
 import org.python.pydev.core.IPythonPartitions;
 
 /**
  * Based on org.eclipse.jdt.internal.ui.text.FastJavaPartitionScanner
+ * 
+ * Could become a replacement
  * 
  * @author fabioz
  */
@@ -15,37 +19,29 @@ public class FastPythonPartitionScanner implements IPartitionTokenScanner, IPyth
 
 	// states
 	private static final int PYTHON= 0;
-	private static final int SINGLE_LINE_COMMENT= 1;
-	private static final int STRING= 2;
-	private static final int MULTI_LINE_STRING= 3;
+	private static final int COMMENT= 1;
+	private static final int SINGLE_LINE_STRING1= 2; //'
+	private static final int SINGLE_LINE_STRING2= 3; //"
+	private static final int MULTI_LINE_STRING1= 4;  //'
+	private static final int MULTI_LINE_STRING2= 5;  //""
+	private static final int BACKQUOTES= 6;  
 	
-	// beginning of prefixes and postfixes
-	private static final int NONE= 0;
-	private static final int BACKSLASH= 1; // postfix for STRING and CHARACTER
-	private static final int SLASH= 2; // prefix for SINGLE_LINE or MULTI_LINE or JAVADOC
-	private static final int SLASH_STAR= 3; // prefix for MULTI_LINE_COMMENT or JAVADOC
-	private static final int SLASH_STAR_STAR= 4; // prefix for MULTI_LINE_COMMENT or JAVADOC
-	private static final int STAR= 5; // postfix for MULTI_LINE_COMMENT or JAVADOC
-	private static final int CARRIAGE_RETURN=6; // postfix for STRING, CHARACTER and SINGLE_LINE_COMMENT
-
-	
-	
-	/** The offset of the last returned token. */
-	private int fTokenOffset;
-	/** The length of the last returned token. */
-	private int fTokenLength;
-
-	/** The state of the scanner. */
-	private int fState;
-	/** The last significant characters read. */
-	private int fLast;
-	/** The amount of characters already read on first call to nextToken(). */
-	private int fPrefixLength;
-
 	
 	/** The scanner. */
 	private final BufferedDocumentScanner fScanner= new BufferedDocumentScanner(1000);	// faster implementation
 
+	private final IToken[] fTokens= new IToken[] {
+			new Token(null),
+			new Token(PY_COMMENT),
+			new Token(PY_SINGLELINE_STRING1),
+			new Token(PY_SINGLELINE_STRING2),
+			new Token(PY_MULTILINE_STRING1),
+			new Token(PY_MULTILINE_STRING2),
+			new Token(PY_BACKQUOTES),
+		};
+	private int fTokenOffset;
+	private int fTokenLength;
+	
 	
 	private static int getState(String contentType) {
 
@@ -53,46 +49,39 @@ public class FastPythonPartitionScanner implements IPartitionTokenScanner, IPyth
 			return PYTHON;
 
 		else if (contentType.equals(PY_COMMENT))
-			return SINGLE_LINE_COMMENT;
+			return COMMENT;
 
 		else if (contentType.equals(PY_SINGLELINE_STRING1))
-			return STRING;
+			return SINGLE_LINE_STRING1;
+		
+		else if (contentType.equals(PY_SINGLELINE_STRING2))
+			return SINGLE_LINE_STRING2;
 
 		else if (contentType.equals(PY_MULTILINE_STRING1))
-			return MULTI_LINE_STRING;
+			return MULTI_LINE_STRING1;
+		
+		else if (contentType.equals(PY_MULTILINE_STRING2))
+			return MULTI_LINE_STRING2;
+		
+		else if (contentType.equals(PY_BACKQUOTES))
+			return BACKQUOTES;
 
-		else
+		else //DEFAULT
 			return PYTHON;
 	}
 
 	public void setPartialRange(IDocument document, int offset, int length, String contentType, int partitionOffset) {
 		fScanner.setRange(document, offset, length);
-		fTokenOffset= partitionOffset;
+		fTokenOffset= offset;
 		fTokenLength= 0;
-		fPrefixLength= offset - partitionOffset;
-		fLast= NONE;
-
-		if (offset == partitionOffset) {
-			// restart at beginning of partition
-			fState= PYTHON;
-		} else {
-			fState= getState(contentType);
-		}
 	}
 
 	public void setRange(IDocument document, int offset, int length) {
 		fScanner.setRange(document, offset, length);
-		fScanner.setRange(document, offset, length);
 		fTokenOffset= offset;
 		fTokenLength= 0;
-		fPrefixLength= 0;
-		fLast= NONE;
-		fState= PYTHON;
 	}
 
-	public IToken nextToken() {
-		return null;
-	}
 
 	public int getTokenOffset() {
 		return fTokenOffset;
@@ -101,5 +90,53 @@ public class FastPythonPartitionScanner implements IPartitionTokenScanner, IPyth
 	public int getTokenLength() {
 		return fTokenLength;
 	}
+
+	/*
+	 * @see org.eclipse.jface.text.rules.ITokenScanner#nextToken()
+	 */
+	public IToken nextToken() {
+		fTokenOffset += fTokenLength;
+		fTokenLength= 0;
+
+		while (true) {
+			final int ch= fScanner.read();
+
+			// characters
+	 		switch (ch) {
+	 		
+	 		case ICharacterScanner.EOF:
+	 			fTokenLength++;
+	 			return Token.EOF;
+	 			
+	 		case '#':
+	 			int offsetEnd = fTokenOffset;
+	 			int ch2 = fScanner.read();
+	 			offsetEnd++;
+	 			
+	 	        while(ch2!= '\n' && ch2 != '\r' && ch != ICharacterScanner.EOF){
+	 	        	offsetEnd++;
+	 	        	ch2 = fScanner.read();
+	 	        }
+	 	        fTokenLength = offsetEnd-fTokenOffset;
+	 	        return fTokens[COMMENT];
+	 	        
+	 		case '\'':
+		 		offsetEnd = fTokenOffset;
+		 		ch2 = fScanner.read();
+		 		offsetEnd++;
+		 		
+		 		while(ch2!= '\n' && ch2 != '\r' && ch != ICharacterScanner.EOF){
+		 			offsetEnd++;
+		 			ch2 = fScanner.read();
+		 		}
+		 		fTokenLength = offsetEnd-fTokenOffset;
+		 		return fTokens[COMMENT];
+	 	        
+	 		default:
+	 			fTokenLength++;
+	 			return fTokens[PYTHON];
+	 		}
+		}
+ 	}
 
 }
