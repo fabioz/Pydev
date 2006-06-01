950a6831f14cef9abf9045be47a3273ebff1fd2d
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2006-06-01 10:33:01 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@1210 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ org.python.pydev.core/src/org/python/pydev/core/performanceeval/Timer.java
@@ -0,0 +1,38 @@
+package org.python.pydev.core.performanceeval;
+
+/**
+ * This class is a helper in performance evaluation
+ */
+public class Timer {
+
+	private long start;
+
+	public Timer(){
+		this.start = System.currentTimeMillis();
+	}
+
+	public void printDiffMillis() {
+		System.out.println("Time Elapsed (millis):"+getDiff());
+	}
+	
+	public void printDiff() {
+		printDiff(null);
+	}
+
+	private long getDiff() {
+		long old = this.start;
+		long newStart = System.currentTimeMillis();
+		long diff = (newStart-old);
+		start = newStart;
+		return diff;
+	}
+
+	public void printDiff(String msg) {
+		double secs = getDiff()/1000.0d;
+		if(msg != null){
+			System.out.println("Time Elapsed for:"+msg+" (secs):"+secs);
+		}else{
+			System.out.println("Time Elapsed (secs):"+secs);
+		}
+	}
+}
