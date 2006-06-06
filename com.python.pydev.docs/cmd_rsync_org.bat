48d87a4645f53a6a930b662aff6f32bef75cb0d2
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-06-06 15:59:23 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@634 42a48d6c-1186-114d-bcb1-a7d39102e91d

---
+++ com.python.pydev.docs/cmd_rsync_org.bat
@@ -0,0 +1,70 @@
+@echo off
+REM *****************************************************************
+REM
+REM CWRSYNC.CMD - Batch file template to start your rsync command (s).
+REM
+REM By Tevfik K. (http://itefix.no)
+REM *****************************************************************
+
+REM ** CUSTOMIZE ** Specify where to find rsync and related files (C:\CWRSYNC)
+SET CWRSYNCHOME=C:\PROGRAM FILES\CWRSYNC
+
+REM Set CYGWIN variable to 'nontsec'. That makes sure that permissions
+REM on your windows machine are not updated as a side effect of cygwin
+REM operations.
+SET CYGWIN=nontsec
+
+REM Set HOME variable to your windows home directory. That makes sure 
+REM that ssh command creates known_hosts in a directory you have access.
+SET HOME=%HOMEDRIVE%%HOMEPATH%
+
+REM Make cwRsync home as a part of system PATH to find required DLLs
+SET CWOLDPATH=%PATH% 
+SET PATH=%PATH%;%CWRSYNCHOME%\BIN
+
+REM Windows paths may contain a colon (:) as a part of drive designation and 
+REM backslashes (example c:\, g:\). However, in rsync syntax, a colon in a 
+REM path means searching for a remote host. Solution: use absolute path 'a la unix', 
+REM replace backslashes (\) with slashes (/) and put -/cygdrive/- in front of the 
+REM drive letter:
+REM 
+REM Example : C:\WORK\* --> /cygdrive/c/work/*
+REM 
+REM Example 1 - rsync recursively to a unix server with an openssh server :
+REM
+REM       rsync -r /cygdrive/c/work/ remotehost:/home/user/work/
+REM
+REM Example 2 - Local rsync recursively 
+REM
+REM       rsync -r /cygdrive/c/work/ /cygdrive/d/work/doc/
+REM
+REM Example 3 - rsync to an rsync server recursively :
+REM    (Double colons?? YES!!)
+REM
+REM       rsync -r /cygdrive/c/doc/ remotehost::module/doc
+REM
+REM Rsync is a very powerful tool. Please look at documentation for other options. 
+REM
+
+REM ** CUSTOMIZE ** Enter your rsync command(s) here
+
+REM Copying open source stuff
+REM Copying open source homepage
+rsync --exclude "*.db" -r /cygdrive/e/eclipse_workspace/com.python.pydev.docs/open_source/final/ shell.sourceforge.net:/home/groups/p/py/pydev/htdocs/
+
+REM Copying open source features
+rsync --exclude "com*" -r /cygdrive/e/eclipse_workspace/org.python.pydev.site/features/ shell.sourceforge.net:/home/groups/p/py/pydev/htdocs/updates/features/
+
+REM Copying open source plugins
+rsync --exclude "com*" -r /cygdrive/e/eclipse_workspace/org.python.pydev.site/plugins/ shell.sourceforge.net:/home/groups/p/py/pydev/htdocs/updates/plugins/
+
+REM Copying open source plugins
+rsync /cygdrive/e/eclipse_workspace/org.python.pydev.site/site.xml shell.sourceforge.net:/home/groups/p/py/pydev/htdocs/updates/site.xml
+
+
+
+
+
+
+
+
