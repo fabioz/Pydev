e8eb48eb1b930c408514eea69508197f3e1435c6
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-06-27 00:20:19 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@677 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.docs/pydev_freemind.mm
+++ com.python.pydev.docs/pydev_freemind.mm
@@ -185,6 +185,9 @@
 <node COLOR="#00b439" CREATED="1140106715906" ID="Freemind_Link_1375402745" MODIFIED="1140106864265" TEXT="Make script to handle all the buld proccess:&#xa;- Creating build: OK&#xa;- Creating e-mail message:&#xa;- Uploading files to sourceforge (partial: only html)&#xa;- Uploading files to fabioz.com&#xa;- Updating site.xml&#xa;- Printing what else needs to be done in sf">
 <font NAME="SansSerif" SIZE="16"/>
 </node>
+<node COLOR="#00b439" CREATED="1151366922593" ID="Freemind_Link_1668151218" MODIFIED="1151366948453" TEXT="Check if Eclipse has spaces in its install path and warn the user about it.">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
 </node>
 </node>
 </map>
