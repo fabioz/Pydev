74628505d732fe1492ce99dab33f01c3f295d46b
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2006-07-15 18:36:37 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@697 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.analysis/src/com/python/pydev/analysis/CtxInsensitiveImportComplProposal.java
+++ com.python.pydev.analysis/src/com/python/pydev/analysis/CtxInsensitiveImportComplProposal.java
@@ -6,44 +6,47 @@ package com.python.pydev.analysis;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.python.pydev.core.docutils.PySelection;
 import org.python.pydev.editor.actions.PyAction;
-import org.python.pydev.editor.codecompletion.PyCompletionProposal;
+import org.python.pydev.editor.codecompletion.PyCompletionProposalExtension2;
 import org.python.pydev.plugin.PydevPlugin;
 
-public class CtxInsensitiveImportComplProposal extends PyCompletionProposal{
+public class CtxInsensitiveImportComplProposal extends PyCompletionProposalExtension2{
 
     public String realImportRep;
 
     public CtxInsensitiveImportComplProposal(String replacementString, int replacementOffset, int replacementLength, int cursorPosition, Image image, String displayString, IContextInformation contextInformation, String additionalProposalInfo, int priority, 
             String realImportRep) {
-        super(replacementString, replacementOffset, replacementLength, cursorPosition, image, displayString, contextInformation, additionalProposalInfo, priority);
+        super(replacementString, replacementOffset, replacementLength, cursorPosition, image, displayString, contextInformation, additionalProposalInfo, priority, ON_APPLY_DEFAUL, "");
         this.realImportRep = realImportRep;
     }
     
-    @Override
-    public void apply(IDocument document) {
+    public void apply(ITextViewer viewer, char trigger, int stateMask, int offset) {
         try {
-        	PySelection selection = new PySelection(document);
-        	int lineToAddImport = selection.getLineAvailableForImport();
+            IDocument document = viewer.getDocument();
+            PySelection selection = new PySelection(document);
+            int lineToAddImport = selection.getLineAvailableForImport();
             String delimiter = PyAction.getDelimiter(document);
             
             //first do the completion
-            document.replace(fReplacementOffset, fReplacementLength, fReplacementString);
+            int dif = offset - fReplacementOffset;
+            document.replace(offset-dif, dif+this.fLen, fReplacementString);
             
             //then do the import 
             if(lineToAddImport >=0 && lineToAddImport <= document.getNumberOfLines()){
                 IRegion lineInformation = document.getLineInformation(lineToAddImport);
                 document.replace(lineInformation.getOffset(), 0, realImportRep+delimiter);
             }
-
+            
         } catch (BadLocationException x) {
             PydevPlugin.log(x);
         }
-    }
+    }    
+    
     
     @Override
     public Point getSelection(IDocument document) {
