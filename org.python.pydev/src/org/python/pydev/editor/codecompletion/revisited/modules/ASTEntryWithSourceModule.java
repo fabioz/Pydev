1f5e293d2e6e536b4b10e4cabce499fac7f30fa0
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2007-01-18 00:49:36 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@1549 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ org.python.pydev/src/org/python/pydev/editor/codecompletion/revisited/modules/ASTEntryWithSourceModule.java
@@ -0,0 +1,21 @@
+package org.python.pydev.editor.codecompletion.revisited.modules;
+
+import org.python.pydev.parser.visitors.scope.ASTEntry;
+
+/**
+ * Used for the creation of an ASTEntry that has a source module related
+ */
+public class ASTEntryWithSourceModule extends ASTEntry{
+
+    private SourceModule module;
+
+    public ASTEntryWithSourceModule(ASTEntry parent, SourceModule module) {
+        super(parent, module.getAst());
+        this.module = module;
+    }
+    
+    public SourceModule getModule(){
+        return module;
+    }
+
+}
