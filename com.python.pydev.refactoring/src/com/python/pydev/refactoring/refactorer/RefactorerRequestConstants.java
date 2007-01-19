69fd14f0e1d050e9862745373eca43fa521dc57b
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2007-01-19 23:27:29 +0000

    Minor
    git-svn-id: svn://localhost/trunk@805 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.refactoring/src/com/python/pydev/refactoring/refactorer/RefactorerRequestConstants.java
+++ com.python.pydev.refactoring/src/com/python/pydev/refactoring/refactorer/RefactorerRequestConstants.java
@@ -1,5 +1,10 @@
 package com.python.pydev.refactoring.refactorer;
 
+/**
+ * Class with the constants for which we have additional information for the refactoring request.
+ * 
+ * @author Fabio
+ */
 public class RefactorerRequestConstants {
     public static final String FIND_DEFINITION_IN_ADDITIONAL_INFO = "findDefinitionInAdditionalInfo";
     public static final String FIND_REFERENCES_ONLY_IN_LOCAL_SCOPE = "findReferencesOnlyOnLocalScope";
