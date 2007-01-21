9df0aff1d0c5721f2e07d2b277947b5fa4abdaf1
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2007-01-21 18:26:23 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@816 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.refactoring/src/com/python/pydev/refactoring/refactorer/RefactorerFindDefinition.java
+++ com.python.pydev.refactoring/src/com/python/pydev/refactoring/refactorer/RefactorerFindDefinition.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CancellationException;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.SubProgressMonitor;
 import org.python.pydev.core.FindInfo;
 import org.python.pydev.core.FullRepIterable;
 import org.python.pydev.core.ICodeCompletionASTManager;
@@ -57,122 +60,136 @@ public class RefactorerFindDefinition {
     public ItemPointer[] findDefinition(RefactoringRequest request) {
         //ok, let's find the definition.
         //1. we have to know what we're looking for (activationToken)
-        request.communicateWork("Finding Definition");
-        List<ItemPointer> pointers = new ArrayList<ItemPointer>();
-        String[] tokenAndQual = PySelection.getActivationTokenAndQual(request.getDoc(), request.ps.getAbsoluteCursorOffset(), true);
-        
-        String modName = null;
-        
-        //all that to try to give the user a 'default' interpreter manager, for whatever he is trying to search
-        //if it is in some pythonpath, that's easy, but if it is some file somewhere else in the computer, this
-        //might turn out a little tricky.
-        if(request.nature == null){
-            //the request is not associated to any project. It is probably a system file. So, let's check it...
-            Tuple<SystemPythonNature,String> infoForFile = PydevPlugin.getInfoForFile(request.file);
-            if(infoForFile != null){
-                modName = infoForFile.o2;
-                request.nature = infoForFile.o1;
-                request.inputName = modName;
-            }else{
-                return new ItemPointer[0];
-            }
-        }
-        
-        
-        if(modName == null){
-            modName = request.resolveModule();
-        }
-        
-        if(request.nature == null){
-            PydevPlugin.logInfo("Unable to resolve nature for find definition request (python or jython interpreter may not be configured).");
-            return new ItemPointer[0];
-        }
-        
-        //check if it is already initialized....
-        try {
-            if(request.nature.isPython()){
-                if(!PydevPlugin.isPythonInterpreterInitialized()){
-                    PydevPlugin.logInfo("Python interpreter manager not initialized.");
+        request.pushMonitor(new SubProgressMonitor(request.getMonitor(), 50));
+        request.getMonitor().beginTask("Find definition", 5);
+        try{
+            request.communicateWork("Finding Definition");
+            List<ItemPointer> pointers = new ArrayList<ItemPointer>();
+            String[] tokenAndQual = PySelection.getActivationTokenAndQual(request.getDoc(), request.ps.getAbsoluteCursorOffset(), true);
+            
+            String modName = null;
+            
+            //all that to try to give the user a 'default' interpreter manager, for whatever he is trying to search
+            //if it is in some pythonpath, that's easy, but if it is some file somewhere else in the computer, this
+            //might turn out a little tricky.
+            if(request.nature == null){
+                //the request is not associated to any project. It is probably a system file. So, let's check it...
+                Tuple<SystemPythonNature,String> infoForFile = PydevPlugin.getInfoForFile(request.file);
+                if(infoForFile != null){
+                    modName = infoForFile.o2;
+                    request.nature = infoForFile.o1;
+                    request.inputName = modName;
+                }else{
                     return new ItemPointer[0];
                 }
-            }else if(request.nature.isJython()){
-                if(!PydevPlugin.isJythonInterpreterInitialized()){
-                    PydevPlugin.logInfo("Jython interpreter manager not initialized.");
-                    return new ItemPointer[0];
-                }
-            }else{
-                throw new RuntimeException("Project is neither python nor jython?");
             }
-        } catch (CoreException e1) {
-            throw new RuntimeException(e1);
-        }
-        //end check if it is already initialized....
-        
-        IModule mod = request.getModule();
-        if(mod == null){
-            PydevPlugin.logInfo("Unable to resolve module for find definition request.");
-            return new ItemPointer[0];
-        }
-
-        if(modName == null){
-            if(mod.getName() == null){
-                if(mod instanceof SourceModule){
-                    SourceModule m = (SourceModule) mod;
-                    modName = "__module_not_in_the_pythonpath__";
-                    m.setName(modName);
-                }
-            }
+            
+            
             if(modName == null){
-                PydevPlugin.logInfo("Unable to resolve module for find definition request (modName == null).");
+                modName = request.resolveModule();
+            }
+            
+            if(request.nature == null){
+                PydevPlugin.logInfo("Unable to resolve nature for find definition request (python or jython interpreter may not be configured).");
                 return new ItemPointer[0];
             }
-        }
-        
-        request.communicateWork("Module name found:"+modName);
-        
-        
-        String tok = tokenAndQual[0] + tokenAndQual[1];
-        List<FindInfo> lFindInfo = new ArrayList<FindInfo>();
-        try {
-            //2. check findDefinition (SourceModule)
-            ArrayList<IDefinition> selected = new ArrayList<IDefinition>();
             
-            IDefinition[] definitions = mod.findDefinition(CompletionState.getEmptyCompletionState(tok, request.nature), request.getBeginLine(), request.getBeginCol(), request.nature, lFindInfo);
-            for (IDefinition definition : definitions) {
-                boolean doAdd = true;
-                if(definition instanceof Definition){
-                    Definition d = (Definition) definition;
-                    doAdd = !findActualTokenFromImportFromDefinition(request, tok, lFindInfo, selected, d);
+            //check if it is already initialized....
+            try {
+                if(request.nature.isPython()){
+                    if(!PydevPlugin.isPythonInterpreterInitialized()){
+                        PydevPlugin.logInfo("Python interpreter manager not initialized.");
+                        return new ItemPointer[0];
+                    }
+                }else if(request.nature.isJython()){
+                    if(!PydevPlugin.isJythonInterpreterInitialized()){
+                        PydevPlugin.logInfo("Jython interpreter manager not initialized.");
+                        return new ItemPointer[0];
+                    }
+                }else{
+                    throw new RuntimeException("Project is neither python nor jython?");
                 }
+            } catch (CoreException e1) {
+                throw new RuntimeException(e1);
+            }
+            //end check if it is already initialized....
+            
+            IModule mod = request.getModule();
+            if(mod == null){
+                PydevPlugin.logInfo("Unable to resolve module for find definition request.");
+                return new ItemPointer[0];
+            }
+    
+            if(modName == null){
+                if(mod.getName() == null){
+                    if(mod instanceof SourceModule){
+                        SourceModule m = (SourceModule) mod;
+                        modName = "__module_not_in_the_pythonpath__";
+                        m.setName(modName);
+                    }
+                }
+                if(modName == null){
+                    PydevPlugin.logInfo("Unable to resolve module for find definition request (modName == null).");
+                    return new ItemPointer[0];
+                }
+            }
+            
+            request.communicateWork("Module name found:"+modName);
+            
+            
+            String tok = tokenAndQual[0] + tokenAndQual[1];
+            List<FindInfo> lFindInfo = new ArrayList<FindInfo>();
+            try {
+                //2. check findDefinition (SourceModule)
+                ArrayList<IDefinition> selected = new ArrayList<IDefinition>();
                 
-                if(doAdd){
-                    selected.add(definition);
+                IDefinition[] definitions = mod.findDefinition(CompletionState.getEmptyCompletionState(tok, request.nature), request.getBeginLine(), request.getBeginCol(), request.nature, lFindInfo);
+                request.communicateWork("Found:"+definitions.length+ " definitions");
+                for (IDefinition definition : definitions) {
+                    boolean doAdd = true;
+                    if(definition instanceof Definition){
+                        Definition d = (Definition) definition;
+                        doAdd = !findActualTokenFromImportFromDefinition(request, tok, lFindInfo, selected, d);
+                    }
+                    request.checkCancelled();
+                    if(doAdd){
+                        selected.add(definition);
+                    }
+                }
+                AnalysisPlugin.getAsPointers(pointers, selected.toArray(new Definition[0]));
+            } catch (CancellationException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            
+            if(pointers.size() == 0 && ((Boolean)request.getAdditionalInfo(RefactorerRequestConstants.FIND_DEFINITION_IN_ADDITIONAL_INFO, true))){
+                String lookForInterface = tokenAndQual[1];
+                List<IInfo> tokensEqualTo = AdditionalProjectInterpreterInfo.getTokensEqualTo(lookForInterface, request.nature,
+                        AbstractAdditionalInterpreterInfo.TOP_LEVEL | AbstractAdditionalInterpreterInfo.INNER);
+                
+                ICodeCompletionASTManager manager = request.nature.getAstManager();
+                if (tokensEqualTo.size() > 100){
+                    //too many matches for that...
+                    throw new TooManyMatchesException("Too Many matches ("+tokensEqualTo.size()+") were found for the requested token:"+lookForInterface, tokensEqualTo.size());
+                }
+                request.communicateWork(StringUtils.format("Found: %s possible matches.", tokensEqualTo.size()));
+                IPythonNature nature = request.nature;
+                for (IInfo info : tokensEqualTo) {
+                    AnalysisPlugin.getDefinitionFromIInfo(pointers, manager, nature, info);
+                    request.checkCancelled();
                 }
             }
-            AnalysisPlugin.getAsPointers(pointers, selected.toArray(new Definition[0]));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        
-        if(pointers.size() == 0 && ((Boolean)request.getAdditionalInfo(RefactorerRequestConstants.FIND_DEFINITION_IN_ADDITIONAL_INFO, true))){
-            String lookForInterface = tokenAndQual[1];
-            List<IInfo> tokensEqualTo = AdditionalProjectInterpreterInfo.getTokensEqualTo(lookForInterface, request.nature,
-                    AbstractAdditionalInterpreterInfo.TOP_LEVEL | AbstractAdditionalInterpreterInfo.INNER);
+            request.communicateWork(StringUtils.format("Found: %s matches.", pointers.size()));
             
-            ICodeCompletionASTManager manager = request.nature.getAstManager();
-            if (tokensEqualTo.size() > 100){
-                //too many matches for that...
-                throw new TooManyMatchesException("Too Many matches ("+tokensEqualTo.size()+") were found for the requested token:"+lookForInterface, tokensEqualTo.size());
-            }
-            request.communicateWork(StringUtils.format("Found: %s possible matches.", tokensEqualTo.size()));
-            IPythonNature nature = request.nature;
-            for (IInfo info : tokensEqualTo) {
-                AnalysisPlugin.getDefinitionFromIInfo(pointers, manager, nature, info);
-            }
+            return pointers.toArray(new ItemPointer[0]);
+        }catch(OperationCanceledException e){
+            //that's ok... it was cancelled
+            return null;
+        }finally{
+            request.getMonitor().done();
+            request.popMonitor();
         }
-        request.communicateWork(StringUtils.format("Found: %s matches.", pointers.size()));
-        
-        return pointers.toArray(new ItemPointer[0]);
     }
     
     /** 
