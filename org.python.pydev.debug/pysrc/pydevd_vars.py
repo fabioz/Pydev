708957b85ec7795d1ad03a936609afe6b6fa3d9a
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2007-02-08 14:36:52 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@1605 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev.debug/pysrc/pydevd_vars.py
+++ org.python.pydev.debug/pysrc/pydevd_vars.py
@@ -257,15 +257,16 @@ def changeAttrExpression( thread_id, frame_id, attr, expression ):
     
     try:
         expression = expression.replace('@LINE@', '\n')
-        if hasattr(frame, 'savelocals'):
-            if attr in frame.f_locals:
-                frame.f_locals[attr] = eval(expression, frame.f_globals, frame.f_locals)
-                frame.savelocals()
-                return
-                
-            elif attr in frame.f_globals:
-                frame.f_globals[attr] = eval(expression, frame.f_globals, frame.f_locals)
-                return
+#tests (needs proposed patch in python accepted)
+#        if hasattr(frame, 'savelocals'):
+#            if attr in frame.f_locals:
+#                frame.f_locals[attr] = eval(expression, frame.f_globals, frame.f_locals)
+#                frame.savelocals()
+#                return
+#                
+#            elif attr in frame.f_globals:
+#                frame.f_globals[attr] = eval(expression, frame.f_globals, frame.f_locals)
+#                return
             
         #default way (only works for changing it in the topmost frame)
         exec '%s=%s' % (attr, expression) in frame.f_globals, frame.f_locals
