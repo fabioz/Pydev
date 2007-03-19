620ed37d0aac74e515fc244f601a5a7711173c1b
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2007-03-19 16:14:59 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@1721 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ org.python.pydev/src_navigator/org/python/pydev/navigator/filters/ImportsFilter.java
@@ -0,0 +1,23 @@
+package org.python.pydev.navigator.filters;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.python.pydev.navigator.PythonNode;
+import org.python.pydev.parser.jython.SimpleNode;
+import org.python.pydev.parser.visitors.NodeUtils;
+
+public class ImportsFilter extends ViewerFilter{
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if(element instanceof PythonNode){
+			PythonNode node = (PythonNode) element;
+			SimpleNode n = node.entry.astThis.node;
+			if(NodeUtils.isImport(n)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
