1e191c7f3332b7624101f117e0fcb2c64190cff4
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2007-04-05 19:08:37 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/trunk@1756 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/src_completions/org/python/pydev/editor/codecompletion/revisited/CompletionStateFactory.java
+++ org.python.pydev/src_completions/org/python/pydev/editor/codecompletion/revisited/CompletionStateFactory.java
@@ -18,5 +18,14 @@ public class CompletionStateFactory {
     public static ICompletionState getEmptyCompletionState(String token, IPythonNature nature) {
         return new CompletionState(-1,-1,token, nature,"");
     }
+    
+    /**
+     * @param line: start at 0
+     * @param col: start at 0
+     * @return a default completion state for globals (act token defined)
+     */
+    public static ICompletionState getEmptyCompletionState(String token, IPythonNature nature, int line, int col) {
+    	return new CompletionState(line, col,token, nature,"");
+    }
 
 }
