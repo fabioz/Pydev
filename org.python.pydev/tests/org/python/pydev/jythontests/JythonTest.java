2c32f482ce1d36b446bc3b419820cd4aa243da71
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2007-05-27 13:25:14 +0000

    Fails if error in unittest structure fails.
    git-svn-id: svn://localhost/trunk@1812 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/tests/org/python/pydev/jythontests/JythonTest.java
+++ org.python.pydev/tests/org/python/pydev/jythontests/JythonTest.java
@@ -5,11 +5,13 @@ package org.python.pydev.jythontests;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.List;
 
 import org.python.pydev.core.TestDependent;
+import org.python.pydev.jython.IPythonInterpreter;
 import org.python.pydev.jython.JythonPlugin;
 
 import junit.framework.TestCase;
@@ -32,8 +34,26 @@ public class JythonTest extends TestCase {
     }
     
     public void testJythonTestsOnPydevPlugin() throws Exception {
+        //asserts will make the test fail
         List<Throwable> errors = JythonPlugin.execAll(null, "test", JythonPlugin.newPythonInterpreter(false), 
                 new File[]{new File(TestDependent.TEST_PYDEV_PLUGIN_LOC+"tests/jysrc/tests")});
+        failIfErrorsRaised(errors, null); //errors will always arrive through exceptions and not through the err stream
+    }
+    
+    public void testJythonTestsOnDebugPlugin() throws Exception {
+        //unittest.TestCase format: the __main__ is required in the global namespace 
+        HashMap<String, Object> locals = new HashMap<String, Object>();
+        locals.put("__name__", "__main__");
+        IPythonInterpreter interpreter = JythonPlugin.newPythonInterpreter(false);
+        ByteArrayOutputStream stdErr = new ByteArrayOutputStream();
+        interpreter.setErr(stdErr);
+        
+        List<Throwable> errors = JythonPlugin.execAll(locals, "test", interpreter, 
+                new File[]{new File(TestDependent.TEST_PYDEV_DEBUG_PLUGIN_LOC+"pysrc/tests")});
+        failIfErrorsRaised(errors, stdErr);
+    }
+
+    private void failIfErrorsRaised(List<Throwable> errors, ByteArrayOutputStream stdErr) throws IOException {
         if(errors.size() > 0){
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             out.write("There have been errors while executing the test scripts in jython.\n\n".getBytes());
@@ -42,20 +62,13 @@ public class JythonTest extends TestCase {
             }
             fail(new String(out.toByteArray()));
         }
-    }
-    
-    public void testJythonTestsOnDebugPlugin() throws Exception {
-        HashMap<String, Object> locals = new HashMap<String, Object>();
-        locals.put("__name__", "__main__");
-        List<Throwable> errors = JythonPlugin.execAll(locals, "test", JythonPlugin.newPythonInterpreter(false), 
-                new File[]{new File(TestDependent.TEST_PYDEV_DEBUG_PLUGIN_LOC+"pysrc/tests")});
-        if(errors.size() > 0){
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            out.write("There have been errors while executing the test scripts in jython.\n\n".getBytes());
-            for (Throwable throwable : errors) {
-                throwable.printStackTrace(new PrintStream(out));
+        if(stdErr != null){
+            String errStr = new String(stdErr.toByteArray());
+            if(errStr.length() > 0){
+                if(errStr.indexOf("FAILED") != -1){
+                    fail(errStr);
+                }
             }
-            fail(new String(out.toByteArray()));
         }
     }
 
