02d4cd3def10cb4c0d71d4fd709c23500467ef37
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2008-01-06 12:25:46 +0000

    1st working implementation of code-completion for java classes in JDT projects.
    git-svn-id: svn://localhost/trunk@1029 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.runalltests/src/com/python/pydev/runalltests2/AllWorkbenchTests.java
+++ com.python.pydev.runalltests/src/com/python/pydev/runalltests2/AllWorkbenchTests.java
@@ -3,6 +3,7 @@ package com.python.pydev.runalltests2;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.python.pydev.editor.codecompletion.revisited.javaintegration.JavaClassModuleTestWorkbench;
 import org.python.pydev.editor.codecompletion.revisited.jython.JythonCompletionWithBuiltinsTestWorkbench;
 import org.python.pydev.editor.codecompletion.revisited.jython.JythonFindDefinitionTestWorkbench;
 
@@ -12,7 +13,7 @@ public class AllWorkbenchTests {
         TestSuite suite = new TestSuite(AllWorkbenchTests.class.getName());
         suite.addTestSuite(JythonCompletionWithBuiltinsTestWorkbench.class);
         suite.addTestSuite(JythonFindDefinitionTestWorkbench.class);
-//        suite.addTestSuite(JavaClassModuleTestWorkbench.class); //removed for now
+        suite.addTestSuite(JavaClassModuleTestWorkbench.class); 
         if (suite.countTestCases() == 0) {
             throw new Error("There are no test cases to run");
         } else {
