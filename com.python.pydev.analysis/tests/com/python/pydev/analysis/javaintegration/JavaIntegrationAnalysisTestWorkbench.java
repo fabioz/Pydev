c1120d74281a92b87fd9211c4f77c02b789df83f
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2008-01-10 21:12:52 +0000

    Code-completion working with the java packages (and not only for java classes).
    git-svn-id: svn://localhost/trunk@1031 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.analysis/tests/com/python/pydev/analysis/javaintegration/JavaIntegrationAnalysisTestWorkbench.java
+++ com.python.pydev.analysis/tests/com/python/pydev/analysis/javaintegration/JavaIntegrationAnalysisTestWorkbench.java
@@ -1,5 +1,6 @@
 package com.python.pydev.analysis.javaintegration;
 
+import org.eclipse.core.runtime.CoreException;
 import org.python.pydev.editor.codecompletion.revisited.javaintegration.AbstractJavaIntegrationTestWorkbench;
 
 public class JavaIntegrationAnalysisTestWorkbench extends AbstractJavaIntegrationTestWorkbench {
@@ -11,7 +12,10 @@ public class JavaIntegrationAnalysisTestWorkbench extends AbstractJavaIntegratio
      */
     public void testJavaClassModule() throws Throwable {
         try{
-            //case 1: try it with the analysis of a java class
+            //case 1: try find definition for java classes
+            checkCase1();
+            
+            //case 2: try context-insensitive code completion
             checkCase1();
             
 //            goToManual();
@@ -22,8 +26,16 @@ public class JavaIntegrationAnalysisTestWorkbench extends AbstractJavaIntegratio
         }
     }
 
-    public void checkCase1() {
-//        goToManual();
+    public void checkCase1() throws CoreException {
+        String mod1Contents = "from javamod1 import javamod2\njavamod2.JavaClass2";
+        setFileContents(mod1Contents);
+        //TODO: Do F3 on JavaClass2
+    }
+    
+    public void checkCase2() throws CoreException {
+        String mod1Contents = "JavaClas";
+        setFileContents(mod1Contents);
+        //TODO: See if JavaClass and JavaClass2 are there (check full package name)
     }
 
 
