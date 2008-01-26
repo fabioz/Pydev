9bcf4c1541982277a189e1f7c9647d8fe20abdc8
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2008-01-26 18:38:12 +0000

    Applied pydev coding std.
    git-svn-id: svn://localhost/trunk@2029 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev.debug/src/org/python/pydev/debug/core/PydevDebugPlugin.java
+++ org.python.pydev.debug/src/org/python/pydev/debug/core/PydevDebugPlugin.java
@@ -24,81 +24,83 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.python.pydev.core.bundle.ImageCache;
 import org.python.pydev.debug.unittest.ITestRunListener;
+
 /**
  * The main plugin for Python Debugger.
  * 
  * Standard plugin functionality: preferences, logging, some utility functions
  */
 public class PydevDebugPlugin extends AbstractUIPlugin {
-	//The shared instance.
-	private static PydevDebugPlugin plugin;
-	
-	public ImageCache imageCache;
+    //The shared instance.
+    private static PydevDebugPlugin plugin;
 
-	/** Listener list **/
+    public ImageCache imageCache;
+
+    /** Listener list **/
     private List listeners = new ArrayList();
 
-	public PydevDebugPlugin() {
-		plugin = this;
-	}
+    public PydevDebugPlugin() {
+        plugin = this;
+    }
 
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		imageCache = new ImageCache(PydevDebugPlugin.getDefault().getBundle().getEntry("/"));		
-	}
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        imageCache = new ImageCache(PydevDebugPlugin.getDefault().getBundle().getEntry("/"));
+    }
 
-	public static PydevDebugPlugin getDefault() {
-		return plugin;
-	}
-	
-	public static String getPluginID() {
-		return getDefault().getBundle().getSymbolicName();
-	}
-	
-	public static IWorkspace getWorkspace() {
-		return ResourcesPlugin.getWorkspace();
-	}
-	
-	public static ImageCache getImageCache() {
-		return plugin.imageCache;
-	}
-	
-	protected void initializeDefaultPluginPreferences() {
-		PydevDebugPrefs.initializeDefaultPreferences(getPluginPreferences());
-	}
+    public static PydevDebugPlugin getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * Returns the active workbench window or <code>null</code> if none
-	 */
-	public static IWorkbenchWindow getActiveWorkbenchWindow() {
-		return getDefault().getWorkbench().getActiveWorkbenchWindow();
-	}
-	
-	public static Status makeStatus(int errorLevel, String message, Throwable e) {
-		return new Status(errorLevel, getPluginID(), errorLevel, message, e);
-	}
-	/**
-	 * @param errorLevel  IStatus.[OK|INFO|WARNING|ERROR]
-	 */
-	public static void log(int errorLevel, String message, Throwable e) {
-		Status s = makeStatus(errorLevel, message, e);
-		getDefault().getLog().log(s);
-	}
-	
-	public static void errorDialog(final String message, final Throwable t) {
-		Display disp = Display.getDefault();
-		disp.asyncExec(new Runnable() {
-			public void run() {
-				IWorkbenchWindow window = getDefault().getWorkbench().getActiveWorkbenchWindow();
-				Shell shell = window == null ? null : window.getShell();
-				if (shell != null) {
-					IStatus status= makeStatus(IStatus.ERROR, "Error logged from Pydev Debug: ", t);	
-					ErrorDialog.openError(shell, "Its an error", message, status);
-				}
-			}	
-		});
-		log(IStatus.ERROR, message, t);
-	}
+    public static String getPluginID() {
+        return getDefault().getBundle().getSymbolicName();
+    }
+
+    public static IWorkspace getWorkspace() {
+        return ResourcesPlugin.getWorkspace();
+    }
+
+    public static ImageCache getImageCache() {
+        return plugin.imageCache;
+    }
+
+    protected void initializeDefaultPluginPreferences() {
+        PydevDebugPrefs.initializeDefaultPreferences(getPluginPreferences());
+    }
+
+    /**
+     * Returns the active workbench window or <code>null</code> if none
+     */
+    public static IWorkbenchWindow getActiveWorkbenchWindow() {
+        return getDefault().getWorkbench().getActiveWorkbenchWindow();
+    }
+
+    public static Status makeStatus(int errorLevel, String message, Throwable e) {
+        return new Status(errorLevel, getPluginID(), errorLevel, message, e);
+    }
+
+    /**
+     * @param errorLevel  IStatus.[OK|INFO|WARNING|ERROR]
+     */
+    public static void log(int errorLevel, String message, Throwable e) {
+        Status s = makeStatus(errorLevel, message, e);
+        getDefault().getLog().log(s);
+    }
+
+    public static void errorDialog(final String message, final Throwable t) {
+        Display disp = Display.getDefault();
+        disp.asyncExec(new Runnable() {
+            public void run() {
+                IWorkbenchWindow window = getDefault().getWorkbench().getActiveWorkbenchWindow();
+                Shell shell = window == null ? null : window.getShell();
+                if (shell != null) {
+                    IStatus status = makeStatus(IStatus.ERROR, "Error logged from Pydev Debug: ", t);
+                    ErrorDialog.openError(shell, "Its an error", message, status);
+                }
+            }
+        });
+        log(IStatus.ERROR, message, t);
+    }
 
     /**
      * 
@@ -106,24 +108,21 @@ public class PydevDebugPlugin extends AbstractUIPlugin {
      * 
      * @throws CoreException
      */
-    public static File getScriptWithinPySrc(String targetExec)
-            throws CoreException {
-    
-        IPath relative = new Path("pysrc").addTrailingSeparator().append(
-                targetExec);
-    
+    public static File getScriptWithinPySrc(String targetExec) throws CoreException {
+
+        IPath relative = new Path("pysrc").addTrailingSeparator().append(targetExec);
+
         Bundle bundle = getDefault().getBundle();
-    
+
         URL bundleURL = Platform.find(bundle, relative);
         URL fileURL;
         try {
             fileURL = Platform.asLocalURL(bundleURL);
             File f = new File(fileURL.getPath());
-    
+
             return f;
         } catch (IOException e) {
-            throw new CoreException(makeStatus(IStatus.ERROR,
-                    "Can't find python debug script", null));
+            throw new CoreException(makeStatus(IStatus.ERROR, "Can't find python debug script", null));
         }
     }
 
@@ -133,70 +132,69 @@ public class PydevDebugPlugin extends AbstractUIPlugin {
      * 
      * @throws CoreException
      */
-    public static File getPySrcPath()
-            throws CoreException {
-    
+    public static File getPySrcPath() throws CoreException {
+
         IPath relative = new Path("pysrc");
-    
+
         Bundle bundle = getDefault().getBundle();
-    
+
         URL bundleURL = Platform.find(bundle, relative);
         URL fileURL;
         try {
             fileURL = Platform.asLocalURL(bundleURL);
             File f = new File(fileURL.getPath());
-    
+
             return f;
         } catch (IOException e) {
-            throw new CoreException(makeStatus(IStatus.ERROR,
-                    "Can't find python debug script", null));
+            throw new CoreException(makeStatus(IStatus.ERROR, "Can't find python debug script", null));
         }
     }
-	public void addTestListener(ITestRunListener listener) {
-		listeners.add(listener);
-	}
-	
-	public void removeTestListener(ITestRunListener listener) {
-		listeners.remove(listener);
-	}
 
-	public List getListeners() {
-		return listeners;
-	}
-	
-	public void fireTestsStarted(int count, String path_to_file) {
-		for (Iterator all=getListeners().iterator(); all.hasNext();) {
-			ITestRunListener each = (ITestRunListener) all.next();
-			each.testsStarted(count, path_to_file);
-		}
-	}
+    public void addTestListener(ITestRunListener listener) {
+        listeners.add(listener);
+    }
 
-	public void fireTestsFinished(String summary) {
-		for (Iterator all=getListeners().iterator(); all.hasNext();) {
-			ITestRunListener each = (ITestRunListener) all.next();
-			each.testsFinished(summary);
-		}
-	}
+    public void removeTestListener(ITestRunListener listener) {
+        listeners.remove(listener);
+    }
 
-	public void fireTestStarted(String klass, String methodName) {
-		for (Iterator all=getListeners().iterator(); all.hasNext();) {
-			ITestRunListener each = (ITestRunListener) all.next();
-			each.testStarted(klass, methodName);
-		}
-	}
+    public List getListeners() {
+        return listeners;
+    }
 
-	public void fireTestOK(String klass, String methodName) {
-		for (Iterator all=getListeners().iterator(); all.hasNext();) {
-			ITestRunListener each = (ITestRunListener) all.next();
-			each.testOK(klass, methodName);
-		}
-	}
+    public void fireTestsStarted(int count, String path_to_file) {
+        for (Iterator all = getListeners().iterator(); all.hasNext();) {
+            ITestRunListener each = (ITestRunListener) all.next();
+            each.testsStarted(count, path_to_file);
+        }
+    }
 
-	public void fireTestFailed(String klass, String methodName, String failureType, String trace) {
-		for (Iterator all=getListeners().iterator(); all.hasNext();) {
-			ITestRunListener each = (ITestRunListener) all.next();
-			each.testFailed(klass, methodName, failureType, trace);
-		}
-	}
+    public void fireTestsFinished(String summary) {
+        for (Iterator all = getListeners().iterator(); all.hasNext();) {
+            ITestRunListener each = (ITestRunListener) all.next();
+            each.testsFinished(summary);
+        }
+    }
+
+    public void fireTestStarted(String klass, String methodName) {
+        for (Iterator all = getListeners().iterator(); all.hasNext();) {
+            ITestRunListener each = (ITestRunListener) all.next();
+            each.testStarted(klass, methodName);
+        }
+    }
+
+    public void fireTestOK(String klass, String methodName) {
+        for (Iterator all = getListeners().iterator(); all.hasNext();) {
+            ITestRunListener each = (ITestRunListener) all.next();
+            each.testOK(klass, methodName);
+        }
+    }
+
+    public void fireTestFailed(String klass, String methodName, String failureType, String trace) {
+        for (Iterator all = getListeners().iterator(); all.hasNext();) {
+            ITestRunListener each = (ITestRunListener) all.next();
+            each.testFailed(klass, methodName, failureType, trace);
+        }
+    }
 
 }
