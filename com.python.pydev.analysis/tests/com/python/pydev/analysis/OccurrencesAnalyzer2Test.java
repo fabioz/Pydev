64ef75befbb83db054bd81e6c65a5c4afed91efc
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2008-02-02 22:29:50 +0000

    Bugfix: __getattribute__ was shadowing __getattr__ (as it was always found in the builtins for some object).
    git-svn-id: svn://localhost/trunk@1050 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.analysis/tests/com/python/pydev/analysis/OccurrencesAnalyzer2Test.java
+++ com.python.pydev.analysis/tests/com/python/pydev/analysis/OccurrencesAnalyzer2Test.java
@@ -14,7 +14,7 @@ public class OccurrencesAnalyzer2Test extends AnalysisTestsBase {
         try {
             OccurrencesAnalyzer2Test analyzer2 = new OccurrencesAnalyzer2Test();
             analyzer2.setUp();
-            analyzer2.testNoEffectInGenExp();
+            analyzer2.testErrorNotShownOnDynamicClass6();
             analyzer2.tearDown();
             System.out.println("finished");
             
@@ -86,6 +86,14 @@ public class OccurrencesAnalyzer2Test extends AnalysisTestsBase {
         doc = new Document(
                 "from extendable.noerr import importer\n"+
                 "print importer.Struct.bar\n"
+        );
+        checkNoError();
+    }
+    
+    public void testErrorNotShownOnDynamicClass6() {
+        doc = new Document(
+                "from extendable.noerr.importer import WithGetAttr2\n"+
+                "print WithGetAttr2.anything\n"
         );
         checkNoError();
     }
