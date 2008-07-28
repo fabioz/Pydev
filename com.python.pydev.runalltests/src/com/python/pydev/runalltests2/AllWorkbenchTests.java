09694775a6697230ed1508b5bb4f3325937fe5a6
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2008-07-28 17:00:40 +0000

    Fixed NPE when creating editor with no interpreter configured. https://sourceforge.net/tracker/index.php?func=detail&aid=2026280&group_id=85796&atid=577329
    git-svn-id: svn://localhost/trunk@1141 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- com.python.pydev.runalltests/src/com/python/pydev/runalltests2/AllWorkbenchTests.java
+++ com.python.pydev.runalltests/src/com/python/pydev/runalltests2/AllWorkbenchTests.java
@@ -6,6 +6,7 @@ import junit.framework.TestSuite;
 import org.python.pydev.editor.codecompletion.revisited.javaintegration.JavaClassModuleTestWorkbench;
 import org.python.pydev.editor.codecompletion.revisited.jython.JythonCompletionWithBuiltinsTestWorkbench;
 import org.python.pydev.editor.codecompletion.revisited.jython.JythonFindDefinitionTestWorkbench;
+import org.python.pydev.plugin.nature.SaveFileWithoutNatureTestWorkbench;
 
 import com.python.pydev.codecompletion.JavaIntegrationPydevComTestWorkbench;
 
@@ -13,10 +14,15 @@ public class AllWorkbenchTests {
 
     public static Test suite() {
         TestSuite suite = new TestSuite(AllWorkbenchTests.class.getName());
+        
+        //Must be 1st (no nature or interpreter configured)
+        suite.addTestSuite(SaveFileWithoutNatureTestWorkbench.class); 
+        
         suite.addTestSuite(JythonCompletionWithBuiltinsTestWorkbench.class);
         suite.addTestSuite(JythonFindDefinitionTestWorkbench.class);
         suite.addTestSuite(JavaClassModuleTestWorkbench.class); 
         suite.addTestSuite(JavaIntegrationPydevComTestWorkbench.class); 
+        
         if (suite.countTestCases() == 0) {
             throw new Error("There are no test cases to run");
         } else {
