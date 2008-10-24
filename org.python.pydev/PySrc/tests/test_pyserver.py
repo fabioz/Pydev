9d1829af52bf25824822100101d084642abf7940
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2008-10-24 16:27:32 +0000

    Should not run in jython
    git-svn-id: svn://localhost/trunk@2355 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev/PySrc/tests/test_pyserver.py
+++ org.python.pydev/PySrc/tests/test_pyserver.py
@@ -1,7 +1,6 @@
 '''
 @author Fabio Zadrozny 
 '''
-
 import sys
 import os
 
@@ -11,172 +10,175 @@ sys.argv[0] = os.path.dirname(sys.argv[0])
 #twice the dirname to get the previous level from this file.
 sys.path.insert(1, os.path.join(  os.path.dirname( sys.argv[0] )) )
 
-try:
-    import inspect
+if sys.platform.find('java') == -1:
+    
+    
+    try:
+        import inspect
+        import pycompletionserver
+        import socket
+        import urllib
+    except ImportError:
+        pass #Not available in jython
+    
     import unittest
-    import pycompletionserver
-    import socket
-    import urllib
-except ImportError:
-    pass #Not available in jython
-
-
-class Test(unittest.TestCase):
-
-    def setUp(self):
-        unittest.TestCase.setUp(self)
-
-    def tearDown(self):
-        unittest.TestCase.tearDown(self)
     
-    def testMessage(self):
-        t = pycompletionserver.T(0,0)
-        
-        l = []
-        l.append(('Def','description'  , 'args'))
-        l.append(('Def1','description1', 'args1'))
-        l.append(('Def2','description2', 'args2'))
-        
-        msg = t.formatCompletionMessage(None,l)
-        self.assertEquals('@@COMPLETIONS(None,(Def,description,args),(Def1,description1,args1),(Def2,description2,args2))END@@', msg)
-        
-        l = []
-        l.append(('Def','desc,,r,,i()ption',''  ))
-        l.append(('Def(1','descriptio(n1',''))
-        l.append(('De,f)2','de,s,c,ription2',''))
-        msg = t.formatCompletionMessage(None,l)
-        self.assertEquals('@@COMPLETIONS(None,(Def,desc%2C%2Cr%2C%2Ci%28%29ption, ),(Def%281,descriptio%28n1, ),(De%2Cf%292,de%2Cs%2Cc%2Cription2, ))END@@', msg)
-
-    def createConnections(self, p1 = 50002,p2 = 50003):
-        '''
-        Creates the connections needed for testing.
-        '''
-        t = pycompletionserver.T(p1,p2)
-        
-        t.start()
-
-        sToWrite = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sToWrite.connect((pycompletionserver.HOST, p1))
-        
-        sToRead = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sToRead.bind((pycompletionserver.HOST, p2))
-        sToRead.listen(1) #socket to receive messages.
-
-        connToRead, addr = sToRead.accept()
-
-        return t, sToWrite, sToRead, connToRead, addr
-        
-
-    def readMsg(self):
-        finish = False
-        msg = ''
-        while finish == False:
-            m = self.connToRead.recv(1024*4)
-            
-            if m.startswith('@@PROCESSING'):
-                print 'Status msg:', msg
-            else:
-                msg += m
-
-            if msg.find('END@@') != -1:
-                finish = True
-
-        return msg
-
-    def testCompletionSocketsAndMessages(self):
-        t, sToWrite, sToRead, self.connToRead, addr = self.createConnections()
-        
-        try:
-            #now that we have the connections all set up, check the code completion messages.
-            msg = urllib.quote_plus('math')
-            sToWrite.send('@@IMPORTS:%sEND@@'%msg) #math completions
-            completions = self.readMsg()
-            #print urllib.unquote_plus(completions)
-            
-            #math is a builtin and because of that, it starts with None as a file
-            start = '@@COMPLETIONS(None,(__doc__,'
-            self.assert_(completions.startswith(start), '%s DOESNT START WITH %s' % ( completions, start) )
+    class Test(unittest.TestCase):
     
-            self.assert_('@@COMPLETIONS' in completions)
-            self.assert_('END@@' in completions)
-
-
-            #now, test search
-            msg = urllib.quote_plus('inspect.ismodule')
-            sToWrite.send('@@SEARCH%sEND@@'%msg) #math completions
-            found = self.readMsg()
-            self.assert_('inspect.py' in found)
-            self.assert_('33' in found or '34' in found)
-
-            #now, test search
-            msg = urllib.quote_plus('inspect.CO_NEWLOCALS')
-            sToWrite.send('@@SEARCH%sEND@@'%msg) #math completions
-            found = self.readMsg()
-            self.assert_('inspect.py' in found)
-            self.assert_('CO_NEWLOCALS' in found)
+        def setUp(self):
+            unittest.TestCase.setUp(self)
+    
+        def tearDown(self):
+            unittest.TestCase.tearDown(self)
+        
+        def testMessage(self):
+            t = pycompletionserver.T(0,0)
             
-            #now, test search
-            msg = urllib.quote_plus('inspect.BlockFinder.tokeneater')
-            sToWrite.send('@@SEARCH%sEND@@'%msg) 
-            found = self.readMsg()
-            self.assert_('inspect.py' in found)
-#            self.assert_('CO_NEWLOCALS' in found)
-
-        #reload modules test
-#        sToWrite.send('@@RELOAD_MODULES_END@@')
-#        ok = self.readMsg()
-#        self.assertEquals('@@MSG_OK_END@@' , ok)
-#        this test is not executed because it breaks our current enviroment.
-        
-        
-        finally:
-            try:
-                print 'succedded...sending kill msg'
-                self.sendKillMsg(sToWrite)
+            l = []
+            l.append(('Def','description'  , 'args'))
+            l.append(('Def1','description1', 'args1'))
+            l.append(('Def2','description2', 'args2'))
+            
+            msg = t.formatCompletionMessage(None,l)
+            self.assertEquals('@@COMPLETIONS(None,(Def,description,args),(Def1,description1,args1),(Def2,description2,args2))END@@', msg)
+            
+            l = []
+            l.append(('Def','desc,,r,,i()ption',''  ))
+            l.append(('Def(1','descriptio(n1',''))
+            l.append(('De,f)2','de,s,c,ription2',''))
+            msg = t.formatCompletionMessage(None,l)
+            self.assertEquals('@@COMPLETIONS(None,(Def,desc%2C%2Cr%2C%2Ci%28%29ption, ),(Def%281,descriptio%28n1, ),(De%2Cf%292,de%2Cs%2Cc%2Cription2, ))END@@', msg)
+    
+        def createConnections(self, p1 = 50002,p2 = 50003):
+            '''
+            Creates the connections needed for testing.
+            '''
+            t = pycompletionserver.T(p1,p2)
+            
+            t.start()
+    
+            sToWrite = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sToWrite.connect((pycompletionserver.HOST, p1))
+            
+            sToRead = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sToRead.bind((pycompletionserver.HOST, p2))
+            sToRead.listen(1) #socket to receive messages.
+    
+            connToRead, addr = sToRead.accept()
+    
+            return t, sToWrite, sToRead, connToRead, addr
+            
+    
+        def readMsg(self):
+            finish = False
+            msg = ''
+            while finish == False:
+                m = self.connToRead.recv(1024*4)
                 
-        
-#                while not hasattr(t, 'ended'):
-#                    pass #wait until it receives the message and quits.
-        
-                    
-                sToRead.close()
-                sToWrite.close()
-                self.connToRead.close()
-            except:
-                pass
-        
-    def sendKillMsg(self, socket):
-        socket.send(pycompletionserver.MSG_KILL_SERVER)
-        
+                if m.startswith('@@PROCESSING'):
+                    print 'Status msg:', msg
+                else:
+                    msg += m
     
-#    def testRefactoringSocketsAndMessages(self):
-#        t, sToWrite, sToRead, self.connToRead, addr = self.createConnections(50002+2,50003+2)
-#
-#        import refactoring
-#        from test_refactoring import delete, createFile, FILE, getInitialFile, getRenameRefactored
-#        try:
-#            createFile(FILE, getInitialFile())
-#            
-#            msg = urllib.quote_plus('@@BIKEfindDefinition %s %s %sEND@@'%(FILE, 7+1, 4))
-#            sToWrite.send(msg) 
-#            result = self.readMsg()
-#            self.assert_('BIKE_OK:' in result)
-#    
-#            msg = urllib.quote_plus('@@BIKErenameByCoordinates %s %s %s %sEND@@'%(FILE, 1+1, 6, 'G'))
-#            sToWrite.send(msg) 
-#            result = self.readMsg()
-#            self.assert_('BIKE_OK:' in result)
-#    
-#            self.sendKillMsg(sToWrite)
-#            
-#    
-#            while not hasattr(t, 'ended'):
-#                pass #wait until it receives the message and quits.
-#        finally:
-#            
-#            sToRead.close()
-#            sToWrite.close()
-#            self.connToRead.close()
+                if msg.find('END@@') != -1:
+                    finish = True
+    
+            return msg
+    
+        def testCompletionSocketsAndMessages(self):
+            t, sToWrite, sToRead, self.connToRead, addr = self.createConnections()
+            
+            try:
+                #now that we have the connections all set up, check the code completion messages.
+                msg = urllib.quote_plus('math')
+                sToWrite.send('@@IMPORTS:%sEND@@'%msg) #math completions
+                completions = self.readMsg()
+                #print urllib.unquote_plus(completions)
+                
+                #math is a builtin and because of that, it starts with None as a file
+                start = '@@COMPLETIONS(None,(__doc__,'
+                self.assert_(completions.startswith(start), '%s DOESNT START WITH %s' % ( completions, start) )
+        
+                self.assert_('@@COMPLETIONS' in completions)
+                self.assert_('END@@' in completions)
+    
+    
+                #now, test search
+                msg = urllib.quote_plus('inspect.ismodule')
+                sToWrite.send('@@SEARCH%sEND@@'%msg) #math completions
+                found = self.readMsg()
+                self.assert_('inspect.py' in found)
+                self.assert_('33' in found or '34' in found)
+    
+                #now, test search
+                msg = urllib.quote_plus('inspect.CO_NEWLOCALS')
+                sToWrite.send('@@SEARCH%sEND@@'%msg) #math completions
+                found = self.readMsg()
+                self.assert_('inspect.py' in found)
+                self.assert_('CO_NEWLOCALS' in found)
+                
+                #now, test search
+                msg = urllib.quote_plus('inspect.BlockFinder.tokeneater')
+                sToWrite.send('@@SEARCH%sEND@@'%msg) 
+                found = self.readMsg()
+                self.assert_('inspect.py' in found)
+    #            self.assert_('CO_NEWLOCALS' in found)
+    
+            #reload modules test
+    #        sToWrite.send('@@RELOAD_MODULES_END@@')
+    #        ok = self.readMsg()
+    #        self.assertEquals('@@MSG_OK_END@@' , ok)
+    #        this test is not executed because it breaks our current enviroment.
+            
+            
+            finally:
+                try:
+                    print 'succedded...sending kill msg'
+                    self.sendKillMsg(sToWrite)
+                    
+            
+    #                while not hasattr(t, 'ended'):
+    #                    pass #wait until it receives the message and quits.
+            
+                        
+                    sToRead.close()
+                    sToWrite.close()
+                    self.connToRead.close()
+                except:
+                    pass
+            
+        def sendKillMsg(self, socket):
+            socket.send(pycompletionserver.MSG_KILL_SERVER)
+            
+        
+    #    def testRefactoringSocketsAndMessages(self):
+    #        t, sToWrite, sToRead, self.connToRead, addr = self.createConnections(50002+2,50003+2)
+    #
+    #        import refactoring
+    #        from test_refactoring import delete, createFile, FILE, getInitialFile, getRenameRefactored
+    #        try:
+    #            createFile(FILE, getInitialFile())
+    #            
+    #            msg = urllib.quote_plus('@@BIKEfindDefinition %s %s %sEND@@'%(FILE, 7+1, 4))
+    #            sToWrite.send(msg) 
+    #            result = self.readMsg()
+    #            self.assert_('BIKE_OK:' in result)
+    #    
+    #            msg = urllib.quote_plus('@@BIKErenameByCoordinates %s %s %s %sEND@@'%(FILE, 1+1, 6, 'G'))
+    #            sToWrite.send(msg) 
+    #            result = self.readMsg()
+    #            self.assert_('BIKE_OK:' in result)
+    #    
+    #            self.sendKillMsg(sToWrite)
+    #            
+    #    
+    #            while not hasattr(t, 'ended'):
+    #                pass #wait until it receives the message and quits.
+    #        finally:
+    #            
+    #            sToRead.close()
+    #            sToWrite.close()
+    #            self.connToRead.close()
 
         
 if __name__ == '__main__':
--- org.python.pydev/PySrc/tests/test_simpleTipper.py
+++ org.python.pydev/PySrc/tests/test_simpleTipper.py
@@ -9,131 +9,133 @@ sys.argv[0] = os.path.dirname(sys.argv[0])
 #twice the dirname to get the previous level from this file.
 sys.path.insert(1, os.path.join(  os.path.dirname( sys.argv[0] )) )
 
-HAS_WX = False
-
-import unittest
-try:
-    import importsTipper
-    import inspect
-except ImportError:
-    pass #Not available in jython
-
-class Test(unittest.TestCase):
-
-    def p(self, t):
-        for a in t:
-            print a
- 
-    def testImports3(self):
-        tip = importsTipper.GenerateTip('os')
-        ret = self.assertIn('path', tip)
-        self.assertEquals('', ret[2])
-
-    def testImports2(self):
-        try:
-            tip = importsTipper.GenerateTip('OpenGL.GLUT')
-            self.assertIn('glutDisplayFunc', tip)
-            self.assertIn('glutInitDisplayMode', tip)
-        except RuntimeError, e:
-            if not 'Unable to import module' in str(e):
-                raise
-
-    def testImports4(self):
-        try:
-            tip = importsTipper.GenerateTip('mx.DateTime.mxDateTime.mxDateTime')
-            self.assertIn('now', tip)
-        except RuntimeError, e:
-            if not 'Unable to import module' in str(e):
-                raise
-        
-    def testImports2a(self):
-        tips = importsTipper.GenerateTip('__builtin__.RuntimeError')
-        self.assertIn('__doc__', tips)
-        
-    def testImports(self):
-        '''
-        You can print the results to check...
-        '''
-        if HAS_WX:
-            tip = importsTipper.GenerateTip('wxPython.wx')
-            self.assertIn('wxApp'        , tip)
-            
-            tip = importsTipper.GenerateTip('wxPython.wx.wxApp')
-            
+if sys.platform.find('java') == -1:
+    
+    HAS_WX = False
+    
+    import unittest
+    try:
+        import importsTipper
+        import inspect
+    except ImportError:
+        pass #Not available in jython
+    
+    class Test(unittest.TestCase):
+    
+        def p(self, t):
+            for a in t:
+                print a
+     
+        def testImports3(self):
+            tip = importsTipper.GenerateTip('os')
+            ret = self.assertIn('path', tip)
+            self.assertEquals('', ret[2])
+    
+        def testImports2(self):
             try:
-                tip = importsTipper.GenerateTip('qt')
-                self.assertIn('QWidget'        , tip)
-                self.assertIn('QDialog'        , tip)
-                
-                tip = importsTipper.GenerateTip('qt.QWidget')
-                self.assertIn('rect'           , tip)
-                self.assertIn('rect'           , tip)
-                self.assertIn('AltButton'      , tip)
-        
-                tip = importsTipper.GenerateTip('qt.QWidget.AltButton')
-                self.assertIn('__xor__'      , tip)
-        
-                tip = importsTipper.GenerateTip('qt.QWidget.AltButton.__xor__')
-                self.assertIn('__class__'      , tip)
+                tip = importsTipper.GenerateTip('OpenGL.GLUT')
+                self.assertIn('glutDisplayFunc', tip)
+                self.assertIn('glutInitDisplayMode', tip)
+            except RuntimeError, e:
+                if not 'Unable to import module' in str(e):
+                    raise
+    
+        def testImports4(self):
+            try:
+                tip = importsTipper.GenerateTip('mx.DateTime.mxDateTime.mxDateTime')
+                self.assertIn('now', tip)
             except RuntimeError, e:
                 if not 'Unable to import module' in str(e):
                     raise
             
-        tip = importsTipper.GenerateTip('__builtin__')
-#        for t in tip[1]:
-#            print t
-        self.assertIn('object'         , tip)
-        self.assertIn('tuple'          , tip)
-        self.assertIn('RuntimeError'   , tip)
-        self.assertIn('RuntimeWarning' , tip)
-        t = self.assertIn('cmp' , tip)
-        self.assertEqual('(x, y)', t[2]) #args
-        
-        t = self.assertIn('isinstance' , tip)
-        self.assertEqual('(object, class_or_type_or_tuple)', t[2]) #args
-        
-        t = self.assertIn('compile' , tip)
-        self.assertEqual('(source, filename, mode)', t[2]) #args
-        
-        t = self.assertIn('setattr' , tip)
-        self.assertEqual('(object, name, value)', t[2]) #args
-        
-        tip = importsTipper.GenerateTip('compiler') 
-        self.assertArgs('parse', '(buf, mode)', tip)
-        self.assertArgs('walk', '(tree, visitor, walker, verbose)', tip)
-        self.assertIn('parse'          , tip)
-        self.assertIn('parseFile'      , tip)
-        
-    def assertArgs(self, tok, args, tips):
-        for a in tips[1]:
-            if tok == a[0]:
-                self.assertEquals(args, a[2])
-                return
-        raise AssertionError('%s not in %s', tok, tips)
-
-    def assertIn(self, tok, tips):
-        for a in tips[1]:
-            if tok == a[0]:
-                return a
-        raise AssertionError('%s not in %s' %(tok, tips))
-
-    def testInspect(self):
-        
-        class C(object):
-            def metA(self, a, b):
-                pass
-        
-        obj = C.metA
-        if inspect.ismethod (obj):
-            pass
-#            print obj.im_func
-#            print inspect.getargspec(obj.im_func)
+        def testImports2a(self):
+            tips = importsTipper.GenerateTip('__builtin__.RuntimeError')
+            self.assertIn('__doc__', tips)
             
-        
-def suite():
-    s = unittest.TestSuite()
-    s.addTest(Test("testImports2"))
-    unittest.TextTestRunner(verbosity=2).run(s)
+        def testImports(self):
+            '''
+            You can print the results to check...
+            '''
+            if HAS_WX:
+                tip = importsTipper.GenerateTip('wxPython.wx')
+                self.assertIn('wxApp'        , tip)
+                
+                tip = importsTipper.GenerateTip('wxPython.wx.wxApp')
+                
+                try:
+                    tip = importsTipper.GenerateTip('qt')
+                    self.assertIn('QWidget'        , tip)
+                    self.assertIn('QDialog'        , tip)
+                    
+                    tip = importsTipper.GenerateTip('qt.QWidget')
+                    self.assertIn('rect'           , tip)
+                    self.assertIn('rect'           , tip)
+                    self.assertIn('AltButton'      , tip)
+            
+                    tip = importsTipper.GenerateTip('qt.QWidget.AltButton')
+                    self.assertIn('__xor__'      , tip)
+            
+                    tip = importsTipper.GenerateTip('qt.QWidget.AltButton.__xor__')
+                    self.assertIn('__class__'      , tip)
+                except RuntimeError, e:
+                    if not 'Unable to import module' in str(e):
+                        raise
+                
+            tip = importsTipper.GenerateTip('__builtin__')
+    #        for t in tip[1]:
+    #            print t
+            self.assertIn('object'         , tip)
+            self.assertIn('tuple'          , tip)
+            self.assertIn('RuntimeError'   , tip)
+            self.assertIn('RuntimeWarning' , tip)
+            t = self.assertIn('cmp' , tip)
+            self.assertEqual('(x, y)', t[2]) #args
+            
+            t = self.assertIn('isinstance' , tip)
+            self.assertEqual('(object, class_or_type_or_tuple)', t[2]) #args
+            
+            t = self.assertIn('compile' , tip)
+            self.assertEqual('(source, filename, mode)', t[2]) #args
+            
+            t = self.assertIn('setattr' , tip)
+            self.assertEqual('(object, name, value)', t[2]) #args
+            
+            tip = importsTipper.GenerateTip('compiler') 
+            self.assertArgs('parse', '(buf, mode)', tip)
+            self.assertArgs('walk', '(tree, visitor, walker, verbose)', tip)
+            self.assertIn('parse'          , tip)
+            self.assertIn('parseFile'      , tip)
+            
+        def assertArgs(self, tok, args, tips):
+            for a in tips[1]:
+                if tok == a[0]:
+                    self.assertEquals(args, a[2])
+                    return
+            raise AssertionError('%s not in %s', tok, tips)
+    
+        def assertIn(self, tok, tips):
+            for a in tips[1]:
+                if tok == a[0]:
+                    return a
+            raise AssertionError('%s not in %s' %(tok, tips))
+    
+        def testInspect(self):
+            
+            class C(object):
+                def metA(self, a, b):
+                    pass
+            
+            obj = C.metA
+            if inspect.ismethod (obj):
+                pass
+    #            print obj.im_func
+    #            print inspect.getargspec(obj.im_func)
+                
+            
+    def suite():
+        s = unittest.TestSuite()
+        s.addTest(Test("testImports2"))
+        unittest.TextTestRunner(verbosity=2).run(s)
 
 if __name__ == '__main__':
     if sys.platform.find('java') == -1:
