1b39fc67eaa233a251ef353fe00dbc8609b88410
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2008-12-14 11:28:00 +0000

    Refactoring TreeBuilder structure.
    git-svn-id: svn://localhost/trunk@2429 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- org.python.pydev.parser/src/org/python/pydev/parser/grammar24/TreeBuilder24.java
+++ org.python.pydev.parser/src/org/python/pydev/parser/grammar24/TreeBuilder24.java
@@ -5,12 +5,12 @@ import java.util.Collections;
 import java.util.Iterator;
 
 import org.python.pydev.parser.grammarcommon.AbstractTreeBuilder;
+import org.python.pydev.parser.grammarcommon.CtxVisitor;
 import org.python.pydev.parser.grammarcommon.ITreeBuilder;
 import org.python.pydev.parser.grammarcommon.ITreeConstants;
 import org.python.pydev.parser.grammarcommon.IdentityNode;
 import org.python.pydev.parser.jython.ParseException;
 import org.python.pydev.parser.jython.SimpleNode;
-import org.python.pydev.parser.jython.Visitor;
 import org.python.pydev.parser.jython.ast.Assert;
 import org.python.pydev.parser.jython.ast.Assign;
 import org.python.pydev.parser.jython.ast.Attribute;
@@ -65,7 +65,6 @@ import org.python.pydev.parser.jython.ast.comprehensionType;
 import org.python.pydev.parser.jython.ast.decoratorsType;
 import org.python.pydev.parser.jython.ast.excepthandlerType;
 import org.python.pydev.parser.jython.ast.exprType;
-import org.python.pydev.parser.jython.ast.expr_contextType;
 import org.python.pydev.parser.jython.ast.keywordType;
 import org.python.pydev.parser.jython.ast.sliceType;
 import org.python.pydev.parser.jython.ast.stmtType;
@@ -1059,91 +1058,5 @@ class ExtraArgValue extends SimpleNode {
     }
     public int getId() {
         return id;
-    }
-}
-
-
-
-class CtxVisitor extends Visitor {
-    private int ctx;
-
-    public CtxVisitor() { }
-
-    public void setParam(SimpleNode node) throws Exception {
-        this.ctx = expr_contextType.Param;
-        visit(node);
-    }
-
-    public void setStore(SimpleNode node) throws Exception {
-        this.ctx = expr_contextType.Store;
-        visit(node);
-    }
-
-    public void setStore(SimpleNode[] nodes) throws Exception {
-        for (int i = 0; i < nodes.length; i++) 
-            setStore(nodes[i]);
-    }
-
-    public void setDelete(SimpleNode node) throws Exception {
-        this.ctx = expr_contextType.Del;
-        visit(node);
-    }
-
-    public void setDelete(SimpleNode[] nodes) throws Exception {
-        for (int i = 0; i < nodes.length; i++) 
-            setDelete(nodes[i]);
-    }
-
-    public void setAugStore(SimpleNode node) throws Exception {
-        this.ctx = expr_contextType.AugStore;
-        visit(node);
-    }
-
-    public Object visitName(Name node) throws Exception {
-        node.ctx = ctx;
-        return null;
-    }
-
-    public Object visitAttribute(Attribute node) throws Exception {
-        node.ctx = ctx;
-        return null;
-    }
-
-    public Object visitSubscript(Subscript node) throws Exception {
-        node.ctx = ctx;
-        return null;
-    }
-
-    public Object visitList(List node) throws Exception {
-        if (ctx == expr_contextType.AugStore) {
-            throw new ParseException(
-                    "augmented assign to list not possible", node);
-        }
-        node.ctx = ctx;
-        traverse(node);
-        return null;
-    }
-
-    public Object visitTuple(Tuple node) throws Exception {
-        if (ctx == expr_contextType.AugStore) {
-            throw new ParseException(
-                    "augmented assign to tuple not possible", node);
-        }
-        node.ctx = ctx;
-        traverse(node);
-        return null;
-    }
-
-    public Object visitCall(Call node) throws Exception {
-        throw new ParseException("can't assign to function call", node);
-    }
-
-    public Object visitListComp(Call node) throws Exception {
-        throw new ParseException("can't assign to list comprehension call",
-                                 node);
-    }
-
-    public Object unhandled_node(SimpleNode node) throws Exception {
-        throw new ParseException("can't assign to operator", node);
     }
 }
---
+++ org.python.pydev.parser/src/org/python/pydev/parser/grammarcommon/CtxVisitor.java
@@ -0,0 +1,108 @@
+package org.python.pydev.parser.grammarcommon;
+
+import org.python.pydev.core.docutils.StringUtils;
+import org.python.pydev.parser.jython.ParseException;
+import org.python.pydev.parser.jython.SimpleNode;
+import org.python.pydev.parser.jython.Visitor;
+import org.python.pydev.parser.jython.ast.Attribute;
+import org.python.pydev.parser.jython.ast.Call;
+import org.python.pydev.parser.jython.ast.List;
+import org.python.pydev.parser.jython.ast.Name;
+import org.python.pydev.parser.jython.ast.Subscript;
+import org.python.pydev.parser.jython.ast.Tuple;
+import org.python.pydev.parser.jython.ast.expr_contextType;
+
+public class CtxVisitor extends Visitor {
+    
+    private int ctx;
+
+    public CtxVisitor() { }
+
+    public void setParam(SimpleNode node) throws Exception {
+        this.ctx = expr_contextType.Param;
+        visit(node);
+    }
+    
+    public void setKwOnlyParam(SimpleNode node) throws Exception {
+        this.ctx = expr_contextType.KwOnlyParam;
+        visit(node);
+    }
+
+    public void setStore(SimpleNode node) throws Exception {
+        this.ctx = expr_contextType.Store;
+        visit(node);
+    }
+
+    public void setStore(SimpleNode[] nodes) throws Exception {
+        for (int i = 0; i < nodes.length; i++) 
+            setStore(nodes[i]);
+    }
+
+    public void setDelete(SimpleNode node) throws Exception {
+        this.ctx = expr_contextType.Del;
+        visit(node);
+    }
+
+    public void setDelete(SimpleNode[] nodes) throws Exception {
+        for (int i = 0; i < nodes.length; i++) 
+            setDelete(nodes[i]);
+    }
+
+    public void setAugStore(SimpleNode node) throws Exception {
+        this.ctx = expr_contextType.AugStore;
+        visit(node);
+    }
+
+    public Object visitName(Name node) throws Exception {
+        if(ctx == expr_contextType.Store){
+            if(node.reserved){
+                throw new ParseException(StringUtils.format("Cannot assign value to %s (because it's a keyword)", node.id), node);
+            }
+        }
+        node.ctx = ctx;
+        return null;
+    }
+
+    public Object visitAttribute(Attribute node) throws Exception {
+        node.ctx = ctx;
+        return null;
+    }
+
+    public Object visitSubscript(Subscript node) throws Exception {
+        node.ctx = ctx;
+        return null;
+    }
+
+    public Object visitList(List node) throws Exception {
+        if (ctx == expr_contextType.AugStore) {
+            throw new ParseException(
+                    "augmented assign to list not possible", node);
+        }
+        node.ctx = ctx;
+        traverse(node);
+        return null;
+    }
+
+    public Object visitTuple(Tuple node) throws Exception {
+        if (ctx == expr_contextType.AugStore) {
+            throw new ParseException(
+                    "augmented assign to tuple not possible", node);
+        }
+        node.ctx = ctx;
+        traverse(node);
+        return null;
+    }
+
+    public Object visitCall(Call node) throws Exception {
+        throw new ParseException("can't assign to function call", node);
+    }
+
+    public Object visitListComp(Call node) throws Exception {
+        throw new ParseException("can't assign to list comprehension call",
+                                 node);
+    }
+
+    public Object unhandled_node(SimpleNode node) throws Exception {
+        throw new ParseException("can't assign to operator:"+node, node);
+    }
+}
