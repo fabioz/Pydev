961937fb0416177fe565071abe7d136c1611079e
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2009-02-22 19:17:22 +0000

    Handling EOF correctly in the grammar (was changing the document before -- which is not a good approach).
    git-svn-id: svn://localhost/trunk@2640 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

--- plugins/org.python.pydev.core/src/org/python/pydev/core/REF.java
+++ plugins/org.python.pydev.core/src/org/python/pydev/core/REF.java
@@ -25,6 +25,7 @@ import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.IllegalCharsetNameException;
@@ -102,18 +103,29 @@ public class REF {
     }
     
     
+    public static Object getAttrObj(Object o, String attr){
+        return getAttrObj(o, attr, false);
+    }
+    
     /**
      * @return the value of some attribute in the given object
      */
-    public static Object getAttrObj(Object o, String attr){
+    public static Object getAttrObj(Object o, String attr, boolean raiseExceptionIfNotAvailable){
         try {
             Field field = REF.getAttr(o, attr);
             if(field != null){
+                //get it even if it's not public!
+                if((field.getModifiers() & Modifier.PUBLIC) == 0){
+                    field.setAccessible(true);
+                }
                 Object obj = field.get(o);
                 return obj;
             }
         }catch (Exception e) {
             //ignore
+            if(raiseExceptionIfNotAvailable){
+                throw new RuntimeException(e);
+            }
         }
         return null;
     }
