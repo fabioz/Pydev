734f6af5d574652f71fbd4100192d273e5271363
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2009-04-01 20:05:17 +0000

    Making release
    git-svn-id: svn://localhost/trunk@1402 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- make_release.py
+++ make_release.py
@@ -19,22 +19,42 @@ def Make():
     pydev_pro_dir = base_dir+r'\pydev_pro'
     pydev_dir = base_dir+r'\pydev'
     
-    os.chdir(pydev_pro_dir)
+    initial_dir = os.getcwd()
+    try:
+        os.chdir(pydev_pro_dir)
+        
+        for d in (pydev_dir, pydev_pro_dir):
+        
+            Execute(['svn', 'revert', '-R', d])
+            remove_unversioned_files.RemoveFilesFrom(d)
+            Execute(['svn', 'up', '--non-interactive', '--force', d])
     
-    for d in (pydev_dir, pydev_pro_dir):
-    
-        Execute(['svn', 'revert', '-R', d])
-        remove_unversioned_files.RemoveFilesFrom(d)
-        Execute(['svn', 'up', '--non-interactive', '--force', d])
+            env = {}
+            env.update(os.environ)
+            
+            env['PATH'] = env['PATH']+os.pathsep+r'W:\eclipse_341_clean\plugins\org.apache.ant_1.7.0.v200803061910\bin'
+            env['JAVA_HOME'] = r'D:\bin\jdk_1_5_09'
+            
+            if d.endswith('pro'):
+                os.chdir(pydev_pro_dir+r'\builders\com.python.pydev.build')
+            else:
+                os.chdir(pydev_pro_dir+r'\builders\org.python.pydev.build')
+            
+            cmds = [
+                'ant',
+                 '-DbuildDirectory=W:/temp_buildDir/pydev',
+                 '-Dbaseos=win32',
+                 '-Dbasews=win32',
+                 '-Dbasearch=x86',
+                 '-Ddeploy.dir=w:/temp_deployDir/pydev',
+                 '-DcleanAfter=false',
+                 '-Dvanilla.eclipse=W:/eclipse_341_clean'
+            ]
+            Execute(cmds, env)
+    finally:
+        os.chdir(initial_dir)
 
-        env = {}
-        env['PATH'] = r'W:\eclipse_341_clean\plugins\org.apache.ant_1.7.0.v200803061910\bin'
-        env['JAVA_HOME'] = r'D:\bin\jdk_1_5_09'
         
-        Execute(['svn', 'up', d], env=env)
-        
-        cd pydev\builders\org.python.pydev.build
-        ant -DbuildDirectory=W:/temp_buildDir/pydev -Dbaseos=win32 -Dbasews=win32 -Dbasearch=x86 -Ddeploy.dir=w:/temp_deployDir/pydev -DcleanAfter=false -Dvanilla.eclipse=W:/eclipse_341_clean
 
 
 #=======================================================================================================================
