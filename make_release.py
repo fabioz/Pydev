44318f4191d1658d7a742f1a3ae57460b553a644
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2009-04-01 19:58:17 +0000

    Making release
    git-svn-id: svn://localhost/trunk@1400 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- make_release.py
+++ make_release.py
@@ -7,8 +7,8 @@ import os
 #=======================================================================================================================
 # Execute
 #=======================================================================================================================
-def Execute(cmds):
-    print 'Executing', ' '.join(cmds), ':', subprocess.call(cmds)
+def Execute(cmds, env=None):
+    print 'Executing', ' '.join(cmds), ':', subprocess.call(cmds, env=env)
 
 
 #=======================================================================================================================
@@ -21,14 +21,21 @@ def Make():
     
     os.chdir(pydev_pro_dir)
     
-    Execute(['svn', 'revert', '-R', pydev_dir])
-    Execute(['svn', 'revert', '-R', pydev_pro_dir])
+    for d n (pydev_dir, pydev_pro_dir):
     
-    remove_unversioned_files.RemoveFilesFrom(pydev_dir)
-    remove_unversioned_files.RemoveFilesFrom(pydev_pro_dir)
-    
-    Execute(['svn', 'up', pydev_dir])
-    Execute(['svn', 'up', pydev_pro_dir])
+        Execute(['svn', 'revert', '-R', d])
+        remove_unversioned_files.RemoveFilesFrom(d)
+        Execute(['svn', 'up', '--non-interactive', '--force', d])
+
+        env = {}
+        env['PATH'] = r'W:\eclipse_341_clean\plugins\org.apache.ant_1.7.0.v200803061910\bin'
+        env['JAVA_HOME'] = r'D:\bin\jdk_1_5_09'
+        
+        Execute(['svn', 'up', d], env=env)
+        
+        cd pydev\builders\org.python.pydev.build
+        ant -DbuildDirectory=W:/temp_buildDir/pydev -Dbaseos=win32 -Dbasews=win32 -Dbasearch=x86 -Ddeploy.dir=w:/temp_deployDir/pydev -DcleanAfter=false -Dvanilla.eclipse=W:/eclipse_341_clean
+
 
 #=======================================================================================================================
 # main
