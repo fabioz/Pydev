0ae0e19fff16e25fcae37326302bdff0ce6d843a
fabioz <fabioz@7f4d9e04-a92a-ab41-bea9-970b690ef4a7>
2009-05-11 16:49:49 +0000

    Added new template and added icons to distribution.
    git-svn-id: svn://localhost/trunk@2774 7f4d9e04-a92a-ab41-bea9-970b690ef4a7

---
+++ plugins/org.python.pydev.customizations/templates/google_app_engine/ask_login/asklogin.py
@@ -0,0 +1,29 @@
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainPage(webapp.RequestHandler):
+    
+    
+    def get(self):
+        user = users.get_current_user()
+
+        if user:
+            self.response.out.write(
+                'Hello %s <a href="%s">Sign out</a><br>Is administrator: %s' % 
+                (user.nickname(), users.create_logout_url("/"), users.is_current_user_admin())
+            )
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == "__main__":
+    main()
