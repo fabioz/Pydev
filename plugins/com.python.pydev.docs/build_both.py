81a9888fe085d52dc38be033979eb3461ed81c11
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2009-07-18 15:30:07 +0000

    Using rst to generate the homepage.
    git-svn-id: svn://localhost/trunk@1487 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- plugins/com.python.pydev.docs/build_both.py
+++ plugins/com.python.pydev.docs/build_both.py
@@ -13,7 +13,51 @@ else:
     LAST_VERSION_TAG = '1.4.7' #Not specified (let's leave one there)
 
 
+
+#=======================================================================================================================
+# BuildFromRst
+#=======================================================================================================================
+def BuildFromRst(source_filename):
+    print source_filename
+    import os
+    from docutils import core
+    
+    # dict of default settings to override (same as in the cmdline params, but as attribute names:
+    #   "--embed-stylesheet" => "embed_stylesheet"
+    settings_overrides = {}
+    
+    import os
+    # publish as html
+    ret = core.publish_file(
+        writer_name='html',
+        source_path=source_filename,
+        destination_path=os.tempnam(),
+        settings_overrides=settings_overrides,
+    )
+    
+    final = ret[ret.find('<body>')+6: ret.find('</body>')].strip()
+    if final.startswith('<div'):
+        final = final[final.find('\n'):]
+        final = final[:final.rfind('</div>')]
+        
+    f = open(source_filename.split('.')[0]+'.contents.htm', 'w')
+    print >> f, final
+    f.close()
+
+
+#=======================================================================================================================
+# GenerateRstInDir
+#=======================================================================================================================
+def GenerateRstInDir(d):
+    for f in os.listdir(d):
+        if f.endswith('.rst'):
+            BuildFromRst(f)
+    
+    
 if __name__ == '__main__':
+    os.chdir(os.path.join(this_script_dir, 'open_source'))
+    GenerateRstInDir('.')
+    
     d1 = 'open_source/scripts/'
     d2 = 'new_homepage/scripts/'
     
