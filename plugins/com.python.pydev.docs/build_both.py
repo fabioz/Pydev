e3d4ec264bb1761216c680bf1c78612982815edb
fabioz <fabioz@42a48d6c-1186-114d-bcb1-a7d39102e91d>
2009-07-31 01:45:35 +0000

    git-svn-id: svn://localhost/trunk@1505 42a48d6c-1186-114d-bcb1-a7d39102e91d

--- plugins/com.python.pydev.docs/build_both.py
+++ plugins/com.python.pydev.docs/build_both.py
@@ -81,14 +81,14 @@ if __name__ == '__main__':
     sys.path.insert(0, os.path.join(this_script_dir, 'new_homepage', 'scripts'))
     sys.path.insert(0, '.')
     
-    os.chdir(os.path.join(this_script_dir, d1))
+    os.chdir(os.path.join(this_script_dir, 'open_source', 'scripts'))
     import build_org #@UnresolvedImport
     build_org.LAST_VERSION_TAG = LAST_VERSION_TAG
     os.chdir('..')
     build_org.DoIt()
     
     os.chdir('..')
-    os.chdir(os.path.join(this_script_dir, d2))
+    os.chdir(os.path.join(this_script_dir, 'new_homepage', 'scripts'))
     import build_com #@UnresolvedImport
     build_com.LAST_VERSION_TAG = LAST_VERSION_TAG
     build_com.DoIt()
