58f8255d245c1e0b3c0cc7607c873fcf74cb2bdc
Fabio <fabioz@aptana.com>
2009-08-31 21:40:49 -0300

    Removed licensing

--- plugins/com.python.pydev/src/com/python/pydev/LicenseType.java
+++
@@ -1,6 +0,0 @@
-package com.python.pydev;
-
-public class LicenseType {
-    public static final int TRIAL = 0;
-    public static final int NOT_TRIAL = 1;
-}
--- plugins/com.python.pydev/src/com/python/pydev/license/defaults/LicenseTypeDefaults.java
+++
@@ -1,6 +0,0 @@
-package com.python.pydev.license.defaults;
-
-public class LicenseTypeDefaults {
-    public static final int REGISTERED = 0;
-    public static final int TRIAL = 1;    
-}
