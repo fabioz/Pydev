fe8703c2bb123a7d68ee1de6afb0b5064a6d00cb
unknown <Fabio@.(none)>
2009-12-25 09:40:34 -0200

    Fixed line endings.

--- fix_new_lines.py
+++ fix_new_lines.py
@@ -4,7 +4,24 @@ for root, dirs, files in os.walk(os.curdir):
     for file in files:
         file = file.lower()
         #import pdb;pdb.set_trace()
-        if os.path.splitext(file)[1] in ('.py', '.pyw', '.java', '.xml', '.html', '.htm', '.txt', '.launch'):
+        if os.path.splitext(file)[1] in (
+            '.py', 
+            '.pyw', 
+            '.java', 
+            '.xml', 
+            '.html', 
+            '.htm', 
+            '.txt', 
+            '.launch', 
+            '.diff', 
+            '.jjt', 
+            '.jjt_template', 
+            '.project', 
+            '.pydevproject', 
+            '.css', 
+            '.exsd', 
+            '.mf'
+            ):
             path = os.path.join(root, file)
             contents = open(path, 'rb').read()
             if '\r' in contents:
