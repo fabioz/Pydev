55e18a29c4bd9077e27b2e0b73bcb67a5ee305ad
Leo Soto <leo.soto@gmail.com>
2010-02-14 01:15:15 -0300

    Build Django WAR package

---
+++ plugins/com.leosoto.bingo/src/com/leosoto/bingo/debug/ui/actions/DjangoWar.java
@@ -0,0 +1,46 @@
+package com.leosoto.bingo.debug.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.python.pydev.core.IInterpreterManager;
+import org.python.pydev.plugin.nature.PythonNature;
+import org.python.pydev.runners.SimpleRunner;
+import org.python.pydev.ui.actions.project.PyRemoveNature;
+
+import com.leosoto.bingo.debug.ui.launching.DjangoManagementRunner;
+
+public class DjangoWar extends PyRemoveNature{
+
+    public void run(IAction action) {
+    	try {
+	    	PythonNature nature = PythonNature.getPythonNature(selectedProject);
+	    	if (nature.getInterpreterType() != IInterpreterManager.INTERPRETER_TYPE_JYTHON) {
+	    		MessageDialog.openInformation(
+	    				null, "Can't create WAR", 
+                        "Creation of WAR packages is only supported on Jython");
+	    		return;
+	    	}
+	    	String projectPythonPath = 
+	    		nature.getPythonPathNature().getOnlyProjectPythonPathStr();
+	    	String javaLibs = null;
+	    	for (String path: projectPythonPath.split("\\|")) {
+	    		if (path.endsWith(".jar")) {
+	    			if (javaLibs == null) {
+	    				javaLibs = path;
+	    			} else { 
+		    			javaLibs += SimpleRunner.getPythonPathSeparator();
+		    			javaLibs += path;
+	    			}
+	    		}
+	    	}    	
+    		String command = "war";
+    		if (javaLibs != null) {
+    			command += " --include-java-libs=" + javaLibs;
+    		}
+    		DjangoManagementRunner.launch(selectedProject, command);
+    	} catch (Exception e) {
+    		throw new RuntimeException(e);
+    	}
+    }
+
+}
