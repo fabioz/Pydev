504cf89c8fc23de44dd814aaa02e6a8a1ef9fe96
fabioz <fabiofz@gmail.com>
2010-03-11 14:07:55 -0300

    Removing reloading code until it becomes more mature in the python side.

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyReloadCode.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyReloadCode.java
@@ -1,18 +1,11 @@
 package org.python.pydev.debug.model;
 
-import java.io.File;
 import java.util.ListResourceBundle;
 
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.text.IDocument;
-import org.python.pydev.core.IPythonNature;
-import org.python.pydev.core.MisconfigurationException;
-import org.python.pydev.debug.model.remote.ReloadCodeCommand;
 import org.python.pydev.editor.IPyEditListener;
 import org.python.pydev.editor.PyEdit;
-import org.python.pydev.plugin.PydevPlugin;
 
 public class PyReloadCode implements IPyEditListener{
 
@@ -24,24 +17,26 @@ public class PyReloadCode implements IPyEditListener{
 
     public void onSave(PyEdit edit, IProgressMonitor monitor){
         
-        File file = edit.getEditorFile();
-        if(file != null){
-            IAdaptable context = DebugUITools.getDebugContext();
-            if(context instanceof PyStackFrame){
-                PyStackFrame stackFrame = (PyStackFrame) context;
-                try{
-                    IPythonNature pythonNature = edit.getPythonNature();
-                    if(pythonNature != null){
-                        String moduleName = pythonNature.resolveModule(file);
-                        stackFrame.getTarget().postCommand(
-                                new ReloadCodeCommand(stackFrame.getTarget(), moduleName));
-                    }
-                }catch(MisconfigurationException e){
-                    PydevPlugin.log(e);
-                }
-                
-            }
-        }
+    	//Reloading code removed for now (still too experimental)
+    	
+//        File file = edit.getEditorFile();
+//        if(file != null){
+//            IAdaptable context = DebugUITools.getDebugContext();
+//            if(context instanceof PyStackFrame){
+//                PyStackFrame stackFrame = (PyStackFrame) context;
+//                try{
+//                    IPythonNature pythonNature = edit.getPythonNature();
+//                    if(pythonNature != null){
+//                        String moduleName = pythonNature.resolveModule(file);
+//                        stackFrame.getTarget().postCommand(
+//                                new ReloadCodeCommand(stackFrame.getTarget(), moduleName));
+//                    }
+//                }catch(MisconfigurationException e){
+//                    PydevPlugin.log(e);
+//                }
+//                
+//            }
+//        }
     }
 
     public void onSetDocument(IDocument document, PyEdit edit, IProgressMonitor monitor){
