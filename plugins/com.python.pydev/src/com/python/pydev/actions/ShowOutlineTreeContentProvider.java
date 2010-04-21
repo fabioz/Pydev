4016541a923d354e800bc16e4fb6d95fb9194566
fabioz <fabiofz@gmail.com>
2010-04-20 21:57:16 -0300

    Dealing with large files: performance improvements in the outline tree content provider.

--- plugins/com.python.pydev/src/com/python/pydev/actions/ShowOutlineTreeContentProvider.java
+++ plugins/com.python.pydev/src/com/python/pydev/actions/ShowOutlineTreeContentProvider.java
@@ -6,8 +6,10 @@ package com.python.pydev.actions;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.python.pydev.parser.jython.SimpleNode;
@@ -18,8 +20,13 @@ import org.python.pydev.parser.visitors.scope.DefinitionsASTIteratorVisitor;
 
 public final class ShowOutlineTreeContentProvider implements ITreeContentProvider {
     
-    private DefinitionsASTIteratorVisitor visitor;
-    private Map<Object, ASTEntry[]> cache = new HashMap<Object, ASTEntry[]>();
+    private List<ASTEntry> outline;
+    
+    private Map<SimpleNode, List<ASTEntry>> objectsWithParentStillNotDeterminedInOutline;
+    private final Map<Object, ASTEntry[]> cache = new HashMap<Object, ASTEntry[]>();
+	private Object[] elementsForCurrentInput;
+	private Object newInput;
+	private static final ASTEntry[] EMPTY = new ASTEntry[0];
 
     public Object[] getChildren(Object element) {
         Object[] ret = (Object[]) cache.get(element);
@@ -29,21 +36,17 @@ public final class ShowOutlineTreeContentProvider implements ITreeContentProvide
         
         ASTEntry entry = (ASTEntry) element;
         
-        if (entry.node instanceof ClassDef || entry.node instanceof FunctionDef) {
-            Iterator<ASTEntry> it = visitor.getOutline();
-            ArrayList<ASTEntry> list = new ArrayList<ASTEntry>();
-            while (it.hasNext()) {
-                ASTEntry next = it.next();
-                if(next.parent != null && next.parent.node == entry.node){
-                    list.add(next);
-                }
-            }
-            ASTEntry[] array = list.toArray(new ASTEntry[0]);
-            cache.put(element, array);
+        if (objectsWithParentStillNotDeterminedInOutline != null && (entry.node instanceof ClassDef || entry.node instanceof FunctionDef)) {
+        	List<ASTEntry> list = objectsWithParentStillNotDeterminedInOutline.remove(entry.node);
+			ASTEntry[] array = EMPTY;
+        	if(list != null){
+        		array = list.toArray(EMPTY);
+        	}
+        	cache.put(element, array);
             return array;
         }
 
-        return null;
+        return EMPTY;
     }
 
     public Object getParent(Object element) {
@@ -62,26 +65,65 @@ public final class ShowOutlineTreeContentProvider implements ITreeContentProvide
     }
 
     public Object[] getElements(Object inputElement) {
-        visitor = DefinitionsASTIteratorVisitor.create((SimpleNode) inputElement);
-        if(visitor == null){
-            return new Object[0];
-        }
-        Iterator<ASTEntry> it = visitor.getOutline();
-        ArrayList<ASTEntry> list = new ArrayList<ASTEntry>();
-        while(it.hasNext()){
-            ASTEntry next = it.next();
-            if(next.parent == null){
-                list.add(next);
-            }
-        }
-        return list.toArray(new ASTEntry[0]);
+    	if(this.newInput == null){
+    		this.inputChanged(null, null, inputElement);
+    	}
+    	Assert.isTrue(inputElement == this.newInput);
+    	return elementsForCurrentInput==null?EMPTY:elementsForCurrentInput;
     }
 
     public void dispose() {
-        //do nothing
+        outline = null;
+        objectsWithParentStillNotDeterminedInOutline = null;
+        cache.clear();
+    	elementsForCurrentInput = null;
+    	newInput = null;
     }
 
     public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+    	if(newInput == this.newInput){
+    		return;
+    	}
+    	
+    	//let go of the old info before getting the new info
+    	dispose();
+    	this.newInput = newInput;
+    	
+    	if(newInput == null){
+    		return;
+    	}
+    	
         //do nothing
+    	DefinitionsASTIteratorVisitor visitor = DefinitionsASTIteratorVisitor.create((SimpleNode) newInput);
+    	if(visitor == null){
+    		elementsForCurrentInput = EMPTY;
+    		return;
+    	}
+    	
+    	outline = new ArrayList<ASTEntry>();
+    	objectsWithParentStillNotDeterminedInOutline = new HashMap<SimpleNode, List<ASTEntry>>();
+    	for(Iterator<ASTEntry> it=visitor.getOutline();it.hasNext();){
+    		ASTEntry next = it.next();
+    		outline.add(next);
+    		if(next.parent != null){
+    			List<ASTEntry> list = objectsWithParentStillNotDeterminedInOutline.get(next.parent.node);
+    			if(list == null){
+    				list = new ArrayList<ASTEntry>();
+    				objectsWithParentStillNotDeterminedInOutline.put(next.parent.node, list);
+    			}
+    			list.add(next);
+    		}
+    	}
+    	
+    	ArrayList<ASTEntry> list = new ArrayList<ASTEntry>();
+    	Iterator<ASTEntry> it = outline.iterator();
+    	while(it.hasNext()){
+    		ASTEntry next = it.next();
+    		if(next.parent == null){
+    			list.add(next);
+    		}
+    	}
+    	elementsForCurrentInput = list.toArray(new ASTEntry[0]);
     }
+
 }
