4fd3980a21ec086cf08d75e94af7211046e38216
fabioz <fabiofz@gmail.com>
2010-05-27 19:57:40 -0300

    Only showing run actions for the run configurations that have an interpreter configured.

---
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/launching/InterpreterTypeTester.java
@@ -0,0 +1,37 @@
+package org.python.pydev.debug.ui.launching;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.python.pydev.core.IInterpreterManager;
+import org.python.pydev.core.MisconfigurationException;
+import org.python.pydev.plugin.PydevPlugin;
+
+public class InterpreterTypeTester extends PropertyTester {
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		IInterpreterManager interpreterManager = null;
+		String str = expectedValue.toString();
+		
+		if("python".equals(str)){
+			interpreterManager = PydevPlugin.getPythonInterpreterManager();
+		}else if("jython".equals(str)){
+			interpreterManager = PydevPlugin.getJythonInterpreterManager();
+		}else if("ironpython".equals(str)){
+			interpreterManager = PydevPlugin.getIronpythonInterpreterManager();
+		}else{
+			PydevPlugin.log("Unable to check for: "+expectedValue);
+		}
+		
+		if(interpreterManager != null){
+			try {
+				String defaultInterpreter = interpreterManager.getDefaultInterpreter();
+				return defaultInterpreter != null;
+			} catch (MisconfigurationException e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+}
