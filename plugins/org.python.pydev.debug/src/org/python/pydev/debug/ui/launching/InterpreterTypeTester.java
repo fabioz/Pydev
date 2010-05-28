34492926abe05eb2b680f83c5c7e043797788537
fabioz <fabiofz@gmail.com>
2010-05-27 23:08:02 -0300

    Minor

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/launching/InterpreterTypeTester.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/launching/InterpreterTypeTester.java
@@ -7,7 +7,6 @@ import org.python.pydev.plugin.PydevPlugin;
 
 public class InterpreterTypeTester extends PropertyTester {
 
-	@Override
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {
 		IInterpreterManager interpreterManager = null;
