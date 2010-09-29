1274970bb5d03f97ee00962d877d71b26f0d8596
fabioz <fabiofz@gmail.com>
2010-09-28 21:42:17 -0300

    Improved django wizard configuration a bit to cover cases where the user does not have django installed or tries to add 'django' as the project name

---
+++ plugins/org.python.pydev.django/src/org/python/pydev/django/ui/wizards/project/ErrorWizardPage.java
@@ -0,0 +1,34 @@
+package org.python.pydev.django.ui.wizards.project;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class ErrorWizardPage extends WizardPage{
+
+    private String msg;
+
+    protected ErrorWizardPage(String pageName, String msg) {
+        super(pageName);
+        this.msg = msg;
+    }
+    
+
+    public void createControl(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NULL);
+        composite.setLayout(new GridLayout());
+        composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+        composite.setFont(parent.getFont());
+        
+        Label label = new Label(composite, SWT.NONE);
+        label.setFont(parent.getFont());
+        label.setText(msg);
+        
+        setErrorMessage("Unexpected error.");
+        setControl(composite);
+    }
+    
+}
