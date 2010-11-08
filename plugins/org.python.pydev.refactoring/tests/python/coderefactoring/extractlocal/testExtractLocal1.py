7e63757d9d40fb39fb9b5f731ef272cf2f58bdce
fabioz <fabiofz@gmail.com>
2010-11-08 15:38:27 -0200

    Removed mail.jar (not used anymore). Added failing test case for issue in extract local variable.

---
+++ plugins/org.python.pydev.refactoring/tests/python/coderefactoring/extractlocal/testExtractLocal1.py
@@ -0,0 +1,14 @@
+class Now:
+    def Foo(self):
+        def Bar(self):
+            return 'grid1'
+        pool[##|'project'##|] = 10
+
+##r
+
+class Now:
+    def Foo(self):
+        def Bar(self):
+            return 'grid1'
+        extracted_variable = 'project'
+        pool[extracted_variable] = 10
