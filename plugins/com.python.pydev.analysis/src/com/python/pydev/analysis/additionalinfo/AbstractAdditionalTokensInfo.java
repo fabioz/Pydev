033d9a11a5b95fbba959423d04a23bbe938bd846
fabioz <fabiofz@gmail.com>
2011-05-16 11:31:24 -0300

    Override with classmethod properly using the name of the class now.

--- plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/additionalinfo/AbstractAdditionalTokensInfo.java
+++ plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/additionalinfo/AbstractAdditionalTokensInfo.java
@@ -18,7 +18,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.SortedMap;
