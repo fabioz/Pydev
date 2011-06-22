0b2318c50098b61af9b8811b21f010a77d30ac43
mxia <mxia@aptana.com>
2011-06-22 14:05:35 -0700

    Fixed APSTUD-2788: created getOutlineContentProvider() and getOutlineLabelProvider() methods in AbstractThemeableEditor class so that the quick outline doesn't need the outline view to be open to work.

--- plugins/org.python.pydev.django_templates/src/org/python/pydev/django_templates/html/editor/DjHTMLEditor.java
+++ plugins/org.python.pydev.django_templates/src/org/python/pydev/django_templates/html/editor/DjHTMLEditor.java
@@ -9,6 +9,8 @@ package org.python.pydev.django_templates.html.editor;
 
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.IVerticalRuler;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.swt.widgets.Composite;
 import org.python.pydev.django_templates.DjPlugin;
 import org.python.pydev.django_templates.IDjConstants;
@@ -17,7 +19,6 @@ import org.python.pydev.django_templates.html.outline.DjHTMLOutlineContentProvid
 import org.python.pydev.django_templates.html.outline.DjHTMLOutlineLabelProvider;
 import org.python.pydev.utils.ICallback;
 
-import com.aptana.editor.common.outline.CommonOutlinePage;
 import com.aptana.editor.common.parsing.FileService;
 import com.aptana.editor.common.validator.IValidationManager;
 import com.aptana.editor.css.ICSSConstants;
@@ -29,67 +30,74 @@ import com.aptana.editor.js.IJSConstants;
 /**
  * @author Fabio Zadrozny
  */
-public class DjHTMLEditor extends HTMLEditor {
+public class DjHTMLEditor extends HTMLEditor
+{
 
-    private DjEditor djEditor;
-    
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.aptana.editor.common.AbstractThemeableEditor#initializeEditor()
-     */
-    @Override
-    protected void initializeEditor() {
-        super.initializeEditor();
-        djEditor = new DjEditor();
-        this.djEditor.registerPrefChangeListener(new ICallback() {
-            
-            public Object call(Object args) throws Exception {
-                return getISourceViewer();
-            }
-        });
-        
-        setSourceViewerConfiguration(new DjHTMLSourceViewerConfiguration(this.djEditor.getChainedPrefStore(), this));
-        setDocumentProvider(DjPlugin.getDefault().getDjHTMLDocumentProvider());
-    }
-    
-    @Override
-    protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
-        ISourceViewer viewer = super.createSourceViewer(parent, ruler, styles);
-        djEditor.onCreateSourceViewer(viewer);
-        return viewer;
-    }
+	private DjEditor djEditor;
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.AbstractThemeableEditor#initializeEditor()
+	 */
+	@Override
+	protected void initializeEditor()
+	{
+		super.initializeEditor();
+		djEditor = new DjEditor();
+		this.djEditor.registerPrefChangeListener(new ICallback()
+		{
 
-    @Override
-    public void dispose() {
-        super.dispose();
-        djEditor.dispose();
-    }
+			public Object call(Object args) throws Exception
+			{
+				return getISourceViewer();
+			}
+		});
 
-    
-    @Override
-    protected FileService createFileService()
-    {
-        FileService fileService = new FileService(IHTMLConstants.CONTENT_TYPE_HTML, new HTMLParseState());
-        IValidationManager validationManager = fileService.getValidationManager();
-        validationManager.addNestedLanguage(ICSSConstants.CONTENT_TYPE_CSS);
-        validationManager.addNestedLanguage(IJSConstants.CONTENT_TYPE_JS);
-        validationManager.addNestedLanguage(IDjConstants.CONTENT_TYPE_DJANGO_HTML);
-        return fileService;
-    }
+		setSourceViewerConfiguration(new DjHTMLSourceViewerConfiguration(this.djEditor.getChainedPrefStore(), this));
+		setDocumentProvider(DjPlugin.getDefault().getDjHTMLDocumentProvider());
+	}
 
-    @Override
-    protected CommonOutlinePage createOutlinePage() {
-        CommonOutlinePage outline = super.createOutlinePage();
-        outline.setContentProvider(new DjHTMLOutlineContentProvider());
-        outline.setLabelProvider(new DjHTMLOutlineLabelProvider(getFileService().getParseState()));
+	@Override
+	protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles)
+	{
+		ISourceViewer viewer = super.createSourceViewer(parent, ruler, styles);
+		djEditor.onCreateSourceViewer(viewer);
+		return viewer;
+	}
 
-        return outline;
-    }
+	@Override
+	public void dispose()
+	{
+		super.dispose();
+		djEditor.dispose();
+	}
 
-    @Override
-    protected char[] getPairMatchingCharacters() {
-        return this.djEditor.getPairMatchingCharacters(super.getPairMatchingCharacters());
-    }
+	@Override
+	protected FileService createFileService()
+	{
+		FileService fileService = new FileService(IHTMLConstants.CONTENT_TYPE_HTML, new HTMLParseState());
+		IValidationManager validationManager = fileService.getValidationManager();
+		validationManager.addNestedLanguage(ICSSConstants.CONTENT_TYPE_CSS);
+		validationManager.addNestedLanguage(IJSConstants.CONTENT_TYPE_JS);
+		validationManager.addNestedLanguage(IDjConstants.CONTENT_TYPE_DJANGO_HTML);
+		return fileService;
+	}
+
+	@Override
+	public ITreeContentProvider getOutlineContentProvider()
+	{
+		return new DjHTMLOutlineContentProvider();
+	}
+
+	@Override
+	public ILabelProvider getOutlineLabelProvider()
+	{
+		return new DjHTMLOutlineLabelProvider(getFileService().getParseState());
+	}
+
+	@Override
+	protected char[] getPairMatchingCharacters()
+	{
+		return this.djEditor.getPairMatchingCharacters(super.getPairMatchingCharacters());
+	}
 }
