051e830979c0022dd3a98cbd58cb88ede5971840
fabioz <fabiofz@gmail.com>
2011-10-18 10:36:35 -0200

    Fixed: opened external files are reopened / breakpoints in external files are properly shown.

--- plugins/org.python.pydev/src/org/python/pydev/editor/PyDocumentProvider.java
+++ plugins/org.python.pydev/src/org/python/pydev/editor/PyDocumentProvider.java
@@ -9,11 +9,41 @@
  */
 package org.python.pydev.editor;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.python.pydev.editorinput.PydevZipFileEditorInput;
 
 /**
  * @author Fabio
  */
 public class PyDocumentProvider extends TextFileDocumentProvider{
+    
+    private Map<Object, IAnnotationModel> moreInfo = new HashMap<Object, IAnnotationModel>();
+    
+    @Override
+    public void connect(Object element) throws CoreException {
+        
+        super.connect(element);
+        if (getFileInfo(element) != null)
+            return;
+        
+        if(element instanceof PydevZipFileEditorInput){
+            moreInfo.put(element, new PyExternalZipFileAnnotationModel((PydevZipFileEditorInput)element));
+        }
+
+    }
+    
+    @Override
+    public IAnnotationModel getAnnotationModel(Object element) {
+        IAnnotationModel annotationModel = super.getAnnotationModel(element);
+        if(annotationModel == null){
+            annotationModel = moreInfo.get(element);
+        }
+        return annotationModel;
+    }
 
 }
