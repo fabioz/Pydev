e6a39406b83938a02c626114aba8cf0f0a296074
fabioz <fabiofz@gmail.com>
2012-06-28 11:54:38 -0300

    Preparing homepage for release 2.6.0

--- plugins/com.python.pydev.docs/build_both.py
+++ plugins/com.python.pydev.docs/build_both.py
@@ -11,7 +11,7 @@ for arg in args:
         version = arg[len('--version='):]
         LAST_VERSION_TAG = version
 else:
-    LAST_VERSION_TAG = '2.5.0' #Not specified (let's leave one there)
+    LAST_VERSION_TAG = '2.6.0' #Not specified (let's leave one there)
 
 
 import build_python_code_block
@@ -55,11 +55,11 @@ def BuildFromRst(source_filename, is_new_homepage=False):
     print source_filename
     import os
     from docutils import core
-    
+
     # dict of default settings to override (same as in the cmdline params, but as attribute names:
     #   "--embed-stylesheet" => "embed_stylesheet"
     settings_overrides = {}
-    
+
     import os
     # publish as html
     ret = core.publish_file(
@@ -68,14 +68,14 @@ def BuildFromRst(source_filename, is_new_homepage=False):
         destination_path=os.tempnam(),
         settings_overrides=settings_overrides,
     )
-    
-    final = ret[ret.find('<body>')+6: ret.find('</body>')].strip()
+
+    final = ret[ret.find('<body>') + 6: ret.find('</body>')].strip()
     if final.startswith('<div'):
         final = final[final.find('\n'):]
         final = final[:final.rfind('</div>')]
-        
+
     rst_contents = open(source_filename, 'r').read()
-    
+
     if rst_contents.startswith('..'):
         image_area_right_area_and_quote_area = ''
         #lines = []
@@ -89,19 +89,19 @@ def BuildFromRst(source_filename, is_new_homepage=False):
             image_area_right_area_and_quote_area = DEFAULT_AREAS
         else:
             image_area_right_area_and_quote_area = DEFAULT_AREAS_MANUAL
-    
+
     name = source_filename.split('.')[0]
     if is_new_homepage:
-        if os.path.exists(name+'.contents.htm'):
-            raise AssertionError('This file should not exist: '+name+'.contents.htm')
-        if os.path.exists(name+'.contents.html'):
-            raise AssertionError('This file should not exist: '+name+'.contents.html')
+        if os.path.exists(name + '.contents.htm'):
+            raise AssertionError('This file should not exist: ' + name + '.contents.htm')
+        if os.path.exists(name + '.contents.html'):
+            raise AssertionError('This file should not exist: ' + name + '.contents.html')
         contents = DEFAULT_CONTENTS_TEMPLATE % (image_area_right_area_and_quote_area,)
         final = contents.replace('<contents_area></contents_area>', '<contents_area>%s</contents_area>' % final)
-        
-    
-    final = final.replace('\r\n','\n').replace('\r','\n')
-    f = open(name+'.contents.rst_html', 'wb')
+
+
+    final = final.replace('\r\n', '\n').replace('\r', '\n')
+    f = open(name + '.contents.rst_html', 'wb')
     print >> f, final
     f.close()
 
@@ -113,40 +113,40 @@ def GenerateRstInDir(d, is_new_homepage=False):
     for f in os.listdir(d):
         if f.endswith('.rst'):
             BuildFromRst(f, is_new_homepage)
-    
-    
+
+
 if __name__ == '__main__':
     this_script_dir = os.path.realpath(os.path.abspath(this_script_dir))
     print 'Directory with this script:', this_script_dir
-    
+
     print 'Generating rst for merged_homepage'
     os.chdir(os.path.join(this_script_dir, 'merged_homepage'))
-    
+
     #Copy the update site redirections
     shutil.rmtree(os.path.join('final', 'updates'), ignore_errors=True)
     shutil.copytree('updates', os.path.join('final', 'updates'))
-    
+
     shutil.rmtree(os.path.join('final', 'nightly'), ignore_errors=True)
     shutil.copytree('nightly', os.path.join('final', 'nightly'))
-    
+
     shutil.copyfile('stylesheet.css', os.path.join('final', 'stylesheet.css'))
     shutil.copyfile('favicon.ico', os.path.join('final', 'favicon.ico'))
     shutil.copyfile('pydev_certificate.cer', os.path.join('final', 'pydev_certificate.cer'))
     shutil.copyfile('video_pydev_20.html', os.path.join('final', 'video_pydev_20.html'))
     shutil.copyfile('video_swfobject.js', os.path.join('final', 'video_swfobject.js'))
-    
+
     GenerateRstInDir('.', True)
-    
+
     sys.path.insert(0, os.path.join(this_script_dir, 'merged_homepage', 'scripts'))
     sys.path.insert(0, '.')
 #    print 'PYTHONPATH changed. Using:'
 #    for p in sys.path:
 #        print '    - ', p
-    
+
     os.chdir(os.path.join(this_script_dir, 'merged_homepage', 'scripts'))
     import build_merged #@UnresolvedImport
     os.chdir(os.path.join(this_script_dir, 'merged_homepage'))
     build_merged.LAST_VERSION_TAG = LAST_VERSION_TAG
     build_merged.DoIt()
-    
+
     sys.stdout.write('Finished\n')
