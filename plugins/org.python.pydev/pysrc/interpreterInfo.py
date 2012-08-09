b03d9387186948208c1cd865c6d94fc711b50340
fabioz <fabiofz@gmail.com>
2012-08-09 20:29:07 -0300

    Fixed issue when unable to get filesystem encoding.

--- plugins/org.python.pydev/pysrc/interpreterInfo.py
+++ plugins/org.python.pydev/pysrc/interpreterInfo.py
@@ -26,11 +26,11 @@ except: #ImportError or AttributeError.
         '''fixes the path so that the format of the path really reflects the directories in the system
         '''
         return path
-    
+
     def join(a, b):
         if a.endswith('/') or a.endswith('\\'):
             return a + b
-        return a+'/'+b
+        return a + '/' + b
 
 
 IS_PYTHON_3K = 0
@@ -48,17 +48,17 @@ try:
     True
 except:
     exec ('True, False = 1,0') #An exec is used so that python 3k does not give a syntax error
-    
+
 import time
 
 if sys.platform == "cygwin":
-    
+
     try:
         import ctypes #use from the system if available
     except ImportError:
         sys.path.append(join(sys.path[0], 'third_party/wrapped_for_pydev'))
         import ctypes
-        
+
     def nativePath(path):
         MAX_PATH = 512  # On cygwin NT, its 260 lately, but just need BIG ENOUGH buffer
         '''Get the native form of the path, like c:\\Foo for /cygdrive/c/Foo'''
@@ -67,7 +67,7 @@ if sys.platform == "cygwin":
         path = fullyNormalizePath(path)
         ctypes.cdll.cygwin1.cygwin_conv_to_win32_path(path, retval) #@UndefinedVariable
         return retval.value
-        
+
 else:
     def nativePath(path):
         return fullyNormalizePath(path)
@@ -76,13 +76,15 @@ else:
 
 def getfilesystemencoding():
     try:
-        return sys.getfilesystemencoding()
+        ret = sys.getfilesystemencoding()
+        if not ret:
+            raise RuntimeError('Unable to get encoding.')
     except:
         #Only available from 2.3 onwards.
         if sys.platform == 'win32':
             return 'mbcs'
         return 'utf-8'
-    
+
 file_system_encoding = getfilesystemencoding()
 
 def tounicode(s):
@@ -90,12 +92,12 @@ def tounicode(s):
         #Depending on the platform variant we may have decode on string or not.
         return s.decode(file_system_encoding)
     return s
-    
+
 def toutf8(s):
     if hasattr(s, 'encode'):
         return s.encode('utf-8')
     return s
-    
+
 
 if __name__ == '__main__':
     try:
@@ -103,16 +105,16 @@ if __name__ == '__main__':
         time.sleep(0.1)
     except:
         pass
-    
+
     try:
         executable = nativePath(sys.executable)
     except:
         executable = sys.executable
-        
+
     if sys.platform == "cygwin" and not executable.endswith('.exe'):
         executable += '.exe'
 
-    
+
     try:
         major = str(sys.version_info[0])
         minor = str(sys.version_info[1])
@@ -123,32 +125,32 @@ if __name__ == '__main__':
         s = string.split(s, '.')
         major = s[0]
         minor = s[1]
-        
+
     s = tounicode('%s.%s') % (tounicode(major), tounicode(minor))
-        
+
     contents = [tounicode('<xml>')]
     contents.append(tounicode('<version>%s</version>') % (tounicode(s),))
-            
+
     contents.append(tounicode('<executable>%s</executable>') % tounicode(executable))
-    
+
     #this is the new implementation to get the system folders 
     #(still need to check if it works in linux)
     #(previously, we were getting the executable dir, but that is not always correct...)
     prefix = tounicode(nativePath(sys.prefix))
     #print_ 'prefix is', prefix
-    
+
 
     result = []
-    
+
     path_used = sys.path
     try:
         path_used = path_used[:] #Use a copy.
     except:
         pass #just ignore it...
-    
+
     for p in path_used:
         p = tounicode(nativePath(p))
-        
+
         try:
             import string #to be compatible with older versions
             if string.find(p, prefix) == 0: #was startswith
@@ -162,20 +164,20 @@ if __name__ == '__main__':
                 result.append((p, True))
             else:
                 result.append((p, False))
-            
+
     for p, b in result:
         if b:
             contents.append(tounicode('<lib path="ins">%s</lib>') % (p,))
         else:
             contents.append(tounicode('<lib path="out">%s</lib>') % (p,))
-    
+
     #no compiled libs
     #nor forced libs
-    
+
     for builtinMod in sys.builtin_module_names:
         contents.append(tounicode('<forced_lib>%s</forced_lib>') % tounicode(builtinMod))
-        
-        
+
+
     contents.append(tounicode('</xml>'))
     unic = tounicode('\n').join(contents)
     inutf8 = toutf8(unic)
@@ -184,7 +186,7 @@ if __name__ == '__main__':
         sys.stdout.buffer.write(inutf8)
     else:
         sys.stdout.write(inutf8)
-    
+
     try:
         sys.stdout.flush()
         sys.stderr.flush()
@@ -192,5 +194,5 @@ if __name__ == '__main__':
         time.sleep(0.1)
     except:
         pass
-    
+
     raise RuntimeError('Ok, this is so that it shows the output (ugly hack for some platforms, so that it releases the output).')
