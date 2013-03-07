7d244bb55550203534e8990284b97d60a3fa951f
fabioz <fabiofz@gmail.com>
2013-03-07 15:20:16 -0300

    Fixed error in license file.

--- plugins/org.python.pydev.core/src/org/python/pydev/core/uiutils/UIUtils.java
+++ plugins/org.python.pydev.core/src/org/python/pydev/core/uiutils/UIUtils.java
@@ -1,8 +1,7 @@
 /**
- * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
- * Please see the license.html included with this distribution for details.
+ * Copyright (c) 2005-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Eclipse Public License (EPL).
+ * Please see the license.txt included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
  */
 package org.python.pydev.core.uiutils;
