f6a63f6d0866a1248c48d05d268de3cd518017cf
fabioz <fabiofz@gmail.com>
2013-03-09 14:09:05 -0300

    Adding copyright to files.

--- plugins/com.aptana.shared_ui/src_overview_ruler/org/python/pydev/overview_ruler/StyledTextWithoutVerticalBar.java
+++ plugins/com.aptana.shared_ui/src_overview_ruler/org/python/pydev/overview_ruler/StyledTextWithoutVerticalBar.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2013 by Brainwy Software Ltda, Inc. All Rights Reserved.
+ * Licensed under the terms of the Eclipse Public License (EPL).
+ * Please see the license.txt included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
 package org.python.pydev.overview_ruler;
 
 import org.eclipse.swt.custom.StyledText;
