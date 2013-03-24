a0bfa15e59a9828eaf3a6fcf5fc755201db8dd9d
fabioz <fabiofz@gmail.com>
2013-03-23 23:18:23 -0300

    Minor

--- plugins/org.python.pydev.shared_core/src/org/python/pydev/shared_core/structure/OrderedMap.java
+++ plugins/org.python.pydev.shared_core/src/org/python/pydev/shared_core/structure/OrderedMap.java
@@ -6,4 +6,12 @@ public class OrderedMap<K, V> extends LinkedHashMap<K, V> {
 
     private static final long serialVersionUID = 3463361572350039096L;
 
+    public OrderedMap() {
+        super();
+    }
+
+    public OrderedMap(int initialCapacity) {
+        super(initialCapacity);
+    }
+
 }
