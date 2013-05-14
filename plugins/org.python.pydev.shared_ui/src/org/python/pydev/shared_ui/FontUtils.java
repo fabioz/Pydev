f40f016858d17acc6b71b30910928900d46b8f2f
André Berg <andre.bergmedia@googlemail.com>
2013-05-14 09:45:30 +0200

    Add getFontData method to FontUtils that allows for specifying a SWT style other than BOLD.

--- plugins/org.python.pydev.shared_ui/src/org/python/pydev/shared_ui/FontUtils.java
+++ plugins/org.python.pydev.shared_ui/src/org/python/pydev/shared_ui/FontUtils.java
@@ -87,16 +87,34 @@ public class FontUtils {
         return new Tuple<String, Integer>(fontName, fontHeight);
     }
 
+    
+    /**
+     * Calls {@link #getFontData(int, boolean)} with {@link SWT#NONE} for the style param.
+     * @see {@link #getFontData(int, int, boolean)}
+     */
     public static FontData getFontData(int usage, boolean useDefaultJFaceFontIfPossible) {
+        return getFontData(usage, SWT.NONE, useDefaultJFaceFontIfPossible);
+    }
+    
+    /**
+     * Select a monospaced font based on intended usage. 
+     * Can be used to provide a consistend code font size between platforms.
+     * 
+     * @param usage intended usage. See {@link IFontUsage} for valid values.
+     * @param style SWT style constants mask
+     * @param useDefaultJFaceFontIfPossible
+     * @return {@link FontData} object
+     */
+    public static FontData getFontData(int usage, int style, boolean useDefaultJFaceFontIfPossible) {
         if (useDefaultJFaceFontIfPossible) {
             FontData[] textFontData = JFaceResources.getTextFont().getFontData();
             if (textFontData.length == 1) {
                 return textFontData[0];
             }
         }
-        Tuple<String, Integer> codeFontDetails = FontUtils.getCodeFontNameAndHeight(IFontUsage.IMAGECACHE);
+        Tuple<String, Integer> codeFontDetails = FontUtils.getCodeFontNameAndHeight(usage);
         String fontName = codeFontDetails.o1;
         int base = codeFontDetails.o2.intValue();
-        return new FontData(fontName, base, SWT.BOLD);
+        return new FontData(fontName, base, style);
     }
 }
