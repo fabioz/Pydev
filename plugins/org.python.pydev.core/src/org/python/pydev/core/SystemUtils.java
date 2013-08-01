3fe628ad6fcb56ccc85e0467eb15d2cacc3a7dad
fabioz <fabiofz@gmail.com>
2013-08-01 18:08:25 -0300

    Removing unused methods.

--- plugins/org.python.pydev.core/src/org/python/pydev/core/SystemUtils.java
+++ plugins/org.python.pydev.core/src/org/python/pydev/core/SystemUtils.java
@@ -1,11 +1,7 @@
 package org.python.pydev.core;
 
-import java.awt.Desktop;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.python.pydev.core.log.Log;
@@ -19,31 +15,6 @@ import org.python.pydev.core.log.Log;
 public class SystemUtils {
 
     /**
-     * Open webpage given by URI with the default system browser.
-     */
-    public static void openWebpage(URI uri) {
-        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
-            try {
-                desktop.browse(uri);
-            } catch (Exception e) {
-                Log.log(e);
-            }
-        }
-    }
-
-    /**
-     * Open webpage given by URL with the default system browser.
-     */
-    public static void openWebpage(URL url) {
-        try {
-            openWebpage(url.toURI());
-        } catch (URISyntaxException e) {
-            Log.log(e);
-        }
-    }
-
-    /**
      * Open a webpage in Eclipse's default browser.
      * 
      * @param url URL address of the webpage
@@ -54,7 +25,7 @@ public class SystemUtils {
         try {
             browser = PlatformUI.getWorkbench().getBrowserSupport().createBrowser(id);
             browser.openURL(url);
-        } catch (PartInitException e) {
+        } catch (Exception e) {
             Log.log(e);
         }
     }
