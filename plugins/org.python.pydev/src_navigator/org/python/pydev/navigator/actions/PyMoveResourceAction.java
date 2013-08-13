93a28ee39949ba656afcf7c3ed7bb709563b527c
Andrew Ferrazzutti <aferrazz@redhat.com>
2013-08-13 16:58:24 -0400

    Bug 987642: complete PYTHONPATH updating features for moving src folders with Move wizard.
    Moving source folders with right click->Move... now correctly updates the PYTHONPATHs of all
    projects involved in the move.

--- plugins/org.python.pydev/src_navigator/org/python/pydev/navigator/actions/PyMoveResourceAction.java
+++ plugins/org.python.pydev/src_navigator/org/python/pydev/navigator/actions/PyMoveResourceAction.java
@@ -7,17 +7,35 @@
 package org.python.pydev.navigator.actions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.actions.MoveResourceAction;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+import org.python.pydev.core.IPythonPathNature;
+import org.python.pydev.core.docutils.StringUtils;
+import org.python.pydev.core.log.Log;
+import org.python.pydev.plugin.nature.PythonNature;
 
 public class PyMoveResourceAction extends MoveResourceAction {
 
@@ -71,7 +89,107 @@ public class PyMoveResourceAction extends MoveResourceAction {
      * Update the PYTHONPATH of the project that originally contained the moved resource,
      * and that of the project it was moved to.
      */
-    private void updatePyPath() {
+    private void updatePyPath(IContainer destination) {
+        try {
+            // Get the PYTHONPATH of the destination project. It may be modified to include the pasted resources.
+            IProject destProject = destination.getProject();
+            IPythonPathNature destPythonPathNature = PythonNature.getPythonPathNature(destProject);
+            List<IPath> destSourcePaths = new ArrayList<IPath>();
+            if (destPythonPathNature != null) {
+                SortedSet<String> destSourcePathSet = new TreeSet<String>(
+                        destPythonPathNature.getProjectSourcePathSet(true));
+                for (String sourceFolderName : destSourcePathSet) {
+                    destSourcePaths.add(Path.fromOSString(sourceFolderName));
+                }
+            }
+            int numOldPaths = destSourcePaths.size();
+
+            // Now find which of the copied resources are source folders, whose paths are in their projects' PYTHONPATH.
+            // NOTE: presently, copied resources must come from the same parent/project. The multiple project checking
+            // used here is kept in case a potential new feature changes that restriction.
+            Map<IProject, List<IPath>> projectSourcePaths = new HashMap<IProject, List<IPath>>();
+            Map<IProject, List<IFolder>> remFoldersOfProjMap = new HashMap<IProject, List<IFolder>>();
+            boolean innerMove = false;
+            for (IResource resource : selected) {
+                if (!(resource instanceof IFolder)) {
+                    continue;
+                }
+                IProject project = resource.getProject();
+                List<IPath> sourcePaths = projectSourcePaths.get(project);
+                if (sourcePaths == null) {
+                    sourcePaths = new ArrayList<IPath>();
+                    IPythonPathNature pythonPathNature = PythonNature.getPythonPathNature(project);
+                    // Ignore resources that come from a non-Python project.
+                    if (pythonPathNature == null) {
+                        continue;
+                    }
+                    SortedSet<String> projectSourcePathSet = new TreeSet<String>(
+                            pythonPathNature.getProjectSourcePathSet(true));
+                    for (String sourceFolderName : projectSourcePathSet) {
+                        sourcePaths.add(Path.fromOSString(sourceFolderName));
+                    }
+                    projectSourcePaths.put(project, sourcePaths);
+                }
+                List<IFolder> remFoldersOfProj = remFoldersOfProjMap.get(project);
+                if (remFoldersOfProj == null) {
+                    remFoldersOfProj = new LinkedList<IFolder>();
+                    remFoldersOfProjMap.put(project, remFoldersOfProj);
+                }
+                remFoldersOfProj.add((IFolder) resource);
+                IPath resourcePath = resource.getFullPath();
+
+                // If the resource or its children are in its original project's PYTHONPATH, add to the destination project's PYTHONPATH.
+                if (destPythonPathNature != null) {
+                    int numSourcePaths = sourcePaths.size();
+                    for (int i = 0; i < numSourcePaths; i++) {
+                        IPath sourcePath = sourcePaths.get(i);
+                        if (resourcePath.isPrefixOf(sourcePath)) {
+                            IPath destSourcePath = destination.getFullPath().append(
+                                    sourcePath.removeFirstSegments(resourcePath.segmentCount() - 1));
+                            if (!destSourcePaths.contains(destSourcePath)) {
+                                destSourcePaths.add(destSourcePath);
+                                // Do this in case a resource was moved within its own project.
+                                if (destProject.equals(project)) {
+                                    sourcePaths.add(destSourcePath);
+                                    innerMove = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // If the destination project's PYTHONPATH was updated, rebuild it.
+            // NOTE: skip this if a resource was moved in its own project, in which case the PYTHONPATH will be rebuilt later.
+            if (destSourcePaths.size() != numOldPaths && !innerMove) {
+                destPythonPathNature.setProjectSourcePath(StringUtils.join("|", destSourcePaths));
+                PythonNature.getPythonNature(destProject).rebuildPath();
+            }
+
+            // Update the PYTHONPATHs of the project(s) resources were moved out of.
+            for (IProject project : remFoldersOfProjMap.keySet()) {
+                boolean removedSomething = false;
+                List<IPath> sourcePaths = projectSourcePaths.get(project);
+                // Check if deleted folders are/contain source folders.
+                for (IFolder remFolder : remFoldersOfProjMap.get(project)) {
+                    IPath remPath = remFolder.getFullPath();
+                    for (int i = 0; i < sourcePaths.size(); i++) {
+                        if (remPath.isPrefixOf(sourcePaths.get(i))) {
+                            sourcePaths.remove(i--);
+                            removedSomething = true;
+                        }
+                    }
+                }
+                // Now update each project's PYTHONPATH, if source folders have been removed.
+                if (removedSomething) {
+                    PythonNature.getPythonPathNature(project).setProjectSourcePath(StringUtils.join("|", sourcePaths));
+                    PythonNature.getPythonNature(project).rebuildPath();
+                }
+            }
+
+        } catch (Exception e) {
+            Log.log(IStatus.ERROR, "Unexpected error setting project properties", e);
+        }
     }
 
     @Override
@@ -84,6 +202,28 @@ public class PyMoveResourceAction extends MoveResourceAction {
         return new StructuredSelection(selected);
     }
 
+    private IPath pyQueryDestinationResource() {
+        // start traversal at root resource, should probably start at a
+        // better location in the tree
+        String title;
+        if (selected.size() == 1) {
+            title = "Choose destination for ''" + selected.get(0).getName() + "'':";
+        } else {
+            title = "Choose destination for " + new Integer(selected.size()) + " selected resources:";
+        }
+        ContainerSelectionDialog dialog = new ContainerSelectionDialog(shellProvider.getShell(),
+                selected.get(0).getParent(), true, title);
+        dialog.setTitle("Move Resources");
+        dialog.setValidator(this);
+        dialog.showClosedProjects(false);
+        dialog.open();
+        Object[] result = dialog.getResult();
+        if (result != null && result.length == 1) {
+            return (IPath) result[0];
+        }
+        return null;
+    }
+
     /*
      * (non-Javadoc) Method declared on IAction.
      */
@@ -92,8 +232,30 @@ public class PyMoveResourceAction extends MoveResourceAction {
             return;
         }
         Helpers.checkValidateState();
-        super.run();
-        updatePyPath();
+        try {
+            operation = createOperation();
+            operation.setModelProviderIds(getModelProviderIds());
+            IPath destination = pyQueryDestinationResource();
+            if (destination == null) {
+                return;
+            }
+            IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+            IContainer container = (IContainer) root.findMember(destination);
+            if (container == null) {
+                return;
+            }
+            runOperation(getResources(selected), container);
+        } finally {
+            operation = null;
+        }
+    }
+
+    @Override
+    protected void runOperation(IResource[] resources, IContainer destination) {
+        super.runOperation(resources, destination);
+        if (destinations.size() > 0) {
+            updatePyPath(destination);
+        }
     }
 
 }
