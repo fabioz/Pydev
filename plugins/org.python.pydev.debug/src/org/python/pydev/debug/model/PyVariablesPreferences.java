5661e1e7ee3c7e071c3426cbb572c4a5e60d9c1e
Andrew Ferrazzutti <aferrazz@redhat.com>
2013-08-28 17:06:36 -0400

    Add missing licenses (EPL) with an automated script.
    Use a script to find which files are missing an EPL header, and to generate & insert an EPL
    header in each such file. (Ignore Jython files and auto-generated grammar files.)

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyVariablesPreferences.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyVariablesPreferences.java
@@ -1,3 +1,14 @@
+/******************************************************************************
+* Copyright (C) 2012  Jonah Graham
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Ecliplse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Jonah Graham <jonah@kichwacoders.com> - initial API and implementation
+******************************************************************************/
 package org.python.pydev.debug.model;
 
 import org.eclipse.jface.preference.IPreferenceStore;
