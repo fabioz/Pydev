e9a390a61c2ce7097e0154a3e787c6b46383240e
Jonah Graham <jonah@kichwacoders.com>
2013-08-30 09:40:07 +0100

    Rewrite of IPython wrapper to use/extend IPython's InteractiveShell
    The original interface to IPython was based on IPython.testing.globalipapp,
    this new version is a complete re-write, but it delivers the same
    functionality.
    
    The reason for this re-write is that further extensions to enable more
    IPython features (such as magics and some hooks) is easier to do with
    a subclass of InteractiveShell.
    
    There are many todos marked in this code for items that can/should be
    improved to enable better IPython integration. These will be part of
    future commits.

--- plugins/org.python.pydev/pysrc/pydev_ipython_console_011.py
+++ plugins/org.python.pydev/pysrc/pydev_ipython_console_011.py
@@ -1,75 +1,112 @@
-from IPython.frontend.terminal.interactiveshell import TerminalInteractiveShell
+"""Interface to TerminalInteractiveShell for PyDev Interactive Console frontend
+   for IPython 0.11 to 1.0+.
+"""
+
+from __future__ import print_function
+from IPython.core.error import UsageError
 from IPython.core.inputsplitter import IPythonInputSplitter
-from IPython.utils import io
-import sys
-import codeop, re
-original_stdout = sys.stdout
-original_stderr = sys.stderr
+from IPython.core.interactiveshell import InteractiveShell, InteractiveShellABC
+try:
+    from IPython.terminal.interactiveshell import TerminalInteractiveShell
+except ImportError:
+    # Versions of IPython [0.11,1.0) had an extra hierarchy level
+    from IPython.frontend.terminal.interactiveshell import TerminalInteractiveShell
+from IPython.utils.traitlets import CBool, Unicode
+
+class PyDevTerminalInteractiveShell(TerminalInteractiveShell):
+    # @todo banner2: put something nice here about PyDev (version number is available?)
+    banner2 = Unicode('', config=True,
+        help="""The part of the banner to be printed after the profile"""
+    )
+    # @todo editor
+    # @todo pager
+    # @todo term_title: (can PyDev's title be changed???, see terminal.py for where to inject code, in particular set_term_title as used by %cd)
+
+    # Disable readline, readline type code is all handled by PyDev (on Java side)
+    readline_use = CBool(False)
+
+    # @todo colors_force: what should this be?
+    # @todo need a show_in_pager hook installed, see page.py:page()
+
+    # autoindent has no meaning in PyDev (PyDev always handles that on the Java side),
+    # and attempting to enable it will print a warning in the absence of readline.
+    autoindent = CBool(False)
+
+    # @todo connect gui to PyDev's enable GUI code
+    # @todo __init__: review which system should be used (piped or raw), (perhaps test with alias ff find / ; ff)
 
 
-#=======================================================================================================================
-# _showtraceback
-#=======================================================================================================================
-def _showtraceback(*args, **kwargs):
-    import traceback;traceback.print_exc()
-    
-    
-    
+    #-------------------------------------------------------------------------
+    # Things related to exceptions
+    #-------------------------------------------------------------------------
+
+    def showtraceback(self, exc_tuple=None, filename=None, tb_offset=None,
+                  exception_only=False):
+        # IPython does a lot of clever stuff with Exceptions. However mostly
+        # it is related to IPython running in a terminal instead of an IDE.
+        # (e.g. it prints out snippets of code around the stack trace)
+        # PyDev does a lot of clever stuff too, so leave exception handling
+        # with default print_exc that PyDev can parse and do its clever stuff
+        # with (e.g. it puts links back to the original source code)
+        import traceback;traceback.print_exc()
+
+    #-------------------------------------------------------------------------
+    # Things related to the terminal
+    #-------------------------------------------------------------------------
+
+    @property
+    def usable_screen_length(self):
+        # @todo we shouldn't reach here when using PyDev, before commit change remove exception
+        raise Exception("usable_screen_length isn't relevant to PyDev, ")
+        return 0
+
+    #-------------------------------------------------------------------------
+    # Things related to aliases
+    #-------------------------------------------------------------------------
+
+    def init_alias(self):
+        # InteractiveShell defines alias's we want, but TerminalInteractiveShell defines
+        # ones we don't. So don't use super and instead go right to InteractiveShell
+        InteractiveShell.init_alias(self)
+
+    #-------------------------------------------------------------------------
+    # Things related to exiting
+    #-------------------------------------------------------------------------
+    def ask_exit(self):
+        """ Ask the shell to exit. Can be overiden and used as a callback. """
+        # @todo I need a way to exit???
+        super(PyDevTerminalInteractiveShell, self).ask_exit()
+
+    #-------------------------------------------------------------------------
+    # Things related to magics
+    #-------------------------------------------------------------------------
+
+    def init_magics(self):
+        super(PyDevTerminalInteractiveShell, self).init_magics()
+        # @todo Any additional magics for PyDev?
+
+InteractiveShellABC.register(PyDevTerminalInteractiveShell)  # @UndefinedVariable
+
 #=======================================================================================================================
 # PyDevFrontEnd
 #=======================================================================================================================
 class PyDevFrontEnd:
 
-    def __init__(self, *args, **kwargs):        
-        #Initialization based on: from IPython.testing.globalipapp import start_ipython
-        
-        self._curr_exec_line = 0
-        # Store certain global objects that IPython modifies
-        _displayhook = sys.displayhook
-        _excepthook = sys.excepthook
-    
+    def __init__(self, *args, **kwargs):
         # Create and initialize our IPython instance.
-        shell = TerminalInteractiveShell.instance()
-        # Create an intput splitter to handle input separation
+        self.ipython = PyDevTerminalInteractiveShell.instance()
+        # Create an input splitter to handle input separation
         self.input_splitter = IPythonInputSplitter()
-
-        shell.showtraceback = _showtraceback
-        # IPython is ready, now clean up some global state...
-        
-        # Deactivate the various python system hooks added by ipython for
-        # interactive convenience so we don't confuse the doctest system
-        sys.displayhook = _displayhook
-        sys.excepthook = _excepthook
-    
-        # So that ipython magics and aliases can be doctested (they work by making
-        # a call into a global _ip object).  Also make the top-level get_ipython
-        # now return this without recursively calling here again.
-        get_ipython = shell.get_ipython
-        try:
-            import __builtin__
-        except:
-            import builtins as __builtin__
-        __builtin__._ip = shell
-        __builtin__.get_ipython = get_ipython
-        
-        # We want to print to stdout/err as usual.
-        io.stdout = original_stdout
-        io.stderr = original_stderr
-
-        self.ipython = shell
 
     def complete(self, string):
         return self.ipython.complete(None, line=string)
-
-    def is_complete(self, string):
-        return  not self.input_splitter.push_accepts_more()
 
     def getNamespace(self):
         return self.ipython.user_ns
 
     def addExec(self, line):
         self.input_splitter.push(line)
-        if self.is_complete(line):
+        if not self.input_splitter.push_accepts_more():
             self.ipython.run_cell(self.input_splitter.source_reset())
             return False
         else:
---
+++ plugins/org.python.pydev/pysrc/tests/test_pydev_ipython_011.py
@@ -0,0 +1,85 @@
+import unittest
+import sys
+import os
+from nose.tools import eq_
+from pprint import pprint
+from pydev_imports import StringIO
+
+# make it as if we were executing from the directory above this one
+sys.argv[0] = os.path.dirname(sys.argv[0])
+# twice the dirname to get the previous level from this file.
+sys.path.insert(1, os.path.join(os.path.dirname(sys.argv[0])))
+
+# PyDevFrontEnd depends on singleton in IPython, so you
+# can't make multiple versions. So we reuse front_end for
+# all the tests
+
+orig_stdout = sys.stdout
+orig_stderr = sys.stderr
+
+stdout = sys.stdout = StringIO()
+stderr = sys.stderr = StringIO()
+
+from pydev_ipython_console_011 import PyDevFrontEnd
+front_end = PyDevFrontEnd()
+
+
+def addExec(code, expected_more=False):
+    more = front_end.addExec(code)
+    eq_(expected_more, more)
+
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        front_end.input_splitter.reset()
+        stdout.truncate(0)
+        stdout.seek(0)
+        stderr.truncate(0)
+        stderr.seek(0)
+    def tearDown(self):
+        pass
+
+
+class TestPyDevFrontEnd(TestBase):
+    def testAddExec_1(self):
+        addExec('if True:', True)
+    def testAddExec_2(self):
+        addExec('if True:\n    testAddExec_a = 10\n', True)
+    def testAddExec_3(self):
+        assert 'testAddExec_a' not in front_end.getNamespace()
+        addExec('if True:\n    testAddExec_a = 10\n\n')
+        assert 'testAddExec_a' in front_end.getNamespace()
+        eq_(front_end.getNamespace()['testAddExec_a'], 10)
+
+    def testGetNamespace(self):
+        assert 'testGetNamespace_a' not in front_end.getNamespace()
+        addExec('testGetNamespace_a = 10')
+        assert 'testGetNamespace_a' in front_end.getNamespace()
+        eq_(front_end.getNamespace()['testGetNamespace_a'], 10)
+
+    def testComplete(self):
+        unused_text, matches = front_end.complete('%')
+        assert len(matches) > 1, 'at least one magic should appear in completions'
+
+        addExec('testComplete_a = 5')
+        addExec('testComplete_b = 10')
+        addExec('testComplete_c = 15')
+        unused_text, matches = front_end.complete('testComplete_')
+        assert len(matches) == 3
+        eq_(set(['testComplete_a', 'testComplete_b', 'testComplete_c']), set(matches))
+
+
+class TestRunningCode(TestBase):
+    def testPrint(self):
+        addExec('print("output")')
+        eq_(stdout.getvalue(), 'output\n')
+        
+    def testQuestionMark_1(self):
+        addExec('?')
+        assert len(stdout.getvalue()) > 1000, 'IPython help should be pretty big'
+
+    def testQuestionMark_2(self):
+        addExec('int?')
+        assert stdout.getvalue().find('Convert') != -1
+
+if __name__ == '__main__':
+    unittest.main()
