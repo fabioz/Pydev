bc590a960cef79eaeca660ff1deddde129163c2d
Jonah Graham <jonah@kichwacoders.com>
2013-08-31 10:48:39 +0100

    Whitespace changes (Ctrl-Shift-F)

--- plugins/org.python.pydev/pysrc/pydev_ipython_console.py
+++ plugins/org.python.pydev/pysrc/pydev_ipython_console.py
@@ -2,8 +2,8 @@ import sys
 from pydev_console_utils import BaseInterpreterInterface
 import re
 
-#Uncomment to force PyDev standard shell.   
-#raise ImportError()
+# Uncomment to force PyDev standard shell.
+# raise ImportError()
 
 try:
     # Versions of IPython from 0.11 were designed to integrate into tools other
@@ -22,7 +22,7 @@ class InterpreterInterface(BaseInterpreterInterface):
     '''
         The methods in this class should be registered in the xml-rpc server.
     '''
-    
+
     def __init__(self, host, client_port):
         self.client_port = client_port
         self.host = host
@@ -32,12 +32,12 @@ class InterpreterInterface(BaseInterpreterInterface):
 
     def doAddExec(self, line):
         return bool(self.interpreter.addExec(line))
-    
-    
+
+
     def getNamespace(self):
         return self.interpreter.getNamespace()
-    
-        
+
+
     def getCompletions(self, text, act_tok):
         try:
             ipython_completion = text.startswith('%')
@@ -45,26 +45,26 @@ class InterpreterInterface(BaseInterpreterInterface):
                 s = re.search(r'\bcd\b', text)
                 if s is not None and s.start() == 0:
                     ipython_completion = True
-                
+
             if ipython_completion:
                 TYPE_LOCAL = '9'
                 _line, completions = self.interpreter.complete(text)
-                
+
                 ret = []
                 append = ret.append
                 for completion in completions:
                     append((completion, '', '', TYPE_LOCAL))
                 return ret
 
-            #Otherwise, use the default PyDev completer (to get nice icons)
+            # Otherwise, use the default PyDev completer (to get nice icons)
             from _pydev_completer import Completer
             completer = Completer(self.getNamespace(), None)
             return completer.complete(act_tok)
         except:
             import traceback;traceback.print_exc()
             return []
-        
-    
+
+
     def close(self):
         sys.exit(0)
 
