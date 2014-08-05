05b481bff7e9c7a43c31fe97f42ab14c4c43bb74
fabioz <fabiofz@gmail.com>
2014-08-05 08:18:00 -0300

    Working around https://bugs.eclipse.org/bugs/show_bug.cgi?id=438641

--- plugins/org.python.pydev.shared_ui/src/org/python/pydev/shared_ui/editor/BaseSourceViewer.java
+++ plugins/org.python.pydev.shared_ui/src/org/python/pydev/shared_ui/editor/BaseSourceViewer.java
@@ -3,7 +3,10 @@ package org.python.pydev.shared_ui.editor;
 import org.eclipse.jface.text.source.IOverviewRuler;
 import org.eclipse.jface.text.source.IVerticalRuler;
 import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Layout;
+import org.python.pydev.shared_core.log.Log;
 
 public class BaseSourceViewer extends SourceViewer implements ITextViewerExtensionAutoEditions {
 
@@ -26,6 +29,23 @@ public class BaseSourceViewer extends SourceViewer implements ITextViewerExtensi
     @Override
     public void setAutoEditionsEnabled(boolean b) {
         this.autoEditionsEnabled = b;
+    }
+
+    @Override
+    protected Layout createLayout() {
+        //Workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=438641
+        return new RulerLayout(GAP_SIZE_1) {
+            @Override
+            protected void layout(Composite composite, boolean flushCache) {
+                StyledText textWidget = getTextWidget();
+                if (textWidget == null) {
+                    Log.log("Error: textWidget is already null. SourceViewer: " + BaseSourceViewer.this + " control: "
+                            + BaseSourceViewer.this.getControl());
+                    return;
+                }
+                super.layout(composite, flushCache);
+            }
+        };
     }
 
 }
