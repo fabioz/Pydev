69fbdaee09207b9b264fa7e69e3d776139b98c9f
David Lehrian <david@lehrian.com>
2014-12-30 13:21:03 -0800

    Added backward compatibility for Django < 1.7
    Added "Sync DB" back for Django < 1.7; Added conditional code back to
    DEFAULT_DJANGO_INTERPRETER_CMDS for Django < 1.5. and made new
    DEFAULT_DJANGO_INTERPRETER_CMDS code conditional for Django >= 1.7.

---
+++ plugins/org.python.pydev.django/src/org/python/pydev/django/debug/ui/actions/DjangoSyncDB.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Eclipse Public License (EPL).
+ * Please see the license.txt included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package org.python.pydev.django.debug.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+
+public class DjangoSyncDB extends DjangoAction {
+
+    @Override
+    public void run(IAction action) {
+        launchDjangoCommand("syncdb", true);
+    }
+
+}
