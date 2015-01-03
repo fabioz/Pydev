4c0bb846fd0faa85996a4248893521b60e908421
fabioz <fabiofz@gmail.com>
2015-01-03 21:53:48 -0200

    Docs

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/remote/AddIgnoreThrownExceptionIn.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/remote/AddIgnoreThrownExceptionIn.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 by Brainwy Software Ltda, Inc. All Rights Reserved.
+ * Copyright (c) 2013-2015 by Brainwy Software Ltda, Inc. All Rights Reserved.
  * Licensed under the terms of the Eclipse Public License (EPL).
  * Please see the license.txt included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
