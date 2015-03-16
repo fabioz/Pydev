2d9bec028b7962b76cd6c2f6362967065098414e
Fabio Zadrozny <fabioz@esss.com.br>
2015-03-16 07:58:56 -0300

    Comments

--- plugins/org.python.pydev.core/src/org/python/pydev/core/UnpackInfo.java
+++ plugins/org.python.pydev.core/src/org/python/pydev/core/UnpackInfo.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2015 by Brainwy Software Ltda. All Rights Reserved.
+ * Licensed under the terms of the Eclipse Public License (EPL).
+ * Please see the license.txt included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
 package org.python.pydev.core;
 
 public class UnpackInfo {
