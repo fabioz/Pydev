bd39dd4d1bdac169fc8ff6801cf9b923dcf1b84b
Fabio Zadrozny <fabioz@esss.com.br>
2016-01-19 13:49:10 -0200

    The code-analysis and token building builders have different keys (so, one won't cancel the other anymore, which could lead to issues both in indexing and in the code-analysis).

--- plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/builder/KeyForAnalysisRunnable.java
+++ plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/builder/KeyForAnalysisRunnable.java
@@ -8,50 +8,68 @@ package com.python.pydev.analysis.builder;
 
 /**
  * Immutable.
- * 
+ *
  * Auto hash code and equals.
- * 
+ *
  * Used as the entry for the analysis runnable (to know if there is already some runnable analyzing a module
  * we want to analyze later)
- * 
+ *
  * @author Fabio
  */
 public class KeyForAnalysisRunnable {
 
     public final String projectName;
     public final String moduleName;
+    private int analysisCause;
 
-    public KeyForAnalysisRunnable(String projectName, String moduleName) {
+    /**
+     * @param analysisCause: we don't mix the parser/builder for analysis.
+     */
+    public KeyForAnalysisRunnable(String projectName, String moduleName, int analysisCause) {
         this.projectName = projectName;
         this.moduleName = moduleName;
+        this.analysisCause = analysisCause;
     }
 
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + analysisCause;
         result = prime * result + ((moduleName == null) ? 0 : moduleName.hashCode());
         result = prime * result + ((projectName == null) ? 0 : projectName.hashCode());
         return result;
     }
 
+    @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         KeyForAnalysisRunnable other = (KeyForAnalysisRunnable) obj;
+        if (analysisCause != other.analysisCause) {
+            return false;
+        }
         if (moduleName == null) {
-            if (other.moduleName != null)
+            if (other.moduleName != null) {
                 return false;
-        } else if (!moduleName.equals(other.moduleName))
+            }
+        } else if (!moduleName.equals(other.moduleName)) {
             return false;
+        }
         if (projectName == null) {
-            if (other.projectName != null)
+            if (other.projectName != null) {
                 return false;
-        } else if (!projectName.equals(other.projectName))
+            }
+        } else if (!projectName.equals(other.projectName)) {
             return false;
+        }
         return true;
     }
 
