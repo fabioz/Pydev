fabioz/Pydev

commit e13fd81fe87365be54aed8405741f84df90f2fca

Fixing issue where stack frames created from debugging multiple processes did not have a reference to the process (and thus din't have a reference to the console too, which made trying to enter statements for such processes give an error such as: [Invalid Frame]: Please select frame to connect the console.) -- i.e.: http://stackoverflow.com/questions/34546963/invalid-frame-when-debugging-with-pydev-and-eclipse)

plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/launching/PythonRunner.java
@@ -53,32 +53,32 @@ import org.python.pydev.shared_ui.debug.RelaunchConstants;
 /**
  * Launches Python process, and connects it to Eclipse's debugger.
  * Waits for process to complete.
- * 
+ *
  * Modeled after org.eclipse.jdt.internal.launching.StandardVMDebugger.
  */
 public class PythonRunner {
 
     /**
      * To listen to changes on PyUnit runs, one would do:
-     
+    
         ICallbackListener<IPyUnitServer> listener = new ICallbackListener<IPyUnitServer>() {
-            
+    
             public Object call(IPyUnitServer pyUnitServer) {
                 IPyUnitServerListener pyUnitViewServerListener = new IPyUnitServerListener() {
-                    
+    
                     public void notifyTestsCollected(String totalTestsCount) {
                     }
-                    
+    
                     public void notifyTest(String status, String location, String test, String capturedOutput, String errorContents,
                             String time) {
                     }
-                    
+    
                     public void notifyStartTest(String location, String test) {
                     }
-                    
+    
                     public void notifyFinished(String totalTimeInSecs) {
                     }
-                    
+    
                     public void notifyDispose() {
                     }
                 };
@@ -87,13 +87,13 @@ public class PythonRunner {
             }
         };
         PythonRunner.onPyUnitServerCreated.registerListener(listener);
-
+    
      */
     public static final CallbackWithListeners<IPyUnitServer> onPyUnitServerCreated = new CallbackWithListeners<IPyUnitServer>();
 
     /**
      * Launches the configuration
-     * 
+     *
      * The code is modeled after Ant launching example.
      */
     public static void run(final PythonRunnerConfig config, ILaunch launch, IProgressMonitor monitor)
@@ -130,9 +130,9 @@ public class PythonRunner {
 
     /**
      * Launches the config in the debug mode.
-     * 
+     *
      * Loosely modeled upon Ant launcher.
-     * @throws JDTNotAvailableException 
+     * @throws JDTNotAvailableException
      */
     private static void runDebug(final PythonRunnerConfig config, final ILaunch launch, IProgressMonitor monitor)
             throws CoreException, IOException, JDTNotAvailableException {
@@ -142,7 +142,7 @@ public class PythonRunner {
         IProgressMonitor subMonitor = new SubProgressMonitor(monitor, 5);
         subMonitor.beginTask("Launching python", 1);
 
-        // Launch & connect to the debugger        
+        // Launch & connect to the debugger
         final RemoteDebugger debugger = new RemoteDebugger();
         final ListenConnector listenConnector = debugger.startConnect(subMonitor, config);
         subMonitor.subTask("Constructing command_line...");
@@ -155,7 +155,7 @@ public class PythonRunner {
 
         //Set the debug target before registering with the debug plugin (we want it before creating the console).
         PyDebugTarget t = new PyDebugTarget(launch, null, config.resource, debugger, config.project);
-        IProcess process;
+        final IProcess process;
         try {
             process = registerWithDebugPluginForProcessType(config.getRunningName(), launch, p, processAttributes,
                     config);
@@ -198,7 +198,8 @@ public class PythonRunner {
                     while (!launch.isTerminated() && !listenConnector.isDisposed()) {
                         Socket socket = listenConnector.waitForConnection();
                         if (socket != null && !socket.isClosed()) {
-                            PyDebugTarget t = new PyDebugTarget(launch, null, config.resource, debugger, config.project);
+                            PyDebugTarget t = new PyDebugTarget(launch, process, config.resource, debugger,
+                                    config.project);
                             try {
                                 t.startTransmission(socket); // this starts reading/writing from sockets
                                 t.initialize();
@@ -230,7 +231,7 @@ public class PythonRunner {
 
         subMonitor.beginTask("Launching python", 1);
 
-        // Launch & connect to the debugger        
+        // Launch & connect to the debugger
         subMonitor.subTask("Constructing command_line...");
         String commandLineAsString = SimpleRunner.getArgumentsAsStr(cmdLine);
         //System.out.println("running command line: "+commandLineAsString);

plugins/org.python.pydev.debug/src_console/org/python/pydev/debug/newconsole/CurrentPyStackFrameForConsole.java
@@ -2,6 +2,7 @@ package org.python.pydev.debug.newconsole;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.debug.core.model.IDebugTarget;
+import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.ui.console.IConsole;
 import org.python.pydev.debug.model.AbstractDebugTarget;
@@ -27,7 +28,8 @@ public class CurrentPyStackFrameForConsole extends AnyPyStackFrameSelected {
     protected boolean acceptsSelection(PyStackFrame stackFrame) {
         if (super.acceptsSelection(stackFrame)) {
             AbstractDebugTarget target = (AbstractDebugTarget) stackFrame.getAdapter(IDebugTarget.class);
-            if (DebugUITools.getConsole(target.getProcess()) == console) {
+            IProcess process = target.getProcess();
+            if (DebugUITools.getConsole(process) == console) {
                 return true;
             }
         }

commit 83bf3519f906a60fd094fb922fcf2bbf7593c581

Commit e13fd81 broke multiprocess debugging when the child process exited and this commit fixes that. Fixes #658, #656.

plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyDebugTarget.java
@@ -21,7 +21,6 @@ import org.python.pydev.core.log.Log;
 import org.python.pydev.debug.console.ConsoleCompletionsPageParticipant;
 import org.python.pydev.debug.model.remote.AbstractRemoteDebugger;
 
-
 /**
  * Debugger class that represents a single python process.
  * 
@@ -38,15 +37,22 @@ public class PyDebugTarget extends AbstractDebugTarget {
      */
     public final IProject project;
     public volatile boolean finishedInit = false;
+    public final boolean isAuxiliaryDebugTarget;
 
     public PyDebugTarget(ILaunch launch, IProcess process, IPath[] file, AbstractRemoteDebugger debugger,
             IProject project) {
+        this(launch, process, file, debugger, project, false);
+    }
+
+    public PyDebugTarget(ILaunch launch, IProcess process, IPath[] file, AbstractRemoteDebugger debugger,
+            IProject project, boolean isAuxiliaryDebugTarget) {
         this.launch = launch;
         this.process = process;
         this.file = file;
         this.debugger = debugger;
         this.threads = new PyThread[0];
         this.project = project;
+        this.isAuxiliaryDebugTarget = isAuxiliaryDebugTarget;
         launch.addDebugTarget(this);
         debugger.addTarget(this);
         IBreakpointManager breakpointManager = DebugPlugin.getDefault().getBreakpointManager();
@@ -73,6 +79,7 @@ public class PyDebugTarget extends AbstractDebugTarget {
         return process;
     }
 
+    @Override
     public boolean canTerminate() {
         if (!finishedInit) {
             //We must finish init to terminate
@@ -83,6 +90,7 @@ public class PyDebugTarget extends AbstractDebugTarget {
         return !this.isTerminated();
     }
 
+    @Override
     public boolean isTerminated() {
         if (!finishedInit) {
             //We must finish init to terminate
@@ -94,12 +102,18 @@ public class PyDebugTarget extends AbstractDebugTarget {
         return process.isTerminated();
     }
 
+    @Override
     public void terminate() {
         if (process != null) {
-            try {
-                process.terminate();
-            } catch (DebugException e) {
-                Log.log(e);
+            if (!this.isAuxiliaryDebugTarget) {
+                try {
+                    // We can only terminate the process if it's not an auxiliary debug target
+                    // (otherwise, when connecting on multiple processes, we may end up terminating
+                    // the main process when the child process exits).
+                    process.terminate();
+                } catch (DebugException e) {
+                    Log.log(e);
+                }
             }
             process = null;
         }

plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/launching/PythonRunner.java
@@ -199,7 +199,7 @@ public class PythonRunner {
                         Socket socket = listenConnector.waitForConnection();
                         if (socket != null && !socket.isClosed()) {
                             PyDebugTarget t = new PyDebugTarget(launch, process, config.resource, debugger,
-                                    config.project);
+                                    config.project, true);
                             try {
                                 t.startTransmission(socket); // this starts reading/writing from sockets
                                 t.initialize();