869d1d6e74ee5e9e7d4dbe3fa53a8f5e107ca6c4
Fabio Zadrozny <fabiofz@gmail.com>
2017-06-22 14:00:26 -0300

    Optimization when dealing with huge files.

--- plugins/org.python.pydev.shared_ui/src_overview_ruler/org/python/pydev/overview_ruler/StyledTextWithoutVerticalBar.java
+++ plugins/org.python.pydev.shared_ui/src_overview_ruler/org/python/pydev/overview_ruler/StyledTextWithoutVerticalBar.java
@@ -6,6 +6,9 @@
  */
 package org.python.pydev.overview_ruler;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ST;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
 
@@ -15,4 +18,51 @@ public final class StyledTextWithoutVerticalBar extends StyledText {
         super(parent, style);
     }
 
+    /**
+     * Optimization:
+     * The method:
+     * org.eclipse.swt.custom.StyledTextRenderer.setStyleRanges(int[], StyleRange[])
+     *
+     * is *extremely* inefficient on huge documents with lots of styles when
+     * ranges are not passed and have to be computed in the block:
+     *
+     * if (newRanges == null && COMPACT_STYLES) {
+     *
+     * So, we just pre-create the ranges here (Same thing on org.brainwy.liclipsetext.editor.common.LiClipseSourceViewer.StyledTextImproved)
+     * A patch should later be given to SWT itself.
+     */
+
+    @Override
+    public void setStyleRanges(StyleRange[] styles) {
+        if (styles != null) {
+            int[] newRanges = new int[styles.length << 1];
+            for (int i = 0, j = 0; i < styles.length; i++) {
+                StyleRange newStyle = styles[i];
+                newRanges[j++] = newStyle.start;
+                newRanges[j++] = newStyle.length;
+            }
+            super.setStyleRanges(newRanges, styles);
+            return;
+        }
+        super.setStyleRanges(styles);
+    }
+
+    @Override
+    public void replaceStyleRanges(int start, int length, StyleRange[] styles) {
+        checkWidget();
+        if (isListening(ST.LineGetStyle)) {
+            return;
+        }
+        if (styles == null) {
+            SWT.error(SWT.ERROR_NULL_ARGUMENT);
+        }
+
+        int[] newRanges = new int[styles.length << 1];
+        for (int i = 0, j = 0; i < styles.length; i++) {
+            StyleRange newStyle = styles[i];
+            newRanges[j++] = newStyle.start;
+            newRanges[j++] = newStyle.length;
+        }
+        setStyleRanges(start, length, newRanges, styles);
+    }
 }
