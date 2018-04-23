10ac7629ef79168143f2f19ec43c6f17782fc304
fabioz <fabiofz@gmail.com>
2018-04-23 14:44:32 -0300

    Minor (renamed local var).

--- plugins/org.python.pydev.core/pysrc/_pydevd_bundle/pydevd_comm.py
+++ plugins/org.python.pydev.core/pysrc/_pydevd_bundle/pydevd_comm.py
@@ -515,12 +515,12 @@ def start_server(port):
 
     try:
         s.listen(1)
-        newSock, _addr = s.accept()
+        new_sock, _addr = s.accept()
         pydevd_log(1, "Connection accepted")
         # closing server socket is not necessary but we don't need it
         s.shutdown(SHUT_RDWR)
         s.close()
-        return newSock
+        return new_sock
 
     except:
         sys.stderr.write("Could not bind to port: %s\n" % (port,))
