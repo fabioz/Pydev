497bc630f6d0d1c076d4587ca069620846529a8a
fabioz <fabiofz@gmail.com>
2018-11-08 13:39:13 -0200

    Improve test for Python 2.

--- plugins/org.python.pydev.core/pysrc/tests_python/test_run.py
+++ plugins/org.python.pydev.core/pysrc/tests_python/test_run.py
@@ -10,9 +10,16 @@ def _run_and_check(testdir, path):
 
 def test_run(testdir):
     from tests_python import debugger_unittest
-    from os.path import os
+    import sys
+    import os
 
-    foo_dir = debugger_unittest._get_debugger_test_file(os.path.join('resources', 'launch', 'foo'))
+    if debugger_unittest.IS_PY3K:
+        foo_dir = debugger_unittest._get_debugger_test_file(os.path.join('resources', 'launch', 'foo'))
+        foo_module = 'tests_python.resources.launch.foo'
+    else:
+        foo_dir = debugger_unittest._get_debugger_test_file(os.path.join('resources', 'launch_py2', 'foo'))
+        foo_module = 'tests_python.resources.launch_py2.foo'
+
     pydevd_dir = os.path.dirname(os.path.dirname(__file__))
     assert os.path.exists(os.path.join(pydevd_dir, 'pydevd.py'))
 
@@ -33,10 +40,11 @@ py_db = pydevd.PyDB()
 py_db.run(%(foo_dir)r, set_trace=False)
 ''' % locals()))
 
-    foo_module = 'tests_python.resources.launch.foo'
+    if sys.version_info[0:2] == (2, 6):
+        # Not valid for Python 2.6
+        return
 
-    if debugger_unittest.IS_PY3K:
-        _run_and_check(testdir, testdir.makepyfile('''
+    _run_and_check(testdir, testdir.makepyfile('''
 import sys
 sys.path.append(%(pydevd_dir)r)
 sys.argv.append('--as-module')
@@ -46,7 +54,7 @@ py_db.ready_to_run = True
 py_db.run(%(foo_module)r, is_module=True)
 ''' % locals()))
 
-        _run_and_check(testdir, testdir.makepyfile('''
+    _run_and_check(testdir, testdir.makepyfile('''
 import sys
 sys.argv.append('--as-module')
 sys.path.append(%(pydevd_dir)r)
