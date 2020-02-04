dbdaeace809bfae6ea19c997a3fc32bdab531210
Fabio Zadrozny <fabiofz@gmail.com>
2020-02-04 14:59:41 -0300

    wip

--- plugins/org.python.pydev.core/pysrc/_pydevd_bundle/pydevd_trace_dispatch_regular.py
+++ plugins/org.python.pydev.core/pysrc/_pydevd_bundle/pydevd_trace_dispatch_regular.py
@@ -181,7 +181,7 @@ def fix_top_level_trace_and_get_trace_func(py_db, frame):
             return f_trace, False
 
     thread_tracer = additional_info.thread_tracer
-    if thread_tracer is None:
+    if thread_tracer is None or thread_tracer._args[0].pydb_disposed:
         thread_tracer = ThreadTracer(args)
         additional_info.thread_tracer = thread_tracer
 
