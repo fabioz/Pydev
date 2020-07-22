d9a56e2c8c61bc7a94593ef055b3351f456f9eb6
Luis Cabral <oluiscabral@hotmail.com>
2020-07-22 15:33:11 -0300

    Update IPyDocIterator.java

--- plugins/org.python.pydev.core/src/org/python/pydev/core/docutils/IPyDocIterator.java
+++ plugins/org.python.pydev.core/src/org/python/pydev/core/docutils/IPyDocIterator.java
@@ -2,13 +2,7 @@ package org.python.pydev.core.docutils;
 
 import java.util.Iterator;
 
-import org.eclipse.jface.text.BadLocationException;
-
 public interface IPyDocIterator extends Iterator<String> {
 
-    public void setStartingOffset(int offset);
-
     public int getLastReturnedLine();
-
-    void setStartingLine(int line) throws BadLocationException;
 }
