fbb6b70684dcb5e969520da7939726b5e47e6871
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2020-09-07 10:19:36 -0300

    Handling MyPy error line comments that silence messages (# noqa). Fixes #Pydev-1090

--- plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/mypy/MypyAnalysis.java
+++ plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/mypy/MypyAnalysis.java
@@ -25,17 +25,22 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITypedRegion;
 import org.python.pydev.ast.runners.SimpleRunner;
 import org.python.pydev.core.IModulesManager;
 import org.python.pydev.core.IPythonNature;
 import org.python.pydev.core.MisconfigurationException;
 import org.python.pydev.core.PythonNatureWithoutProjectException;
+import org.python.pydev.core.docutils.ParsingUtils;
+import org.python.pydev.core.docutils.PySelection;
 import org.python.pydev.core.log.Log;
+import org.python.pydev.core.partition.PyPartitionScanner;
 import org.python.pydev.plugin.nature.PythonNature;
 import org.python.pydev.shared_core.callbacks.ICallback;
 import org.python.pydev.shared_core.callbacks.ICallback0;
 import org.python.pydev.shared_core.io.FileUtils;
 import org.python.pydev.shared_core.markers.PyMarkerUtils;
+import org.python.pydev.shared_core.partitioner.FastPartitioner;
 import org.python.pydev.shared_core.process.ProcessUtils;
 import org.python.pydev.shared_core.string.FastStringBuffer;
 import org.python.pydev.shared_core.string.StringUtils;
@@ -236,6 +241,7 @@ import com.python.pydev.analysis.external.WriteToStreamHelper;
 
     @Override
     public void afterRunProcess(String output, String errors, IExternalCodeAnalysisStream out) {
+        ParsingUtils parsingUtils = ParsingUtils.create(document);
         output = output.trim();
         errors = errors.trim();
         Map<LineCol, MessageInfo> lineColToMessage = new HashMap<>();
@@ -294,6 +300,19 @@ import com.python.pydev.analysis.external.WriteToStreamHelper;
                     }
                     if (markerSeverity != -1) {
                         line = Integer.parseInt(outputLine.substring(m.start(2), m.end(2))) - 1;
+                        String lineContents = PySelection.getLine(document, line);
+                        ITypedRegion partition = ((FastPartitioner) PyPartitionScanner
+                                .checkPartitionScanner(document))
+                                        .getPartition(document.getLineOffset(line) + lineContents.length() - 1);
+                        if (ParsingUtils.isCommentContentType(partition.getType())) {
+                            FastStringBuffer commentBuf = new FastStringBuffer();
+                            parsingUtils.eatComments(commentBuf, partition.getOffset());
+                            commentBuf.trim().deleteFirst();
+                            commentBuf.trim();
+                            if ("noqa".equals(commentBuf.toString())) {
+                                continue;
+                            }
+                        }
                         messageId = "mypy";
                         message = outputLine.substring(m.start(5), m.end(5)).trim();
 
