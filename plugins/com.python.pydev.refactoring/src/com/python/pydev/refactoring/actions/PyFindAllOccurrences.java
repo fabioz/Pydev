1f332600f44bc70f81803ed342bbbe410b97238a
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2020-09-09 09:41:16 -0300

    Update __init__ searches

--- plugins/com.python.pydev.refactoring/src/com/python/pydev/refactoring/actions/PyFindAllOccurrences.java
+++ plugins/com.python.pydev.refactoring/src/com/python/pydev/refactoring/actions/PyFindAllOccurrences.java
@@ -14,6 +14,8 @@ import org.eclipse.search.ui.NewSearchUI;
 import org.python.pydev.ast.refactoring.AbstractPyRefactoring;
 import org.python.pydev.ast.refactoring.IPyRefactoring2;
 import org.python.pydev.ast.refactoring.RefactoringRequest;
+import org.python.pydev.core.docutils.PySelection;
+import org.python.pydev.core.docutils.PySelection.LineStartingScope;
 import org.python.pydev.editor.actions.refactoring.PyRefactorAction;
 
 import com.python.pydev.refactoring.search.FindOccurrencesSearchQuery;
@@ -26,6 +28,16 @@ public class PyFindAllOccurrences extends PyRefactorAction {
         RefactoringRequest req = getRefactoringRequest(new NullProgressMonitor()); //as we're doing it in the background
         req.fillActivationTokenAndQualifier();
         if (req.qualifier != null && req.qualifier.trim().length() > 0) {
+            if (req.activationToken != null && req.activationToken.trim().length() == 0
+                    && "__init__".equals(req.qualifier)) {
+                LineStartingScope line = ps.getPreviousLineThatStartsScope(PySelection.INDENT_TOKENS, false,
+                        Integer.MAX_VALUE);
+                String className = PySelection.getClassNameInLine(line.lineStartingScope);
+                int col = line.lineStartingScope.indexOf(className);
+                int len = className.length();
+                req.ps = new PySelection(ps.getDoc(), line.iLineStartingScope, col, len);
+                req.fillActivationTokenAndQualifier();
+            }
             NewSearchUI.runQueryInBackground(newQuery(r, req));
         }
         return null;
