6be792dd4a18c4d1d20df56c85d08cbd2a16144c
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2020-12-03 09:12:24 -0300

    Fix exception pretty-printing ExtSlice. Fixes #PyDev-1106

--- plugins/org.python.pydev.parser/src/org/python/pydev/parser/prettyprinterv2/MakeAstValidForPrettyPrintingVisitor.java
+++ plugins/org.python.pydev.parser/src/org/python/pydev/parser/prettyprinterv2/MakeAstValidForPrettyPrintingVisitor.java
@@ -31,6 +31,7 @@ import org.python.pydev.parser.jython.ast.DictComp;
 import org.python.pydev.parser.jython.ast.Ellipsis;
 import org.python.pydev.parser.jython.ast.Exec;
 import org.python.pydev.parser.jython.ast.Expr;
+import org.python.pydev.parser.jython.ast.ExtSlice;
 import org.python.pydev.parser.jython.ast.For;
 import org.python.pydev.parser.jython.ast.FunctionDef;
 import org.python.pydev.parser.jython.ast.Global;
@@ -1082,6 +1083,19 @@ public class MakeAstValidForPrettyPrintingVisitor extends VisitorBase {
     @Override
     public Object visitExpr(Expr node) throws Exception {
         handleSimpleNode(node);
+        return null;
+    }
+
+    @Override
+    public Object visitExtSlice(ExtSlice node) throws Exception {
+        fixNode(node);
+        if (node.dims != null) {
+            for (int i = 0; i < node.dims.length; i++) {
+                node.dims[i].accept(this);
+                nextCol();
+            }
+        }
+        fixAfterNode(node);
         return null;
     }
 
