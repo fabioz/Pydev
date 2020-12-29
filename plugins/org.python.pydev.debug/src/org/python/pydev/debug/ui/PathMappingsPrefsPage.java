e65613818a53031bcb262737b919766183b2cc89
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2020-12-29 12:18:08 -0300

    Update PathMappingsPrefsPage.java

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/PathMappingsPrefsPage.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/PathMappingsPrefsPage.java
@@ -1,12 +1,17 @@
 package org.python.pydev.debug.ui;
 
+import java.util.List;
+
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.python.pydev.json.eclipsesource.JsonArray;
+import org.python.pydev.json.eclipsesource.JsonObject;
+import org.python.pydev.json.eclipsesource.JsonValue;
 import org.python.pydev.plugin.PydevPlugin;
 import org.python.pydev.plugin.preferences.PyDevEditorPreferences;
-import org.python.pydev.shared_ui.field_editors.PathMappingsFieldEditor;
+import org.python.pydev.shared_ui.field_editors.JsonFieldEditor;
 
 /**
  * Preferences for the locations that should be translated -- used when the debugger is not able
@@ -36,8 +41,46 @@ public class PathMappingsPrefsPage extends FieldEditorPreferencePage implements 
     @Override
     protected void createFieldEditors() {
         Composite p = getFieldEditorParent();
-        addField(new PathMappingsFieldEditor(PyDevEditorPreferences.PATHS_FROM_ECLIPSE_TO_PYTHON,
-                "Path Mappings JSON input", p));
+        JsonFieldEditor jsonFieldEditor = new JsonFieldEditor(PyDevEditorPreferences.PATHS_FROM_ECLIPSE_TO_PYTHON,
+                "Path Mappings JSON input", p);
+
+        jsonFieldEditor.setAdditionalJsonValidation((json) -> {
+            // Validate JSON and return error
+            if (!checkPathMappingsFormat(json)) {
+                return "Not a valid Path Mappings input.";
+            }
+            // No error
+            return null;
+        });
+        addField(jsonFieldEditor);
+    }
+
+    private boolean checkPathMappingsFormat(JsonValue json) {
+        if (!json.isArray()) {
+            return false;
+        }
+        JsonArray array = json.asArray();
+        if (array.size() == 0) {
+            return false;
+        }
+        for (JsonValue value : array) {
+            if (!value.isObject()) {
+                return false;
+            }
+            JsonObject object = value.asObject();
+            if (object.size() != 2) {
+                return false;
+            }
+            List<String> objectNames = object.names();
+            if (!objectNames.contains("localRoot") || !objectNames.contains("remoteRoot")) {
+                return false;
+            }
+
+            if (!object.get("localRoot").isString() || !object.get("remoteRoot").isString()) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
