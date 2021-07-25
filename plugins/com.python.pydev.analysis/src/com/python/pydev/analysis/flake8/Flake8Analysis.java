5f1e999a1e2618e1792151b43d6677849fceba4e
Fabio Zadrozny <fabiofz@gmail.com>
2021-07-25 09:21:07 -0300

    Always set --format=default for Flake8. Fixes #PyDev-1143

--- plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/flake8/Flake8Analysis.java
+++ plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/flake8/Flake8Analysis.java
@@ -94,7 +94,13 @@ import com.python.pydev.analysis.external.WriteToStreamHelper;
             return;
         }
         File workingDir = project.getLocation().toFile();
-        cmdList.addAll(userArgsAsList);
+        for (String s : userArgsAsList) {
+            if (s.startsWith("--format=")) {
+                continue; // ignore that as we'll add the '--format' as needed ourselves.
+            }
+            cmdList.add(s);
+        }
+        cmdList.add("--format=default");
         cmdList.add(target);
         String[] args = cmdList.toArray(new String[0]);
 
