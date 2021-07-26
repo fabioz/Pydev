4231c67eb9006386a5d277d7010c57ebac7b8d4f
oluiscabral <oluiscabral@hotmail.com>
2021-07-26 08:41:26 -0300

    update name_contextType

--- plugins/org.python.pydev.parser/src/org/python/pydev/parser/jython/ast/name_contextType.java
+++ plugins/org.python.pydev.parser/src/org/python/pydev/parser/jython/ast/name_contextType.java
@@ -3,19 +3,21 @@ package org.python.pydev.parser.jython.ast;
 
 
 public interface name_contextType {
-    public static final int ClassName = 1;
-    public static final int FunctionName = 2;
-    public static final int KeywordName = 3;
-    public static final int ImportName = 4;
-    public static final int VarArg = 5;
-    public static final int KwArg = 6;
-    public static final int ImportModule = 7;
-    public static final int Attrib = 8;
-    public static final int GlobalName = 9;
-    public static final int NonLocalName = 10;
+    public static final int PatternName = 1;
+    public static final int ClassName = 2;
+    public static final int FunctionName = 3;
+    public static final int KeywordName = 4;
+    public static final int ImportName = 5;
+    public static final int VarArg = 6;
+    public static final int KwArg = 7;
+    public static final int ImportModule = 8;
+    public static final int Attrib = 9;
+    public static final int GlobalName = 10;
+    public static final int NonLocalName = 11;
 
     public static final String[] name_contextTypeNames = new String[] {
         "<undef>",
+        "PatternName",
         "ClassName",
         "FunctionName",
         "KeywordName",
