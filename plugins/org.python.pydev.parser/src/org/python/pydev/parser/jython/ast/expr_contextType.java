8549b4207e35004986d36733dda9ea6b763dc076
oluiscabral <oluiscabral@hotmail.com>
2021-08-04 11:13:30 -0300

    Add pattern type to expr ctx constants

--- plugins/org.python.pydev.parser/src/org/python/pydev/parser/jython/ast/expr_contextType.java
+++ plugins/org.python.pydev.parser/src/org/python/pydev/parser/jython/ast/expr_contextType.java
@@ -3,18 +3,20 @@ package org.python.pydev.parser.jython.ast;
 
 
 public interface expr_contextType {
-    public static final int Load = 1;
-    public static final int Store = 2;
-    public static final int Del = 3;
-    public static final int AugLoad = 4;
-    public static final int AugStore = 5;
-    public static final int Param = 6;
-    public static final int Artificial = 7;
-    public static final int KwOnlyParam = 8;
-    public static final int NamedStore = 9;
+    public static final int Pattern = 1;
+    public static final int Load = 2;
+    public static final int Store = 3;
+    public static final int Del = 4;
+    public static final int AugLoad = 5;
+    public static final int AugStore = 6;
+    public static final int Param = 7;
+    public static final int Artificial = 8;
+    public static final int KwOnlyParam = 9;
+    public static final int NamedStore = 10;
 
     public static final String[] expr_contextTypeNames = new String[] {
         "<undef>",
+        "Pattern",
         "Load",
         "Store",
         "Del",
