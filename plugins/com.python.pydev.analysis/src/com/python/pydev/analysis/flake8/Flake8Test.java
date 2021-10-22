ad46e4da990660956f0b1aec158241d3fd5d5c1c
oluiscabral <oluiscabral@hotmail.com>
2021-10-22 09:55:59 -0300

    Clean and update Flake8 tests for #Pydev 1146

--- plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/flake8/Flake8Test.java
+++ plugins/com.python.pydev.analysis/src/com/python/pydev/analysis/flake8/Flake8Test.java
@@ -23,6 +23,8 @@ public class Flake8Test extends TestCase {
 
     private File tempDir;
     private FileStub file;
+    private int index = 0;
+    private List<MarkerInfo> markers;
 
     @Override
     protected void setUp() throws Exception {
@@ -33,89 +35,119 @@ public class Flake8Test extends TestCase {
         file = new FileStub(project, new File("snippet.py"));
     }
 
-    public void testMarkersMessage1()
-            throws CoreException, MisconfigurationException, PythonNatureWithoutProjectException {
-        IDocument document = new Document("x = 10");
-        Flake8Analysis flake8Analysis = new Flake8Analysis(file, document, file.getLocation(),
-                new NullProgressMonitor(), null);
-        flake8Analysis.afterRunProcess(file.getFullPath().toOSString() + ":1:7: W292 no newline at end of file", "",
-                null);
-        List<MarkerInfo> markers = flake8Analysis.getMarkers(file);
+    private void setUpTestForAssertions(IDocument document, String output) {
+        Flake8Analysis flake8Analysis = new Flake8Analysis(file, document, null, new NullProgressMonitor(), null);
+        flake8Analysis.afterRunProcess(output, "", null);
+        setMarkersAndResetIndex(flake8Analysis);
+    }
+
+    private void setMarkersAndResetIndex(Flake8Analysis flake8Analysis) {
+        markers = flake8Analysis.getMarkers(file);
+        index = 0;
+    }
+
+    private void assertMarkersValidity(int expectedAmountOfMarkers) {
+        assertEquals(expectedAmountOfMarkers, markers.size());
         for (MarkerInfo marker : markers) {
             assertEquals(marker.lineStart, marker.lineEnd);
         }
-        assertEquals(1, markers.size());
-        assertMarkerEquals(0, 0, 0, "W292", "no newline at end of file", markers);
     }
 
-    public void testMarkersMessage2()
+    /** Check the specified marker has been processed correctly
+    *
+    * @param line (zero based)
+    * @param column (zero based)
+    * @param violation_code flake8 violation code
+    * @param message flake8 descriptive message
+    * @param actualMarkers markers
+    */
+    private void assertMarkerEquals(int line, int column, String violation_code, String message) {
+        MarkerInfo marker = markers.get(index);
+        assertEquals(index + ": MarkerInfo lineStart", line, marker.lineStart);
+        assertEquals(index + ": MarkerInfo colStart", column, marker.colStart);
+        assertEquals(index + ": MarkerInfo message_id", violation_code,
+                marker.additionalInfo.get(Flake8Visitor.FLAKE8_MESSAGE_ID));
+        assertEquals(index + ": MarkerInfo message", "Flake8: " + message, marker.message);
+        Map<String, Object> attribMap = marker.getAsMap();
+        assertEquals(index + ": IMarker.LINE_NUMBER", line + 1, attribMap.get(IMarker.LINE_NUMBER));
+        index++;
+    }
+
+    public void testMarkersMessage()
             throws CoreException, MisconfigurationException, PythonNatureWithoutProjectException {
         IDocument document = new Document("x = 10");
-        Flake8Analysis flake8Analysis = new Flake8Analysis(file, document, file.getLocation(),
-                new NullProgressMonitor(), null);
-        flake8Analysis.afterRunProcess(file.getFullPath() + ":1:7: W292 no newline at end of file", "",
-                null);
-        List<MarkerInfo> markers = flake8Analysis.getMarkers(file);
-        for (MarkerInfo marker : markers) {
-            assertEquals(marker.lineStart, marker.lineEnd);
-        }
-        assertEquals(1, markers.size());
-        assertMarkerEquals(0, 0, 0, "W292", "no newline at end of file", markers);
+        String output = ".\\snippet.py:1:7: W292 no newline at end of file";
+        setUpTestForAssertions(document, output);
+        assertMarkersValidity(1);
+        assertMarkerEquals(0, 0, "W292", "no newline at end of file");
     }
 
-    public void testMarkersMessage3() {
+    public void testMarkersMessage1() {
         IDocument document = new Document("import math\n" +
-                "x = \"blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla\"\n"
-                +
+                "x = \"longlinelonglinelonglinelonglinelonglinelonglinelonglinelonglinelonglinelongline\"\n" +
                 "y = 20\n" +
                 "    f = 10\n" +
                 "def foo():\n" +
                 "        w = 10\n" +
                 "    pass");
-        Flake8Analysis flake8Analysis = new Flake8Analysis(file, document, null, new NullProgressMonitor(),
-                null);
+
         String output = ".\\snippet.py:1:1: E902 IndentationError: unindent does not match any outer indentation level\n"
                 +
-                ".\\snippet.py:2:80: E501 line too long (129 > 79 characters)\n" +
+                ".\\snippet.py:2:80: E501 line too long (80 > 79 characters)\n" +
                 ".\\snippet.py:4:5: E113 unexpected indentation\n" +
                 ".\\snippet.py:5:1: E302 expected 2 blank lines, found 0\n" +
                 ".\\snippet.py:6:9: E117 over-indented";
-        flake8Analysis.afterRunProcess(output, "", null);
 
-        List<MarkerInfo> markers = flake8Analysis.getMarkers(file);
-        for (MarkerInfo marker : markers) {
-            assertEquals(marker.lineStart, marker.lineEnd);
-        }
-
-        assertEquals(5, markers.size());
-        assertMarkerEquals(0, 0, 0, "E902", "IndentationError: unindent does not match any outer indentation level",
-                markers);
-        assertMarkerEquals(1, 1, 79, "E501", "line too long (129 > 79 characters)", markers);
-        assertMarkerEquals(2, 3, 4, "E113", "unexpected indentation", markers);
-        assertMarkerEquals(3, 4, 0, "E302", "expected 2 blank lines, found 0", markers);
-        assertMarkerEquals(4, 5, 8, "E117", "over-indented", markers);
+        setUpTestForAssertions(document, output);
+        assertMarkersValidity(5);
+        assertMarkerEquals(0, 0, "E902", "IndentationError: unindent does not match any outer indentation level");
+        assertMarkerEquals(1, 79, "E501", "line too long (80 > 79 characters)");
+        assertMarkerEquals(3, 4, "E113", "unexpected indentation");
+        assertMarkerEquals(4, 0, "E302", "expected 2 blank lines, found 0");
+        assertMarkerEquals(5, 8, "E117", "over-indented");
     }
 
-    /** Check the specified marker has been processed correctly
-    *
-    * @param index index within markers list
-    * @param line zero based index of the line in the document
-    * @param column zero based index of the column in the document
-    * @param message_id pylint short code for message
-    * @param message descriptive message
-    * @param actualMarkers markers
-    */
-    private void assertMarkerEquals(int index, int line, int column, String message_id, String message,
-            List<MarkerInfo> actualMarkers) {
-        MarkerInfo actualMarker = actualMarkers.get(index);
-        assertEquals(index + ": MarkerInfo lineStart", line, actualMarker.lineStart);
-        assertEquals(index + ": MarkerInfo colStart", column, actualMarker.colStart);
-        assertEquals(index + ": MarkerInfo message_id", message_id,
-                actualMarker.additionalInfo.get(Flake8Visitor.FLAKE8_MESSAGE_ID));
-        assertEquals(index + ": MarkerInfo message", "Flake8: " + message, actualMarker.message);
-        Map<String, Object> attribMap = actualMarker.getAsMap();
-        //User readable line number (1 based)
-        assertEquals(index + ": IMarker.LINE_NUMBER", line + 1, attribMap.get(IMarker.LINE_NUMBER));
+    public void testMarkerSpecificNoqa() {
+        IDocument document = new Document("" +
+                "y = 20\n" +
+                "    f = 10 #noqa: E302\n" + // this line will not return a flake8 E302 violation code, so the actual return will not be silenced.
+                "def foo():\n" +
+                "        w = 10\n" +
+                "    pass");
 
+        String output = "" +
+                ".\\snippet.py:1:1: E902 IndentationError: unindent does not match any outer level\n" +
+                ".\\snippet.py:2:5: E113 unexpected indentation\n" +
+                ".\\snippet.py:3:1: E302 expected 2 blank lines, found 0\n" +
+                ".\\snippet.py:4:9: E117 over-indented";
+
+        setUpTestForAssertions(document, output);
+        assertMarkersValidity(4);
+        assertMarkerEquals(0, 0, "E902", "IndentationError: unindent does not match any outer level");
+        assertMarkerEquals(1, 4, "E113", "unexpected indentation");
+        assertMarkerEquals(2, 0, "E302", "expected 2 blank lines, found 0");
+        assertMarkerEquals(3, 8, "E117", "over-indented");
+    }
+
+    public void testMarkerSpecificNoqa1() {
+        IDocument document = new Document("" +
+                "y = 20\n" +
+                "    f = 10 #noqa: E113\n" + // this should silence the specific flake8 E113 violation output at line.
+                "def foo():\n" +
+                "        w = 10\n" +
+                "    pass");
+
+        String output = "" +
+                ".\\snippet.py:1:1: E902 IndentationError: unindent does not match any outer level\n" +
+                ".\\snippet.py:2:5: E113 unexpected indentation\n" +
+                ".\\snippet.py:3:1: E302 expected 2 blank lines, found 0\n" +
+                ".\\snippet.py:4:9: E117 over-indented";
+
+        setUpTestForAssertions(document, output);
+        assertMarkersValidity(3);
+        assertMarkerEquals(0, 0, "E902", "IndentationError: unindent does not match any outer level");
+        assertMarkerEquals(1, 4, "E113", "unexpected indentation");
+        assertMarkerEquals(2, 0, "E302", "expected 2 blank lines, found 0");
+        assertMarkerEquals(3, 8, "E117", "over-indented");
     }
 }
