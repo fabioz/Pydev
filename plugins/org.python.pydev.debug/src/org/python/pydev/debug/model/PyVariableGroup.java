d33962d1a4134243fc5cfd3d6c9eab2b4612acb7
Fabio Zadrozny <fabiofz@gmail.com>
2022-03-01 15:59:13 -0300

    Fixed case where the debugger wouldn't expand variable children correctly.

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyVariableGroup.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/model/PyVariableGroup.java
@@ -71,4 +71,9 @@ public class PyVariableGroup extends PyVariable
         return threadId;
     }
 
+    @Override
+    public String getUniqueId() {
+        return "PyVariableGroup - " + threadId + " - " + name;
+    }
+
 }
