13614d2d2cf1ea57571261d9d979776a55c4c9fd
Fabio Zadrozny <fabiofz@gmail.com>
2024-11-10 07:08:49 -0300

    Update typeshed / PyDev 13.0.0

--- rootproject/dev.py
+++ rootproject/dev.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import sys
 import subprocess
 
-_VERSION = '12.2.0'
+_VERSION = '13.0.0'
 
 _pydev_root = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
